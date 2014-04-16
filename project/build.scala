c854e3577106eec24e8a3bc3408906395585e1e0
Eugene Burmako <xeno.by@gmail.com>
2014-04-16 16:58:32 +0200

    creates an aggregating sbt project

--- project/build.scala
+++ project/build.scala
@@ -22,29 +22,8 @@ object build extends Build {
         println(s"Going for custom scala home at $scalaHome")
         Some(file(scalaHome))
       } else None
-    }
-    // TODO: how to I make this recursion work?
-    // run <<= run in Compile in sandbox,
-    // test <<= test in Test in tests
-  )
-
-  // http://stackoverflow.com/questions/20665007/how-to-publish-only-when-on-master-branch-under-travis-and-sbt-0-13
-  val publishOnlyWhenOnMaster = taskKey[Unit]("publish task for Travis (don't publish when building pull requests, only publish when the build is triggered by merge into master)")
-  def publishOnlyWhenOnMasterImpl = Def.taskDyn {
-    import scala.util.Try
-    val travis   = Try(sys.env("TRAVIS")).getOrElse("false") == "true"
-    val pr       = Try(sys.env("TRAVIS_PULL_REQUEST")).getOrElse("false") != "false"
-    val branch   = Try(sys.env("TRAVIS_BRANCH")).getOrElse("??")
-    val snapshot = version.value.trim.endsWith("SNAPSHOT")
-    (travis, pr, branch, snapshot) match {
-      case (true, false, "master", true) => publish
-      case _                             => Def.task ()
-    }
-  }
-
-  lazy val publishableSettings = sharedSettings ++ Seq(
+    },
     publishMavenStyle := true,
-    publishArtifact in Compile := true,
     publishOnlyWhenOnMaster := publishOnlyWhenOnMasterImpl.value,
     publishTo <<= version { v: String =>
       val nexus = "https://oss.sonatype.org/"
@@ -72,41 +51,68 @@ object build extends Build {
         <system>GitHub</system>
         <url>https://github.com/scalareflect/core/issues</url>
       </issueManagement>
-    ),
-    credentials ++= loadCredentials().toList
+    )
   )
 
-  def loadCredentials(): Option[Credentials] = {
-    val mavenSettingsFile = System.getProperty("maven.settings.file")
-    if (mavenSettingsFile != null) {
-      println("Loading Sonatype credentials from " + mavenSettingsFile)
-      try {
-        import scala.xml._
-        val settings = XML.loadFile(mavenSettingsFile)
-        def readServerConfig(key: String) = (settings \\ "settings" \\ "servers" \\ "server" \\ key).head.text
-        Some(Credentials(
-          "Sonatype Nexus Repository Manager",
-          "oss.sonatype.org",
-          readServerConfig("username"),
-          readServerConfig("password")
-        ))
-      } catch {
-        case ex: Exception =>
-          println("Failed to load Maven settings from " + mavenSettingsFile + ": " + ex)
-          None
-      }
-    } else {
-      for {
-        realm <- sys.env.get("SCALAREFLECT_MAVEN_REALM")
-        domain <- sys.env.get("SCALAREFLECT_MAVEN_DOMAIN")
-        user <- sys.env.get("SCALAREFLECT_MAVEN_USER")
-        password <- sys.env.get("SCALAREFLECT_MAVEN_PASSWORD")
-      } yield {
-        println("Loading Sonatype credentials from environment variables")
-        Credentials(realm, domain, user, password)
-      }
+  // http://stackoverflow.com/questions/20665007/how-to-publish-only-when-on-master-branch-under-travis-and-sbt-0-13
+  val publishOnlyWhenOnMaster = taskKey[Unit]("publish task for Travis (don't publish when building pull requests, only publish when the build is triggered by merge into master)")
+  def publishOnlyWhenOnMasterImpl = Def.taskDyn {
+    import scala.util.Try
+    val travis   = Try(sys.env("TRAVIS")).getOrElse("false") == "true"
+    val pr       = Try(sys.env("TRAVIS_PULL_REQUEST")).getOrElse("false") != "false"
+    val branch   = Try(sys.env("TRAVIS_BRANCH")).getOrElse("??")
+    val snapshot = version.value.trim.endsWith("SNAPSHOT")
+    (travis, pr, branch, snapshot) match {
+      case (true, false, "master", true) => publish
+      case _                             => Def.task ()
     }
   }
+
+  lazy val publishableSettings = sharedSettings ++ Seq(
+    publishArtifact in Compile := true,
+    publishArtifact in Test := false,
+    credentials ++= {
+      val mavenSettingsFile = System.getProperty("maven.settings.file")
+      if (mavenSettingsFile != null) {
+        println("Loading Sonatype credentials from " + mavenSettingsFile)
+        try {
+          import scala.xml._
+          val settings = XML.loadFile(mavenSettingsFile)
+          def readServerConfig(key: String) = (settings \\ "settings" \\ "servers" \\ "server" \\ key).head.text
+          Some(Credentials(
+            "Sonatype Nexus Repository Manager",
+            "oss.sonatype.org",
+            readServerConfig("username"),
+            readServerConfig("password")
+          ))
+        } catch {
+          case ex: Exception =>
+            println("Failed to load Maven settings from " + mavenSettingsFile + ": " + ex)
+            None
+        }
+      } else {
+        for {
+          realm <- sys.env.get("SCALAREFLECT_MAVEN_REALM")
+          domain <- sys.env.get("SCALAREFLECT_MAVEN_DOMAIN")
+          user <- sys.env.get("SCALAREFLECT_MAVEN_USER")
+          password <- sys.env.get("SCALAREFLECT_MAVEN_PASSWORD")
+        } yield {
+          println("Loading Sonatype credentials from environment variables")
+          Credentials(realm, domain, user, password)
+        }
+      }
+    }.toList
+  )
+
+  lazy val root = Project(
+    id = "root",
+    base = file("root")
+  ) settings (
+    sharedSettings : _*
+  ) settings (
+    test in Test := (test in tests in Test).value,
+    packagedArtifacts := Map.empty
+  ) aggregate (core, tests)
 
   lazy val core = Project(
     id   = "core",
@@ -132,8 +138,9 @@ object build extends Build {
   ) settings (
     sharedSettings: _*
   ) settings (
-    libraryDependencies += "org.scalatest" %% "scalatest" % "2.1.0" % "test",
+    libraryDependencies += "org.scalatest" %% "scalatest" % "2.1.2" % "test",
     libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.11.3" % "test",
+    packagedArtifacts := Map.empty,
     scalacOptions ++= Seq()
   ) dependsOn (core)
 }
