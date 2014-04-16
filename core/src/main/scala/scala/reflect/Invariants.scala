f9fac7d3d97463d635c7c63034535a0fc257ff1c
Eugene Burmako <xeno.by@gmail.com>
2014-04-16 19:06:56 +0200

    better notation. we're not c# after all

--- core/src/main/scala/scala/reflect/Invariants.scala
+++ core/src/main/scala/scala/reflect/Invariants.scala
@@ -4,58 +4,35 @@ package scala.reflect
 
 object Invariants {
   // TODO: implement this with a macro that fetches and checks an invariant that corresponds to T if any
-  def check[T](artifact: T): Unit = ???
-
-  def Interpolate(tree: Tree.Term.Interpolate): Unit = {
-    // TODO: also check that prefix is alphanumeric
-    require(tree.parts.nonEmpty, "Term.Interpolate's parts must not be empty")
-    require(tree.parts.length == tree.args.length + 1, "Term.Interpolate args' size must be one less than its parts")
-  }
-
-  def Match(tree: Tree.Term.Match): Unit = {
-    require(tree.cases.nonEmpty, "Term.Match must contain at least one case")
-  }
-
-  def Function(tree: Tree.Term.Function): Unit = {
-    require(tree.params.length == 1 || tree.params.forall(!_.annots.contains(Tree.Annot.Implicit)),
-            "function can only have one implicit param")
-  }
-
-  def PartialFunction(tree: Tree.Term.PartialFunction): Unit = {
-    require(tree.cases.nonEmpty, "Term.PartialFunction must contain at least one case")
-  }
-
-  def Extractor(tree: Tree.Pat.Extractor): Unit = {
-    require(tree.ref.isStableId, "extractor pattern ref must be a stable id")
-  }
-
-  def For(tree: Tree.Term.For): Unit = {
-    // TODO: invariant: first element must be generator, at least one element
-  }
-
-  def Typed(tree: Tree.Pat.Typed): Unit = {
-    require(tree.lhs.isInstanceOf[Tree.Pat.Wildcard] || tree.lhs.isInstanceOf[Tree.Term.Ident],
-            "Pat.Type's lhs must be either Pat.Wildcard or Term.Ident")
-  }
-
-  def Select(tree: Tree.Type.Select): Unit = {
-    require(tree.qual.isPath, "Type.Select's qual must be a path")
-  }
-
-  def Singleton(tree: Tree.Type.Singleton): Unit = {
-    require(tree.ref.isPath, "Type.Singleton's ref must be a path")
-  }
-
-  def Package(tree: Tree.Defn.Package): Unit = {
-    require(tree.ref.isQualId, "Defn.Package's ref must be a qualifier id")
-  }
-
-  def Clause(tree: Tree.Import.Clause): Unit = {
-    require(tree.ref.isStableId, "Import.Clause's ref must be a stable id")
-  }
-
-  def Template(tree: Tree.Template): Unit = {
-    require(tree.parents.length == 0 || tree.parents.tail.forall(_.argss.isEmpty),
-            "only first Template parent may have value parameters")
+  def check[T](x: T): Unit = x match {
+    case x: Tree.Term.Interpolate =>
+      // TODO: also check that prefix is alphanumeric
+      require(x.parts.nonEmpty, "Term.Interpolate's parts must not be empty")
+      require(x.parts.length == x.args.length + 1, "Term.Interpolate args' size must be one less than its parts")
+    case x: Tree.Term.Match =>
+      require(x.cases.nonEmpty, "Term.Match must contain at least one case")
+    case x: Tree.Term.Function =>
+      require(x.params.length == 1 || x.params.forall(!_.annots.contains(Tree.Annot.Implicit)),
+              "function can only have one implicit param")
+    case x: Tree.Term.PartialFunction =>
+      require(x.cases.nonEmpty, "Term.PartialFunction must contain at least one case")
+    case x: Tree.Pat.Extractor =>
+      require(x.ref.isStableId, "extractor pattern ref must be a stable id"
+    case x: Tree.Term.For =>
+      // TODO: invariant: first element must be generator, at least one element
+    case x: Tree.Pat.Typed =>
+      require(x.lhs.isInstanceOf[Tree.Pat.Wildcard] || x.lhs.isInstanceOf[Tree.Term.Ident],
+              "Pat.Type's lhs must be either Pat.Wildcard or Term.Ident")
+    case x: Tree.Type.Select =>
+      require(x.qual.isPath, "Type.Select's qual must be a path")
+    case x: Tree.Type.Singleton =>
+      require(x.ref.isPath, "Type.Singleton's ref must be a path")
+    case x: Tree.Defn.Package =>
+      require(x.ref.isQualId, "Defn.Package's ref must be a qualifier id")
+    case x: Tree.Import.Clause =>
+      require(x.ref.isStableId, "Import.Clause's ref must be a stable id")
+    case x: Tree.Template =>
+      require(x.parents.length == 0 || x.parents.tail.forall(_.argss.isEmpty),
+              "only first Template parent may have value parameters")
   }
 }
