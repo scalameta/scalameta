ec30f5a484c72f15e94dafcf2aa41f9e33cbc9c8
Denys Shabalin <denys.shabalin@typesafe.com>
2014-05-03 18:24:13 +0200

    kill unused settings

--- core/src/main/scala/cbc/util/Settings.scala
+++ core/src/main/scala/cbc/util/Settings.scala
@@ -1,23 +1,6 @@
 package cbc.util
 
 object settings {
-  val Xposdebug = false
-  val Xprintpos = false
-  val XshowtreesCompact = false
-  val XshowtreesStringified = false
-  val YmethodInfer = false
-  val Yposdebug = false
-  val Yrangepos = false
-  val YvirtClasses = false
-  val debug = false
-  val developer = false
   val future = false
-  val maxClassfileName = 512
-  val maxTupleArity = 23
-  val nowarnings = false
-  val printtypes = false
-  val prompt = false
-  val uniqid = false
-  val verbose = false
   val nouescape = false
 }
