039ec9009d129f68f402390aff43063d7cceec50
Denys Shabalin <denys.shabalin@typesafe.com>
2014-05-07 20:24:00 +0200

    intelligently filter cases not to generate too much code

--- core/src/main/scala/scala/reflect/quasiquotes/LiftableCaseClass.scala
+++ core/src/main/scala/scala/reflect/quasiquotes/LiftableCaseClass.scala
@@ -4,16 +4,4 @@ import org.scalareflect.LiftableMaterializer
 object Materialized {
   val materializer = new { val u: reflect.runtime.universe.type = reflect.runtime.universe } with LiftableMaterializer
   val instances = materializer.materialize[reflect.core.Tree]
-
-}
-
-object Test extends App {
-  import reflect.runtime.universe.Quasiquote
-  import reflect.core._, Aux._
-  import Materialized.instances._
-  val t =  Decl.Type(Nil, Type.Ident("F", false),
-                 TypeParam(Nil, Some(Type.Ident("T", false)),
-                           Nil, Nil, Nil, TypeBounds.empty) :: Nil,
-                 TypeBounds.empty)
-  println(q"$t")
 }
--- macros/src/main/scala/org/scalareflect/liftable.scala
+++ macros/src/main/scala/org/scalareflect/liftable.scala
@@ -8,35 +8,60 @@ trait LiftableMaterializer {
   def materialize[T]: Any = macro LiftableMaterializerImpl.expand[T]
 }
 
-class LiftableMaterializerImpl(val c: Context) {
-  import c.universe._, internal.gen.{mkAttributedRef => attrRef}
-
+class LiftableMaterializerImpl(val c: Context) { import c.universe._
   val q"$prefix.materialize[..$_]" = c.macroApplication
   val u = q"$prefix.u"
 
-  def fields(sym: ClassSymbol) =
+  def unreachable: Nothing = c.abort(c.macroApplication.pos, "unreachable")
+
+  def fieldNames(sym: ClassSymbol) =
     sym.info.decls.collectFirst {
       case m: MethodSymbol if m.isPrimaryConstructor => m
     }.get.paramLists.head.map { _.name }
 
+  def isBranch(sym: ClassSymbol) = sym.isTrait && sym.isSealed
+  def isLeaf(sym: ClassSymbol) = sym.isCaseClass && sym.isFinal
+
+  def leafs(sym: ClassSymbol): List[ClassSymbol] =
+    if (isLeaf(sym)) List(sym)
+    else if (isBranch(sym)) sym.knownDirectSubclasses.toList.flatMap { case cs: ClassSymbol => cs.info; leafs(cs) }
+    else unreachable
+
   def materialize(sym: ClassSymbol): (c.Tree, List[ClassSymbol]) = {
     sym.info
     val (body, deps) =
-      if (sym.isTrait && sym.isSealed) {
-        val subclasses = sym.knownDirectSubclasses.toList.map { case cs: ClassSymbol => cs }
-        val cases = subclasses.map { subcls => cq""" x: ${attrRef(subcls)} => q"$$x" """ }
-        val body = if (cases.nonEmpty) q"value match { case ..$cases }" else q"???"
-        (body, subclasses)
-      } else if (sym.isCaseClass && sym.isFinal) {
-        val arguments = fields(sym).map { case name: TermName => q""" q"$${value.$name}" """ }
-        val body = q"$u.Apply($u.reify(${attrRef(c.mirror.staticModule(sym.fullName))}).tree, List(..$arguments))"
+      if (isBranch(sym)) {
+        val subclasses = sym.knownDirectSubclasses.map {
+          case cs: ClassSymbol => cs.info; cs
+        }.toList.sortBy(sym => (isLeaf(sym), sym.fullName.toString))
+        val included = {
+          def loop(totry: List[ClassSymbol],
+                   included: List[ClassSymbol]): List[ClassSymbol] = totry match {
+            case Nil          => included
+            case head :: tail =>
+              if (leafs(head).toSet subsetOf included.map(leafs).flatten.toSet) {
+                loop(tail, included)
+              } else
+                loop(tail, head :: included)
+          }
+          loop(subclasses, Nil).reverse
+        }
+        val cases = included.map { subcls =>
+          cq""" x: $subcls => implicitly[$u.Liftable[$subcls]].apply(x) """
+        }
+        val body = if (cases.nonEmpty) q"(value: @_root_.scala.unchecked) match { case ..$cases }" else q"???"
+        (body, subclasses.toList)
+
+      } else if (isLeaf(sym)) {
+        val arguments = fieldNames(sym).map { case name: TermName => q""" q"$${value.$name}" """ }
+        val body = q"$u.Apply($u.reify(${c.mirror.staticModule(sym.fullName)}).tree, List(..$arguments))"
         (body, Nil)
       } else
-        c.abort(c.macroApplication.pos, s"can't materialize liftable for $sym")
+        unreachable
     val objName = TermName(s"Liftable for ${sym.fullName.replace(".", "_")}")
     val expansion = q"""
-      implicit object $objName extends $u.Liftable[${attrRef(sym)}] {
-        def apply(value: ${attrRef(sym)}) = $body
+      implicit object $objName extends $u.Liftable[$sym] {
+        def apply(value: $sym) = $body
       }
     """
     (expansion, deps)
@@ -48,7 +73,7 @@ class LiftableMaterializerImpl(val c: Context) {
       case head :: tail =>
         val (expansion, deps) = materialize(head)
         val newAlreadyExpanded = alreadyExpanded + head
-        val newToExpand = (deps ++ tail).filter(dep => !alreadyExpanded.contains(dep))
+        val newToExpand = (deps ++ tail).filter(dep => !newAlreadyExpanded.contains(dep))
         expansion :: loop(newToExpand, newAlreadyExpanded)
     }
     loop(rootSym :: Nil, Set.empty)
