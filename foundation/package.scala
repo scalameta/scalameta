1e826fef36552434b99c8a6685463923fa7fbcfd
Eugene Burmako <xeno.by@gmail.com>
2014-05-21 14:46:33 +0200

    unreachable is now an Error

--- foundation/package.scala
+++ foundation/package.scala
@@ -1,6 +1,6 @@
 package org
 
 package object scalareflect {
-  case object UnreachableException extends Exception("this code path should've been unreachable")
-  def unreachable = throw UnreachableException
+  case object UnreachableError extends Error("this code path should've been unreachable")
+  def unreachable = throw UnreachableError
 }
