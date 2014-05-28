982fa8c754b54f78ff650b8a72109e0725369929
Eugene Burmako <xeno.by@gmail.com>
2014-05-28 18:40:49 +0200

    a todo on typeSignatureIn

--- reflection/semantic/Members.scala
+++ reflection/semantic/Members.scala
@@ -10,6 +10,7 @@ trait MemberOps {
   implicit class SemanticMemberOps(tree: Member) {
     // TODO: expose type parameter instantiation facilities, e.g. `def foo[T]: T = ...` => `def foo: Int = ...`
     def ref: Ref = tree match {
+      // TODO: this logic is not enough. if a member is a synthetic typeSignatureIn'd thing, we also need to remember its prefix
       case self: Aux.Self => self.name.getOrElse(Term.This(None))
       case named: Has.Name => named.name
     }
