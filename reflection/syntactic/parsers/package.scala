dcdabaf4023818ddd252331905d4ccc298730b75
Eugene Burmako <xeno.by@gmail.com>
2014-06-12 11:14:25 +0200

    read => parse

--- reflection/syntactic/parsers/package.scala
+++ reflection/syntactic/parsers/package.scala
@@ -17,18 +17,18 @@ package object parsers {
   implicit val stringToSource: Convert[String, Source] = Convert.apply(Source.String(_))
   implicit val fileToSource: Convert[java.io.File, Source] = Convert.apply(Source.File(_))
 
-  trait Read[T] extends Convert[Source, T]
-  object Read {
-    def apply[T](f: Source => T): Read[T] = new Read[T] { def apply(source: Source): T = f(source) }
-    implicit val readCompUnit: Read[Aux.CompUnit] = apply(source => new Parser(source).parseTopLevel())
-    implicit val readTerm: Read[Term] = apply(source => new Parser(source).parseTerm())
-    implicit val readType: Read[Type] = apply(source => new Parser(source).parseType())
-    implicit val readPat: Read[Pat] = apply(source => new Parser(source).parsePat())
-    implicit val readStats: Read[List[Stmt.Template]] = apply(source => new Parser(source).parseStats())
+  trait Parse[T] extends Convert[Source, T]
+  object Parse {
+    def apply[T](f: Source => T): Parse[T] = new Parse[T] { def apply(source: Source): T = f(source) }
+    implicit val readCompUnit: Parse[Aux.CompUnit] = apply(source => new Parser(source).parseTopLevel())
+    implicit val readTerm: Parse[Term] = apply(source => new Parser(source).parseTerm())
+    implicit val readType: Parse[Type] = apply(source => new Parser(source).parseType())
+    implicit val readPat: Parse[Pat] = apply(source => new Parser(source).parsePat())
+    implicit val readStats: Parse[List[Stmt.Template]] = apply(source => new Parser(source).parseStats())
   }
 
   implicit class RichSource[T](val sourceLike: T)(implicit ev: Convert[T, Source]) {
     private val source: Source = ev(sourceLike)
-    def read[T](implicit ev: Read[T]): T = ev(source)
+    def parse[T](implicit ev: Parse[T]): T = ev(source)
   }
 }
