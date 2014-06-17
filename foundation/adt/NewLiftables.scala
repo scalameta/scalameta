xeno-by/core

simple quasiquoting facility: no unquoting, automatic liftable

foundation/adt/NewLiftables.scala
@@ -1,0 +1,59 @@
+package org.scalareflect.adt
+
+import scala.language.experimental.macros
+import scala.reflect.macros.blackbox.Context
+import org.scalareflect.adt.Internal.Adt
+import scala.{Seq => _}
+import scala.collection.immutable.Seq
+
+trait NewLiftables {
+  val u: scala.reflect.macros.Universe
+  implicit def materializeAdt[T <: Adt]: u.Liftable[T] = macro NewLiftableMacros.impl[T]
+  implicit def liftSeq[T: u.Liftable]: u.Liftable[Seq[T]] = u.Liftable { seq => u.Liftable.liftList[T].apply(seq.toList) }
+}
+
+class NewLiftableMacros(val c: Context) extends AdtReflection {
+  val u: c.universe.type = c.universe
+  import c.universe._
+  def impl[T: WeakTypeTag]: c.Tree = {
+    val root = weakTypeOf[T].typeSymbol.asAdt.root
+    val leafs = weakTypeOf[T].typeSymbol.asAdt.root.allLeafs
+    if (leafs.isEmpty) c.abort(c.enclosingPosition, s"$root hasn't been initialized properly")
+    val u = q"${c.prefix}.u"
+    val mainParam = c.freshName(TermName("x"))
+    val mainModule = c.freshName(TermName("Module"))
+    val mainMethod = c.freshName(TermName("instanceFor" + root.prefix.capitalize.replace(".", "")))
+    val localParam = c.freshName(TermName("x"))
+    val leafLiftNames = leafs.map(leaf => c.freshName(TermName("lift" + leaf.prefix.capitalize.replace(".", ""))))
+    val lifts = leafs.zip(leafLiftNames).map({ case (leaf, name) =>
+      val init = q"""$u.Ident($u.TermName("_root_"))""": Tree
+      val namePath = leaf.sym.fullName.split('.').foldLeft(init)((acc, part) => q"$u.Select($acc, $u.TermName($part))")
+      val localParam = c.freshName(TermName("x"))
+      val args = leaf.fields.map(f => {
+        val name = q"$u.Ident($u.TermName(${f.name.toString}))"
+        val value = q"_root_.scala.Predef.implicitly[$u.Liftable[${f.tpe}]].apply($localParam.${f.name})"
+        q"$u.AssignOrNamedArg($name, $value)"
+      })
+      q"def $name($localParam: ${leaf.sym.asType.toType}): $u.Tree = $u.Apply($namePath, $args)"
+    })
+    val clauses = leafs.zip(leafLiftNames).map({ case (leaf, name) =>
+      q"if ($mainParam.isInstanceOf[${leaf.sym.asType.toType}]) result = $name($mainParam.asInstanceOf[${leaf.sym.asType.toType}])"
+    })
+    q"""
+      $u.Liftable(($mainParam: ${weakTypeOf[T]}) => {
+        object $mainModule {
+          val materializeAdt = "shadow the adt materializer"
+          ..$lifts
+          implicit def $mainMethod[T <: ${root.sym}]: $u.Liftable[T] = $u.Liftable(($localParam: T) => {
+            var result: $u.Tree = null
+            if ($mainParam == null) result = q"null"
+            ..$clauses
+            if (result == null) sys.error("none of leafs matched " + $mainParam.getClass)
+            result
+          })
+        }
+        $mainModule.$mainMethod.apply($mainParam)
+      })
+    """
+  }
+}

foundation/ast/branch.scala
@@ -14,8 +14,9 @@ class BranchMacros(val c: Context) {
   def impl(annottees: Tree*): Tree = {
     def transform(cdef: ClassDef): ClassDef = {
       val ClassDef(mods @ Modifiers(flags, privateWithin, anns), name, tparams, templ) = cdef
-      // TODO: this should emit @ast.branch, not @adt.branch
-      val anns1 = q"new _root_.org.scalareflect.adt.branch" +: anns
+      val Adt = q"_root_.org.scalareflect.adt"
+      val AstInternal = q"_root_.org.scalareflect.ast.internal"
+      val anns1 = q"new $AstInternal.branch" +: q"new $Adt.branch" +: anns
       ClassDef(Modifiers(flags, privateWithin, anns1), name, tparams, templ)
     }
     val expanded = annottees match {

foundation/ast/internal.scala
@@ -5,6 +5,9 @@ import scala.annotation.StaticAnnotation
 import scala.reflect.macros.blackbox.Context
 
 object internal {
+  trait Ast extends org.scalareflect.adt.Internal.Adt
+  class root extends StaticAnnotation
+  class branch extends StaticAnnotation
   class astClass extends StaticAnnotation
   class astCompanion extends StaticAnnotation
 
@@ -14,15 +17,12 @@ object internal {
   def initField[T](f: T): T = macro Macros.initField
   def initParam[T](f: T): T = macro Macros.initField
 
-  class Macros(val c: Context) {
+  class Macros(val c: Context) extends org.scalareflect.adt.AdtReflection {
+    val u: c.universe.type = c.universe
     import c.universe._
     import definitions._
     def productPrefix[T](implicit T: c.WeakTypeTag[T]) = {
-      def loop(sym: Symbol): String = {
-        if (sym.owner.isPackageClass) sym.name.toString
-        else loop(sym.owner) + "." + sym.name.toString
-      }
-      q"${loop(T.tpe.typeSymbol)}"
+      q"${T.tpe.typeSymbol.asLeaf.prefix}"
     }
     def loadField(f: c.Tree) = {
       val q"this.$finternalName" = f

foundation/ast/root.scala
@@ -20,6 +20,8 @@ class RootMacros(val c: Context) {
       val Origin = tq"_root_.scala.reflect.core.Origin"
       val SeqAny = tq"_root_.scala.collection.immutable.Seq[_root_.scala.Any]"
       val Scratchpads = tq"_root_.scala.Predef.Map[$Host, $SeqAny]"
+      val Adt = q"_root_.org.scalareflect.adt"
+      val AstInternal = q"_root_.org.scalareflect.ast.internal"
       val q"..$boilerplate" = q"""
         // NOTE: these are internal APIs designed to be used only by hosts
         // TODO: these APIs will most likely change in the future
@@ -39,9 +41,9 @@ class RootMacros(val c: Context) {
         private[core] def internalCopy(prototype: $Tree = internalPrototype, parent: $Tree = internalParent, scratchpads: $Scratchpads = internalScratchpads, origin: $Origin = origin): ThisType
       """
       val stats1 = stats ++ boilerplate
-      // TODO: this should emit @ast.root, not @adt.root
-      val anns1 = q"new _root_.org.scalareflect.adt.root" +: anns
-      q"${Modifiers(flags, privateWithin, anns1)} trait $name[..$tparams] extends { ..$earlydefns } with ..$parents { $self => ..$stats1 }"
+      val anns1 = q"new $AstInternal.root" +: q"new $Adt.root" +: anns
+      val parents1 = parents :+ tq"$AstInternal.Ast"
+      q"${Modifiers(flags, privateWithin, anns1)} trait $name[..$tparams] extends { ..$earlydefns } with ..$parents1 { $self => ..$stats1 }"
     }
     val expanded = annottees match {
       case (cdef @ ClassDef(mods, _, _, _)) :: rest if mods.hasFlag(TRAIT) => transform(cdef) :: rest

reflection/syntactic/quasiquotes/Macros.scala
@@ -3,10 +3,17 @@ package syntactic.quasiquotes
 
 import scala.reflect.macros.whitebox.Context
 import scala.reflect.core.{Tree => PalladiumTree}
+import org.scalareflect.adt._
 
-class Macros[C <: Context](val c: C) {
-  import c.universe._
-  def apply(macroApplication: Tree, parse: String => PalladiumTree): Tree = {
-    ???
+class Macros[C <: Context](val c: C) extends AdtReflection with NewLiftables {
+  val u: c.universe.type = c.universe
+  import c.universe.{Tree => _, _}
+  import c.universe.{Tree => ScalaTree}
+  def apply(macroApplication: ScalaTree, parse: String => PalladiumTree): ScalaTree = {
+    val q"$_($_.apply(..$parts)).$_.apply[..$_](..$args)" = macroApplication
+    if (args.length != 0) c.abort(macroApplication.pos, "unquoting is not supported yet")
+    val liveTree: PalladiumTree = parse(parts.map{case q"${part: String}" => part}.mkString(""))
+    val reifiedTree: ScalaTree = implicitly[Liftable[PalladiumTree]].apply(liveTree)
+    reifiedTree
   }
 }

scalameta/scalameta

foundation/adt/NewLiftables.scala
@@ -1,0 +1,59 @@
+package org.scalameta.adt
+
+import scala.language.experimental.macros
+import scala.reflect.macros.blackbox.Context
+import org.scalameta.adt.Internal.Adt
+import scala.{Seq => _}
+import scala.collection.immutable.Seq
+
+trait NewLiftables {
+  val u: scala.reflect.macros.Universe
+  implicit def materializeAdt[T <: Adt]: u.Liftable[T] = macro NewLiftableMacros.impl[T]
+  implicit def liftSeq[T: u.Liftable]: u.Liftable[Seq[T]] = u.Liftable { seq => u.Liftable.liftList[T].apply(seq.toList) }
+}
+
+class NewLiftableMacros(val c: Context) extends AdtReflection {
+  val u: c.universe.type = c.universe
+  import c.universe._
+  def impl[T: WeakTypeTag]: c.Tree = {
+    val root = weakTypeOf[T].typeSymbol.asAdt.root
+    val leafs = weakTypeOf[T].typeSymbol.asAdt.root.allLeafs
+    if (leafs.isEmpty) c.abort(c.enclosingPosition, s"$root hasn't been initialized properly")
+    val u = q"${c.prefix}.u"
+    val mainParam = c.freshName(TermName("x"))
+    val mainModule = c.freshName(TermName("Module"))
+    val mainMethod = c.freshName(TermName("instanceFor" + root.prefix.capitalize.replace(".", "")))
+    val localParam = c.freshName(TermName("x"))
+    val leafLiftNames = leafs.map(leaf => c.freshName(TermName("lift" + leaf.prefix.capitalize.replace(".", ""))))
+    val lifts = leafs.zip(leafLiftNames).map({ case (leaf, name) =>
+      val init = q"""$u.Ident($u.TermName("_root_"))""": Tree
+      val namePath = leaf.sym.fullName.split('.').foldLeft(init)((acc, part) => q"$u.Select($acc, $u.TermName($part))")
+      val localParam = c.freshName(TermName("x"))
+      val args = leaf.fields.map(f => {
+        val name = q"$u.Ident($u.TermName(${f.name.toString}))"
+        val value = q"_root_.scala.Predef.implicitly[$u.Liftable[${f.tpe}]].apply($localParam.${f.name})"
+        q"$u.AssignOrNamedArg($name, $value)"
+      })
+      q"def $name($localParam: ${leaf.sym.asType.toType}): $u.Tree = $u.Apply($namePath, $args)"
+    })
+    val clauses = leafs.zip(leafLiftNames).map({ case (leaf, name) =>
+      q"if ($mainParam.isInstanceOf[${leaf.sym.asType.toType}]) result = $name($mainParam.asInstanceOf[${leaf.sym.asType.toType}])"
+    })
+    q"""
+      $u.Liftable(($mainParam: ${weakTypeOf[T]}) => {
+        object $mainModule {
+          val materializeAdt = "shadow the adt materializer"
+          ..$lifts
+          implicit def $mainMethod[T <: ${root.sym}]: $u.Liftable[T] = $u.Liftable(($localParam: T) => {
+            var result: $u.Tree = null
+            if ($mainParam == null) result = q"null"
+            ..$clauses
+            if (result == null) sys.error("none of leafs matched " + $mainParam.getClass)
+            result
+          })
+        }
+        $mainModule.$mainMethod.apply($mainParam)
+      })
+    """
+  }
+}

foundation/ast/branch.scala
@@ -14,8 +14,9 @@ class BranchMacros(val c: Context) {
   def impl(annottees: Tree*): Tree = {
     def transform(cdef: ClassDef): ClassDef = {
       val ClassDef(mods @ Modifiers(flags, privateWithin, anns), name, tparams, templ) = cdef
-      // TODO: this should emit @ast.branch, not @adt.branch
-      val anns1 = q"new _root_.org.scalameta.adt.branch" +: anns
+      val Adt = q"_root_.org.scalameta.adt"
+      val AstInternal = q"_root_.org.scalameta.ast.internal"
+      val anns1 = q"new $AstInternal.branch" +: q"new $Adt.branch" +: anns
       ClassDef(Modifiers(flags, privateWithin, anns1), name, tparams, templ)
     }
     val expanded = annottees match {

foundation/ast/internal.scala
@@ -5,6 +5,9 @@ import scala.annotation.StaticAnnotation
 import scala.reflect.macros.blackbox.Context
 
 object internal {
+  trait Ast extends org.scalameta.adt.Internal.Adt
+  class root extends StaticAnnotation
+  class branch extends StaticAnnotation
   class astClass extends StaticAnnotation
   class astCompanion extends StaticAnnotation
 
@@ -14,15 +17,12 @@ object internal {
   def initField[T](f: T): T = macro Macros.initField
   def initParam[T](f: T): T = macro Macros.initField
 
-  class Macros(val c: Context) {
+  class Macros(val c: Context) extends org.scalameta.adt.AdtReflection {
+    val u: c.universe.type = c.universe
     import c.universe._
     import definitions._
     def productPrefix[T](implicit T: c.WeakTypeTag[T]) = {
-      def loop(sym: Symbol): String = {
-        if (sym.owner.isPackageClass) sym.name.toString
-        else loop(sym.owner) + "." + sym.name.toString
-      }
-      q"${loop(T.tpe.typeSymbol)}"
+      q"${T.tpe.typeSymbol.asLeaf.prefix}"
     }
     def loadField(f: c.Tree) = {
       val q"this.$finternalName" = f

foundation/ast/root.scala
@@ -20,6 +20,8 @@ class RootMacros(val c: Context) {
       val Origin = tq"_root_.scala.meta.Origin"
       val SeqAny = tq"_root_.scala.collection.immutable.Seq[_root_.scala.Any]"
       val Scratchpads = tq"_root_.scala.Predef.Map[$Host, $SeqAny]"
+      val Adt = q"_root_.org.scalameta.adt"
+      val AstInternal = q"_root_.org.scalameta.ast.internal"
       val q"..$boilerplate" = q"""
         // NOTE: these are internal APIs designed to be used only by hosts
         // TODO: these APIs will most likely change in the future
@@ -39,9 +41,9 @@ class RootMacros(val c: Context) {
         private[meta] def internalCopy(prototype: $Tree = internalPrototype, parent: $Tree = internalParent, scratchpads: $Scratchpads = internalScratchpads, origin: $Origin = origin): ThisType
       """
       val stats1 = stats ++ boilerplate
-      // TODO: this should emit @ast.root, not @adt.root
-      val anns1 = q"new _root_.org.scalameta.adt.root" +: anns
-      q"${Modifiers(flags, privateWithin, anns1)} trait $name[..$tparams] extends { ..$earlydefns } with ..$parents { $self => ..$stats1 }"
+      val anns1 = q"new $AstInternal.root" +: q"new $Adt.root" +: anns
+      val parents1 = parents :+ tq"$AstInternal.Ast"
+      q"${Modifiers(flags, privateWithin, anns1)} trait $name[..$tparams] extends { ..$earlydefns } with ..$parents1 { $self => ..$stats1 }"
     }
     val expanded = annottees match {
       case (cdef @ ClassDef(mods, _, _, _)) :: rest if mods.hasFlag(TRAIT) => transform(cdef) :: rest

scalameta/syntactic/quasiquotes/Macros.scala
@@ -3,10 +3,18 @@ package syntactic.quasiquotes
 
 import scala.reflect.macros.whitebox.Context
 import scala.meta.{Tree => PalladiumTree}
+import org.scalameta.adt._
 
-class Macros[C <: Context](val c: C) {
-  import c.universe._
-  def apply(macroApplication: Tree, parse: String => PalladiumTree): Tree = {
-    ???
+class Macros[C <: Context](val c: C) extends AdtReflection with NewLiftables {
+  val u: c.universe.type = c.universe
+  import c.universe.{Tree => _, _}
+  import c.universe.{Tree => ScalaTree}
+  def apply(macroApplication: ScalaTree, parse: String => PalladiumTree): ScalaTree = {
+    val TermQuote = "denied" // TODO: find a cleaner way out of this mess
+    val q"$_($_.apply(..$parts)).$_.apply[..$_](..$args)" = macroApplication
+    if (args.length != 0) c.abort(macroApplication.pos, "unquoting is not supported yet")
+    val liveTree: PalladiumTree = parse(parts.map{case q"${part: String}" => part}.mkString(""))
+    val reifiedTree: ScalaTree = implicitly[Liftable[PalladiumTree]].apply(liveTree)
+    reifiedTree
   }
 }