af6db91511969a6377da3570a952bf1c78f0c1e5
Eugene Burmako <xeno.by@gmail.com>
2014-07-18 14:44:13 +0300

    does away with syntax analyzer hijacking

--- plugin/src/main/scala/Plugin.scala
+++ plugin/src/main/scala/Plugin.scala
@@ -3,13 +3,11 @@ package internal.hosts.scalacompiler
 
 import scala.tools.nsc.Global
 import scala.tools.nsc.plugins.{Plugin => NscPlugin, PluginComponent => NscPluginComponent}
-import parser.HijackSyntaxAnalyzer
 import macros.{MacroPlugin => PalladiumMacroPlugin}
 import macros.RenumberPhase
 import persistence.PersistencePhase
 
 class Plugin(val global: Global) extends NscPlugin
-                                    with HijackSyntaxAnalyzer
                                     with PalladiumMacroPlugin
                                     with RenumberPhase
                                     with PersistencePhase
@@ -18,7 +16,5 @@ class Plugin(val global: Global) extends NscPlugin
   val description = """Hosts Project Palladium in scalac.
   For more information visit https://github.com/scalareflect/scalahost"""
   val components = List[NscPluginComponent](RenumberComponent, PersistenceComponent)
-  val hijackedSyntaxAnalyzer = hijackSyntaxAnalyzer()
-  if (global.syntaxAnalyzer ne hijackedSyntaxAnalyzer) sys.error("failed to hijack syntaxAnalyzer")
   global.analyzer.addMacroPlugin(palladiumMacroPlugin)
 }
