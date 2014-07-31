e5e5a8218a334099f9225aa047548174b6458ce1
Eugene Burmako <xeno.by@gmail.com>
2014-07-31 12:40:59 +0200

    migrates to scala 2.11.2

--- project/build.scala
+++ project/build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object build extends Build {
   lazy val sharedSettings = Defaults.defaultSettings ++ Seq(
-    scalaVersion := "2.11.0",
+    scalaVersion := "2.11.2",
     crossVersion := CrossVersion.binary,
     version := "0.1.0-SNAPSHOT",
     organization := "org.scalameta",
