72f838d2d5c0f317136126fb294d24e00ce1c813
Eugene Burmako <xeno.by@gmail.com>
2014-08-07 14:43:36 +0200

    fixes broken tests (that's an embarassment!)

--- tests/test/show/ShowSuite.scala
+++ tests/test/show/ShowSuite.scala
@@ -4,7 +4,7 @@ import scala.meta.syntactic.show._
 class ShowSuite extends ParseSuite {
   test("val x: Int (raw)") {
     val valx = templStat("val x: Int")
-    assert(valx.show[Raw] === "Decl.Val(List(), List(Term.Name(x)), Type.Name(Int))")
+    assert(valx.show[Raw] === "Decl.Val(List(), List(Term.Name(\"x\")), Type.Name(\"Int\"))")
   }
 
   test("val x: Int (code)") {
