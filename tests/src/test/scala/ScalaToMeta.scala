f7a9056682e47e8932b6c2ab6bd1859db364db42
Eugene Burmako <xeno.by@gmail.com>
2014-09-03 14:51:46 +0200

    fully automates the tests that I used to run and eyeball manually

--- tests/src/test/scala/ScalaToMeta.scala
+++ tests/src/test/scala/ScalaToMeta.scala
@@ -119,3 +119,92 @@ class ScalaToMeta extends ToolboxHarness {
       |}
     """)
   }
+
+  test("old Join test") {
+    checkScalaToMeta("""
+      |import org.scalatest._
+      |
+      |class OldMacros extends FunSuite {
+      |  test("old join") {
+      |    import scala.language.reflectiveCalls
+      |    val x = new { val x = 2 }
+      |    val y = new { val y = 3 }
+      |    val result = Join(x, y)
+      |    assert(result.x === 2)
+      |    assert(result.y === 3)
+      |  }
+      |}
+    """,
+    """
+      |import org.scalatest._
+      |class OldMacros extends FunSuite {
+      |  test("old join") {
+      |    import scala.language.reflectiveCalls
+      |    val x = new { val x = 2 }
+      |    val y = new { val y = 3 }
+      |    val result = Join(x, y)
+      |    assert(result.x === 2)
+      |    assert(result.y === 3)
+      |  }
+      |}
+    """)
+  }
+
+  test("new Join test") {
+    checkScalaToMeta("""
+      |import org.scalatest._
+      |
+      |import scala.language.reflectiveCalls
+      |import scala.language.experimental.macros
+      |import scala.meta._
+      |import scala.meta.semantic._
+      |import scala.meta.semantic.errors.throwExceptions
+      |
+      |class NewMacros extends FunSuite {
+      |  test("new join") {
+      |    def join[T, U](x: T, y: U): Any = macro {
+      |      val xfields = x.tpe.vals.map(f => f -> q"xtemp")
+      |      val yfields = y.tpe.vals.map(f => f -> q"ytemp")
+      |      val getters = (xfields ++ yfields).map{ case (f, ref) => q"val ${f.name} = $ref.${f.name}" }
+      |      c.whitebox(qQQQ
+      |        val xtemp = $x
+      |        val ytemp = $y
+      |        new { ..$getters }
+      |      QQQ)
+      |    }
+      |    val result = join(new { val x = 2 }, new { val y = 3 })
+      |    assert(result.x === 2)
+      |    assert(result.y === 3)
+      |  }
+      |}
+    """,
+    """
+      |import org.scalatest._
+      |import scala.language.reflectiveCalls
+      |import scala.language.experimental.macros
+      |import scala.meta._
+      |import scala.meta.semantic._
+      |import scala.meta.semantic.errors.throwExceptions
+      |class NewMacros extends FunSuite {
+      |  test("new join") {
+      |    def join[T, U](x: T, y: U): Any = macro {
+      |      val xfields = x.tpe.vals.map(f => f -> q"xtemp")
+      |      val yfields = y.tpe.vals.map(f => f -> q"ytemp")
+      |      val getters = (xfields ++ yfields).map({
+      |        case (f, ref) =>
+      |          q"val ${f.name} = $ref.${f.name}"
+      |      })
+      |      c.whitebox(qQQQ
+      |        val xtemp = $x
+      |        val ytemp = $y
+      |        new { ..$getters }
+      |      QQQ)
+      |    }
+      |    val result = join(new { val x = 2 }, new { val y = 3 })
+      |    assert(result.x === 2)
+      |    assert(result.y === 3)
+      |  }
+      |}
+    """)
+  }
+}
