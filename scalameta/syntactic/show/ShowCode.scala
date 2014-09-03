114970f5847610ab5c6dd3357c1e6b955428b8ca
Eugene Burmako <xeno.by@gmail.com>
2014-09-03 13:50:34 +0200

    minor prettyprinting fix for Self in ShowCode

--- scalameta/syntactic/show/ShowCode.scala
+++ scalameta/syntactic/show/ShowCode.scala
@@ -252,14 +252,14 @@ object Code {
     case t: Parent => s(t.tpe, t.argss)
     case t: Self =>
       if (t.name.isEmpty && t.decltpe.isEmpty) s()
-      else s(" ", t.name, t.decltpe, " => ")
+      else s(" ", t.name, t.decltpe, " =>")
     case t: Template =>
       if (t.early.isEmpty && t.parents.isEmpty && t.self.name.isEmpty && t.self.decltpe.isEmpty && t.stats.isEmpty) s()
       else {
         val pearly = if (t.early.isEmpty) s() else s("{ ", r(t.early, "; "), " } with ")
         val pbody = if (t.self.name.isEmpty && t.self.decltpe.isEmpty && !t.hasStats) s()
-                    else if (t.stats.length == 1 && !s(t.stats.head).toString.contains("\n")) s("{ ", t.self, t.stats.head, " }")
-                    else s("{", t.self, r(t.stats.map(i(_)), ""), n("}"))
+                    else if (t.stats.length == 1 && !s(t.stats.head).toString.contains("\n")) s("{ ", t.self, " ", t.stats.head, " }")
+                    else s("{", t.self, " ", r(t.stats.map(i(_)), ""), n("}"))
         val pparents = if (t.parents.nonEmpty) s(r(t.parents, " with "), " ") else s()
         s(pearly, pparents, pbody)
       }
