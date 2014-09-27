cca5622c4de16d06d4c983eaa09c77813399d66b
Eugene Burmako <xeno.by@gmail.com>
2014-09-27 10:40:35 +0200

    prettyprinting fix for semicolons after case statements

--- tests/test/show/ShowSuite.scala
+++ tests/test/show/ShowSuite.scala
@@ -183,4 +183,14 @@ class ShowSuite extends ParseSuite {
   test("secondary ctor") {
     assert(compUnit("class C(x: Int) { def this() { this(2); println(\"OBLIVION!!!\") } }").show[Code] === "class C(x: Int) { def this() { this(2); println(\"OBLIVION!!!\") } }")
   }
+
+  test("case semicolons") {
+    assert(templStat("x match { case y => foo1; foo2 }").show[Code] === """
+      |x match {
+      |  case y =>
+      |    foo1
+      |    foo2
+      |}
+    """.trim.stripMargin)
+  }
 }
