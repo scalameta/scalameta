750855acc62c5ec4823f2957eb21028e5189c972
Eugene Burmako <xeno.by@gmail.com>
2014-09-29 15:42:48 +0200

    okay now the test sort of works

---
+++ tests/src/test/resources/ScalaToMeta/Types/Expected.scala
@@ -0,0 +1,31 @@
+object Types {
+  class A
+  class B
+  class C
+  class X
+  trait Y
+  class Z[T] { type U = T }
+  class D1 extends X
+  class D2 extends B with Y
+  class D3 extends Y
+  class D4 extends B with Y
+  val x5 = new X
+  val x6 = new X
+  val x7 = new B with Y
+  @X @X class D8
+  type T9 = X => X
+  type T10 = (X, X)
+  type T11 = X { def x: Int }
+  type T12 = B with Y { def x: Int }
+  new X match {
+    case x13: X =>
+  }
+  class D14[T](val x15: X)(implicit evidence$1: Z[T]) { self16: X =>
+    val x17: X = new X
+    def x18(x19: X)(x20: X): X = x20: X: @X
+    type T21 >: X <: X
+    type T22 = Z[X]
+    type T23 = Z[T] forSome { type T }
+    type T24 = Z[X]#U
+  }
+}
