5a0e5c16e2b1f66d0052570b1b3414377ae662b1
Eugene Burmako <xeno.by@gmail.com>
2014-10-06 14:42:31 +0200

    new tests for the upcoming prototype of blackbox def macros

---
+++ tests/src/test/resources/ScalaToMeta/OldSerializer/Original.scala
@@ -0,0 +1,8 @@
+sealed trait List
+final case class Cons(head: Int, tail: List) extends List
+final case object Nil extends List
+
+object OldSerializer {
+  val list: List = Cons(1, Cons(2, Nil))
+  println(serialization.serialize(list))
+}
