7e3959654dd9586f37ab885f77e5345a1b899287
Eugene Burmako <xeno.by@gmail.com>
2014-10-06 23:22:34 +0200

    much better diagnostics for require

--- foundation/invariants/Exceptions.scala
+++ foundation/invariants/Exceptions.scala
@@ -2,13 +2,14 @@ package org.scalameta.invariants
 
 class InvariantFailedException(message: String) extends Exception(message)
 object InvariantFailedException {
-  def raise(invariant: String, failures: List[String]): InvariantFailedException = {
+  def raise(invariant: String, failures: List[String], locals: Map[String, Any]): InvariantFailedException = {
     val mandatory = s"""
       |invariant failed:
       |when verifying $invariant
       |found that ${failures.head}
     """.trim.stripMargin
     val optionalFailures = failures.tail.headOption.map(_ => "\n" + failures.tail.map("and also " + _).mkString("\n")).getOrElse("")
-    throw new InvariantFailedException(mandatory + optionalFailures)
+    val optionalLocals = if (locals.nonEmpty) "\n" + locals.toList.sortBy(_._1).map({ case (k, v) => s"where $k = $v"}).mkString("\n")
+    throw new InvariantFailedException(mandatory + optionalFailures + optionalLocals)
   }
 }
