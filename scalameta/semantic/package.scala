1683b36e1b539271dee81dc32d51a22bbf4ec623
Eugene Burmako <xeno.by@gmail.com>
2014-10-16 16:26:11 +0200

    moves macro APIs into a dedicated package (no more c._)

--- scalameta/semantic/package.scala
+++ scalameta/semantic/package.scala
@@ -349,12 +349,4 @@ package object semantic {
     @hosted def tparams(name: String): TypeParam.Named = tree.uniqueMember[TypeParam.Named](name.toString)
     @hosted def tparams(name: scala.Symbol): TypeParam.Named = tree.uniqueMember[TypeParam.Named](name.toString)
   }
-
-  object c {
-    @hosted(macroApi = true) def warning(msg: String): Unit = delegate
-    @hosted(macroApi = true) def error(msg: String): Unit = delegate
-    @hosted(macroApi = true) def abort(msg: String): Nothing = delegate
-    @hosted(macroApi = true) def resources: Seq[String] = delegate
-    @hosted(macroApi = true) def resource(url: String): Array[Byte] = delegate
-  }
 }
