593f8f31f14ce21bb40156409dd1edb151bb205d
Eugene Burmako <xeno.by@gmail.com>
2014-11-13 14:36:30 +0100

    finishes evicting stubs from the semantic API

--- scalameta/macros/Macros.scala
+++ scalameta/macros/Macros.scala
@@ -5,9 +5,9 @@ import org.scalameta.adt._
 import org.scalameta.annotations._
 
 package object macros {
-  @hosted(macroApi = true) def warning(msg: String): Unit = delegate
-  @hosted(macroApi = true) def error(msg: String): Unit = delegate
-  @hosted(macroApi = true) def abort(msg: String): Nothing = delegate
-  @hosted(macroApi = true) def resources: Seq[String] = delegate
-  @hosted(macroApi = true) def resource(url: String): Array[Byte] = delegate
+  @hosted(macroApi = true) def warning(msg: String): Unit = ???
+  @hosted(macroApi = true) def error(msg: String): Unit = ???
+  @hosted(macroApi = true) def abort(msg: String): Nothing = ???
+  @hosted(macroApi = true) def resources: Seq[String] = ???
+  @hosted(macroApi = true) def resource(url: String): Array[Byte] = ???
 }
