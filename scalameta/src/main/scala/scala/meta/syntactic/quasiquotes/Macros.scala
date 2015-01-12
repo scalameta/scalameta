8b7983d5c70b3d825eb2c2c5eb86e9ab1d52e0c0
Eugene Burmako <xeno.by@gmail.com>
2015-01-12 12:32:18 +0100

    s/palladium/scala.meta/g

--- scalameta/src/main/scala/scala/meta/syntactic/quasiquotes/Macros.scala
+++ scalameta/src/main/scala/scala/meta/syntactic/quasiquotes/Macros.scala
@@ -7,17 +7,17 @@ import org.scalameta.adt._
 class Macros[C <: Context](val c: C) extends AdtReflection with NewLiftables {
   val u: c.universe.type = c.universe
   import c.universe.{Tree => _, _}
-  import c.universe.{Tree => ScalaTree}
-  import scala.meta.{Tree => PalladiumTree}
-  def apply(macroApplication: ScalaTree, parse: String => PalladiumTree): ScalaTree = {
+  import c.universe.{Tree => ScalaReflectTree}
+  import scala.meta.{Tree => ScalaMetaTree}
+  def apply(macroApplication: ScalaReflectTree, parse: String => ScalaMetaTree): ScalaReflectTree = {
     val TermQuote = "denied" // TODO: find a cleaner way out of this mess
     val q"$_($_.apply(..$partlits)).$_.apply[..$_](..$argtrees)" = macroApplication
     val parts = partlits.map{ case q"${part: String}" => part }
     def ndots(s: String): Int = if (s.endsWith(".")) ndots(s.stripSuffix(".")) + 1 else 0
     val args = argtrees.zipWithIndex.map{ case (tree, i) => (c.freshName("dummy"), ndots(parts(i)), tree) }
     val snippet = (parts.init.zip(args).flatMap{ case (part, (id, ndots, _)) => List(if (ndots != 1) part.stripSuffix("." * ndots) else part, s"$id") } :+ parts.last).mkString("")
-    val liveTree: PalladiumTree = parse(snippet)
-    val reifiedTree: ScalaTree = implicitly[Liftable[PalladiumTree]].apply(liveTree)
+    val liveTree: ScalaMetaTree = parse(snippet)
+    val reifiedTree: ScalaReflectTree = implicitly[Liftable[ScalaMetaTree]].apply(liveTree)
     reifiedTree
   }
 }
