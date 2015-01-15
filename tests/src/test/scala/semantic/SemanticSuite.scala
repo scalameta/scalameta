cf82669532891a701f0968eb4bd539922029d734
Eugene Burmako <xeno.by@gmail.com>
2015-01-15 22:08:40 +0100

    having to expose internal types creates much more problems that I initially thought
    now we have to make our APIs internal-proof, both in contravariant and in covariant positions...

--- tests/src/test/scala/semantic/SemanticSuite.scala
+++ tests/src/test/scala/semantic/SemanticSuite.scala
@@ -7,16 +7,20 @@ class SemanticSuite extends FunSuite {
     import scala.meta.semantic._
     implicit val c: Context = ???
     val term1a: api.Term = ???
-    term1a.tpe
+    (term1a.tpe: api.Type)
     val term2a: api.Term with api.Type = ???
-    term2a.tpe
+    (term2a.tpe: api.Type)
     val term3a: api.Term.Ref = ???
-    term3a.tpe
+    (term3a.tpe: api.Type)
     val term1b: impl.Term = ???
-    term1b.tpe
+    (term1b.tpe: api.Type)
     val term2b: impl.Term with impl.Type = ???
-    term2b.tpe
+    (term2b.tpe: api.Type)
     val term3b: impl.Term.Ref = ???
-    term3b.tpe
+    (term3b.tpe: api.Type)
+    (typeOf[Int]: api.Type.Ref)
+    (typeOf[Int]: impl.Type.Ref)
+    (termOf(Int): api.Term.Ref)
+    (termOf(Int): impl.Term.Ref)
   }
 }
