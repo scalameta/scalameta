0db9820db2549a6123e2121e13fb3ff6262f1ab1
Eugene Burmako <xeno.by@gmail.com>
2015-02-02 13:58:18 +0100

    semantic quasiquotes now require a semantic context

--- scalameta/src/main/scala/scala/meta/quasiquotes/Flavor.scala
+++ scalameta/src/main/scala/scala/meta/quasiquotes/Flavor.scala
@@ -2,6 +2,9 @@ package scala.meta
 
 import org.scalameta.adt._
 import scala.meta.quasiquotes.Flavor
+import scala.meta.internal.quasiquotes.EnableMacros
+import scala.language.experimental.{macros => prettyPlease}
+import scala.reflect.macros.blackbox.Context
 
 package quasiquotes {
   @root trait Flavor
@@ -15,6 +18,19 @@ package syntactic {
 
 package semantic {
   package object quasiquotes {
-    @leaf implicit object Enable extends Flavor
+    @leaf object EnableImpl extends Flavor
+    implicit def Enable: EnableImpl.type = macro EnableMacros.impl
+  }
+}
+
+package internal.quasiquotes {
+  class EnableMacros(val c: Context) {
+    def impl: c.Tree = {
+      import c.universe._
+      val TermQuote = "shadow scala.meta quasiquotes"
+      val context = c.inferImplicitValue(typeOf[scala.meta.semantic.Context], silent = true)
+      if (context.isEmpty) c.abort(c.enclosingPosition, "semantic quasiquotes require an implicit scala.meta.semantic.Context")
+      q"_root_.scala.meta.semantic.quasiquotes.`package`.EnableImpl"
+    }
   }
 }
--- scalameta/src/main/scala/scala/meta/quasiquotes/Macros.scala
+++ scalameta/src/main/scala/scala/meta/quasiquotes/Macros.scala
@@ -14,12 +14,12 @@ class Macros[C <: Context](val c: C) extends AdtReflection with Liftables {
   import c.universe.{Tree => _, _}
   import c.universe.{Tree => ScalaReflectTree}
   import scala.meta.{Tree => ScalaMetaTree}
-  val TermQuote = "denied" // TODO: find a cleaner way out of this mess
+  val TermQuote = "shadow scala.meta quasiquotes"
   case class Dummy(id: String, ndots: Int, arg: ScalaReflectTree)
 
   def apply(macroApplication: ScalaReflectTree, scalaMetaParse: String => ScalaMetaTree): ScalaReflectTree = {
     val SyntacticFlavor = symbolOf[scala.meta.syntactic.quasiquotes.Enable.type]
-    val SemanticFlavor = symbolOf[scala.meta.semantic.quasiquotes.Enable.type]
+    val SemanticFlavor = symbolOf[scala.meta.semantic.quasiquotes.EnableImpl.type]
     val flavor = c.inferImplicitValue(typeOf[scala.meta.quasiquotes.Flavor]).tpe.typeSymbol
     flavor match {
       case SyntacticFlavor =>
