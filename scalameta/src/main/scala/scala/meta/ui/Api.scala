e9269dbef9a8e5d04a4d751c8b82b9f1c68ed737
Eugene Burmako <xeno.by@gmail.com>
2015-02-14 20:09:27 +0100

    some cleanup of the public API
    I don't think that we need to expose companion objects for
    scala.meta.ui.{Code, Raw, Semantics}, because basic functionality
    of prettyprinting works without them just fine.

--- scalameta/src/main/scala/scala/meta/ui/Api.scala
+++ scalameta/src/main/scala/scala/meta/ui/Api.scala
@@ -9,13 +9,8 @@ trait Api {
   val Exception = scala.meta.ui.Exception
 
   type Code[T] = scala.meta.ui.Code[T]
-  val Code = scala.meta.ui.Code
-
   type Raw[T] = scala.meta.ui.Raw[T]
-  val Raw = scala.meta.ui.Raw
-
   type Semantics[T] = scala.meta.ui.Semantics[T]
-  val Semantics = scala.meta.ui.Semantics
 
   // NOTE: I wish there was a way to avoid duplication and ambiguities wrt org.scalameta.show
   implicit class ShowOps[T](val x: T) {
