f7da51fa0e620e2ab8474ae2cb94788de69b212f
Eugene Burmako <xeno.by@gmail.com>
2015-02-21 16:16:44 +0100

    @quasiquote annotation no longer generates macro implementations
    Now everything is routed through scala.meta.internal.quasiquote.Macros.
    This requires a bit of runtime reflection at compile time, but has
    the advantages of: 1) modularizing quasiquotes away from the package class,
    2) being friendlier to scaladoc (marking the enclosing trait as private[meta]
    forces scaladoc to hide implicit conversions to quasiquote interpolators
    from the left panel, which is super nice).

---
+++ scalameta/src/main/scala/scala/meta/Quasiquotes.scala
@@ -0,0 +1,27 @@
+package scala
+package meta
+
+import org.scalameta.annotations._
+import scala.meta._
+
+private[meta] trait Quasiquotes {
+  // TODO: it would be ideal to have these as annotations on corresponding AST nodes
+  // e.g. instead of `@branch trait Stat extends Tree`
+  // we will have `@quasiquote('q) @branch trait Stat extends Tree`
+  // that would probably allow us for every AST node to have an associated quasiquote interpolator in the doc
+  // upd. this might also require non-local macro expansion because of hierarchical structure of the `scala.meta` package
+  // (if we annotate scala.meta.package.Type.Arg, we need to somehow put the TypeQuote implicit class into scala.meta.package)
+  @quasiquote[Stat]('q)                implicit class TermQuote(ctx: StringContext)
+  @quasiquote[Term.Arg]('arg)          implicit class TermArgQuote(ctx: StringContext)
+  @quasiquote[Term.Param]('param)      implicit class TermParamQuote(ctx: StringContext)
+  @quasiquote[Type.Arg]('t)            implicit class TypeQuote(ctx: StringContext)
+  @quasiquote[Type.Param]('tparam)     implicit class TypeParamQuote(ctx: StringContext)
+  @quasiquote[Case, Pat.Arg]('p)       implicit class CaseOrPatternQuote(ctx: StringContext)
+  @quasiquote[Pat.Type]('pt)           implicit class PatternTypeQuote(ctx: StringContext)
+  @quasiquote[Ctor.Ref, Term]('ctor)   implicit class CtorQuote(ctx: StringContext)
+  @quasiquote[Template]('template)     implicit class TemplateQuote(ctx: StringContext)
+  @quasiquote[Mod]('mod)               implicit class ModQuote(ctx: StringContext)
+  @quasiquote[Enumerator]('enumerator) implicit class EnumeratorQuote(ctx: StringContext)
+  @quasiquote[Importee]('importee)     implicit class ImporteeQuote(ctx: StringContext)
+  @quasiquote[Source]('source)         implicit class SourceQuote(ctx: StringContext)
+}
