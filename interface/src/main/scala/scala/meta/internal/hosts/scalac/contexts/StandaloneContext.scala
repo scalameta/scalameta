70e54f29e371ff77db7946823c0d6ca6214c6047
Eugene Burmako <xeno.by@gmail.com>
2015-02-22 22:26:49 +0100

    hotfixes for the new test harness

--- interface/src/main/scala/scala/meta/internal/hosts/scalac/contexts/StandaloneContext.scala
+++ interface/src/main/scala/scala/meta/internal/hosts/scalac/contexts/StandaloneContext.scala
@@ -14,21 +14,24 @@ import scala.meta.internal.{ast => m}
 class StandaloneContext(options: String) extends ScalahostSemanticContext(mkGlobal(options)) {
   private val reporter: StoreReporter = g.reporter.require[StoreReporter]
   def define(code: String): m.Tree = {
-    val gtree = g.newUnitParser(code, "<scalahost>").parse()
+    g.reporter.reset()
+    val gunit = g.newCompilationUnit(code, "<scalahost>")
+    val gtree = g.newUnitParser(gunit).parse()
+    if (reporter.hasErrors) sys.error("parse has failed:" + EOL + (reporter.infos map (_.msg) mkString EOL))
     val gtypedtree = {
       import g.{reporter => _, _}
       import analyzer._
       val run = new Run
-      reporter.reset()
       phase = run.namerPhase
       globalPhase = run.namerPhase
-      val namer = newNamer(rootContext(NoCompilationUnit))
+      val namer = newNamer(rootContext(gunit))
       namer.enterSym(gtree)
       phase = run.typerPhase
       globalPhase = run.typerPhase
-      val typer = newTyper(rootContext(NoCompilationUnit))
+      val typer = newTyper(rootContext(gunit))
       val typedpkg = typer.typed(gtree).require[Tree]
-      if (reporter.hasErrors) sys.error("typecheck has failed:" + EOL + EOL + (reporter.infos map (_.msg) mkString EOL))
+      for (workItem <- gunit.toCheck) workItem()
+      if (reporter.hasErrors) sys.error("typecheck has failed:" + EOL + (reporter.infos map (_.msg) mkString EOL))
       typedpkg.require[PackageDef]
     }
     val _ = toMtree.computeConverters // TODO: necessary because of macro expansion order
