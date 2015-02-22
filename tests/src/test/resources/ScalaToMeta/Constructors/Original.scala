454b04c4c81ba9f1917b03f7afdd0ec7c9bcc0ff
Eugene Burmako <xeno.by@gmail.com>
2015-02-22 11:37:47 +0100

    test for a crash in Adjunction.scala in scalaz
    Here we lose the fact that the ctor's type is written in infix notation,
    so we'll have to make up for this with trivia.

--- tests/src/test/resources/ScalaToMeta/Constructors/Original.scala
+++ tests/src/test/resources/ScalaToMeta/Constructors/Original.scala
@@ -7,4 +7,6 @@ object Constructors {
   class C4(x: Int) { def this() = this(0) }
   class C5(x: Int) { def this() { this(0); println("hello world") } }
   class C6[T: scala.reflect.ClassTag](x: Int) { def this() = this(0) }
+  class C7[T, U]
+  new (Int C7 Int)
 }
