f6eeef2779e94713fb40e3f7e05412b945a0b595
Eugene Burmako <xeno.by@gmail.com>
2015-02-22 22:19:46 +0100

    finally can test enum arguments to classfile annotations

--- tests/src/test/resources/ScalaToMeta/Annotations/Original.scala
+++ tests/src/test/resources/ScalaToMeta/Annotations/Original.scala
@@ -6,7 +6,6 @@ object Annotations {
   @SerialVersionUID(value = 1L) class C1
   @SerialVersionUID(value = 0 + 1L) class C2
   @SerialVersionUID(value = -1L) class C3
-  // TODO: toolbox-based test harness doesn't support enum arguments for classfile annotations
-  // @Retention(RetentionPolicy.RUNTIME) class annot1
-  // @Retention(RUNTIME) class annot2
+  @Retention(RetentionPolicy.RUNTIME) class annot1
+  @Retention(RUNTIME) class annot2
 }
