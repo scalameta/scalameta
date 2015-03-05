8f2d73ae1014a64969de6f58b2bd7b1daa712296
Eugene Burmako <xeno.by@gmail.com>
2015-03-05 22:48:34 +0100

    another Unquote/Ellipsis quickfix

--- scalameta/src/main/scala/scala/meta/internal/parsers/Helpers.scala
+++ scalameta/src/main/scala/scala/meta/internal/parsers/Helpers.scala
@@ -48,6 +48,7 @@ private[meta] object Helpers {
     }
     def ctorTpe: Type = {
       def loop(tree: Tree): Type = tree match {
+        case Unquote(tree, _) => Unquote(tree, classOf[Type])
         case Ctor.Name(value) => Type.Name(value)
         case Ctor.Ref.Select(qual, name) => Type.Select(qual, Type.Name(name.value))
         case Ctor.Ref.Project(qual, name) => Type.Project(qual, Type.Name(name.value))
