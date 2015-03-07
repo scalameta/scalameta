622c4febbebea5969ad169248e953aa29493fba6
Eugene Burmako <xeno.by@gmail.com>
2015-03-07 14:40:33 +0100

    provides (trivial) implementations of the macro API

--- scalameta/src/main/scala/scala/meta/macros/Api.scala
+++ scalameta/src/main/scala/scala/meta/macros/Api.scala
@@ -3,11 +3,12 @@ package macros
 
 import org.scalameta.adt._
 import org.scalameta.annotations._
+import scala.meta.macros.{Context => MacroContext}
 
 private[meta] trait Api {
-  @hosted(macroApi = true) def warning(msg: String): Unit = ???
-  @hosted(macroApi = true) def error(msg: String): Unit = ???
-  @hosted(macroApi = true) def abort(msg: String): Nothing = ???
-  @hosted(macroApi = true) def resources: Seq[String] = ???
-  @hosted(macroApi = true) def resource(url: String): Array[Byte] = ???
+  @hosted(macroApi = true) def warning(msg: String): Unit = implicitly[MacroContext].warning(msg)
+  @hosted(macroApi = true) def error(msg: String): Unit = implicitly[MacroContext].error(msg)
+  @hosted(macroApi = true) def abort(msg: String): Nothing = implicitly[MacroContext].abort(msg)
+  @hosted(macroApi = true) def resources: Seq[String] = implicitly[MacroContext].resources.keys.toList.sortBy(Predef.identity _)
+  @hosted(macroApi = true) def resource(url: String): Array[Byte] = implicitly[MacroContext].resources(url)
 }
