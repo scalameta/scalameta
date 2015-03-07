5dc46b10d0e84c54b8926188860f3fa91d7c4a5c
Eugene Burmako <xeno.by@gmail.com>
2015-03-07 11:59:38 +0100

    adds SemanticException.cause

--- scalameta/src/main/scala/scala/meta/Exceptions.scala
+++ scalameta/src/main/scala/scala/meta/Exceptions.scala
@@ -4,6 +4,6 @@ import org.scalameta.adt._
 import org.scalameta.unreachable
 
 @root trait MetaException extends Exception
-@leaf class TokenizeException(input: Input, offset: Int, msg: String) extends Exception(msg + " at " + offset) with MetaException
-@leaf class ParseException(input: Input, token: Token, msg: String) extends Exception(msg + " at " + token) with MetaException
-@leaf class SemanticException(msg: String) extends Exception(msg) with MetaException
+@leaf class TokenizeException(input: Input, offset: Int, message: String) extends Exception(message + " at " + offset) with MetaException
+@leaf class ParseException(input: Input, token: Token, message: String) extends Exception(message + " at " + token) with MetaException
+@leaf class SemanticException(message: String, cause: Option[Throwable] = None) extends Exception(message, cause.orNull) with MetaException
