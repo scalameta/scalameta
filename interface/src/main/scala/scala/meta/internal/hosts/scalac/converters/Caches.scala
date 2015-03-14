35ef3d8115d85c5a1e3819985e2fc6a3424627af
Eugene Burmako <xeno.by@gmail.com>
2015-03-14 18:13:01 +0100

    fixes cacheAllMembers so that it actually works

--- interface/src/main/scala/scala/meta/internal/hosts/scalac/converters/Caches.scala
+++ interface/src/main/scala/scala/meta/internal/hosts/scalac/converters/Caches.scala
@@ -14,12 +14,33 @@ import scala.meta.internal.{hygiene => h}
 
 // This module explicitly lists caches that are used by the conversions.
 // Some of them are here to improve performance, but some of them are necessary for uniqueness and/or correctness.
-// TODO: make `hsymToNativeMmemberCache` private
 trait Caches extends GlobalToolkit with MetaToolkit {
   self: Api =>
 
   protected lazy val symbolTable = new SymbolTable()
   protected lazy val lsymToMmemberCache = mutable.Map[(g.Type, l.Symbol), m.Member]()
   protected lazy val tpeCache = TwoWayCache[g.Type, m.Type.Arg]()
-  lazy val hsymToNativeMmemberCache = mutable.Map[h.Symbol, m.Member]()
+  protected lazy val hsymToNativeMmemberCache = mutable.Map[h.Symbol, m.Member]()
+
+  def cacheAllMembers[T <: m.Tree](x: T): T = {
+    def cache(x: m.Member): Unit = {
+      val denot = x.name.require[m.Name].denot
+      if (x.name.isBinder && !x.isInstanceOf[m.Ctor.Primary] && !x.name.isInstanceOf[m.Name.Anonymous]) {
+        require(denot != h.Denotation.Zero && debug(x, x.show[Raw]))
+        require(denot.symbol != h.Symbol.Zero && debug(x, x.show[Raw]))
+        // TODO: it seems that we can't have this yet
+        // require(!hsymToNativeMmemberCache.contains(denot.symbol) && debug(x, x.show[Raw]))
+        hsymToNativeMmemberCache(denot.symbol) = x
+      }
+    }
+    def loop(x: Any): Unit = x match {
+      case x: m.Tree => cacheAllMembers(x)
+      case x: List[_] => x.foreach(loop)
+      case x: Some[_] => loop(x.get)
+      case x => // do nothing
+    }
+    x match { case x: m.Member => cache(x); case _ => }
+    x.productIterator.foreach(loop)
+    x
+  }
 }
