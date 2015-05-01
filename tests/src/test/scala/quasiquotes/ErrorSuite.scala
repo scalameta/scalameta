d5603e000fbcd94f05146a16b9335928d85f5e3e
Eugene Burmako <xeno.by@gmail.com>
2015-05-01 14:11:53 +0200

    restoring ErrorSuite.scala

--- tests/src/test/scala/quasiquotes/ErrorSuite.scala
+++ tests/src/test/scala/quasiquotes/ErrorSuite.scala
@@ -1,115 +1,115 @@
-// import org.scalatest._
-// import org.scalameta.tests._
+import org.scalatest._
+import org.scalameta.tests._
 
-// class ErrorSuite extends FunSuite {
-//   implicit val errorsWithPositionsPlease = Style.WithPositions
+class ErrorSuite extends FunSuite {
+  implicit val errorsWithPositionsPlease = Style.WithPositions
 
-//   test("q\"foo + class\"") {
-//     assert(typecheckError("""
-//       import scala.meta._
-//       import scala.meta.dialects.Scala211
-//       q"foo + class"
-//     """) === """
-//       |<macro>:4: ; expected but class found.
-//       |      q"foo + class"
-//       |              ^
-//     """.trim.stripMargin)
-//   }
+  test("q\"foo + class\"") {
+    assert(typecheckError("""
+      import scala.meta._
+      import scala.meta.dialects.Scala211
+      q"foo + class"
+    """) === """
+      |<macro>:4: ; expected but class found.
+      |      q"foo + class"
+      |              ^
+    """.trim.stripMargin)
+  }
 
-//   test("q\"foo($x)\" when x has incompatible type") {
-//     assert(typecheckError("""
-//       import scala.meta._
-//       import scala.meta.dialects.Scala211
-//       class Dummy
-//       val x = new Dummy
-//       q"foo($x)"
-//     """) === """
-//       |<macro>:6: type mismatch when unquoting;
-//       | found   : Dummy
-//       | required: scala.meta.Term.Arg
-//       |      q"foo($x)"
-//       |            ^
-//     """.trim.stripMargin)
-//   }
+  test("q\"foo($x)\" when x has incompatible type") {
+    assert(typecheckError("""
+      import scala.meta._
+      import scala.meta.dialects.Scala211
+      class Dummy
+      val x = new Dummy
+      q"foo($x)"
+    """) === """
+      |<macro>:6: type mismatch when unquoting;
+      | found   : Dummy
+      | required: scala.meta.Term.Arg
+      |      q"foo($x)"
+      |            ^
+    """.trim.stripMargin)
+  }
 
-//   test("q\"foo(..$xs)\" when xs has incompatible type") {
-//     assert(typecheckError("""
-//       import scala.meta._
-//       import scala.meta.dialects.Scala211
-//       class Dummy
-//       val xs = List(new Dummy)
-//       q"foo(..$xs)"
-//     """) === """
-//       |<macro>:6: type mismatch when unquoting;
-//       | found   : List[Dummy]
-//       | required: scala.collection.immutable.Seq[scala.meta.Term.Arg]
-//       |      q"foo(..$xs)"
-//       |              ^
-//     """.trim.stripMargin)
-//   }
+  test("q\"foo(..$xs)\" when xs has incompatible type") {
+    assert(typecheckError("""
+      import scala.meta._
+      import scala.meta.dialects.Scala211
+      class Dummy
+      val xs = List(new Dummy)
+      q"foo(..$xs)"
+    """) === """
+      |<macro>:6: type mismatch when unquoting;
+      | found   : List[Dummy]
+      | required: scala.collection.immutable.Seq[scala.meta.Term.Arg]
+      |      q"foo(..$xs)"
+      |              ^
+    """.trim.stripMargin)
+  }
 
-//   test("q\"...$xs\"") {
-//     assert(typecheckError("""
-//       import scala.meta._
-//       import scala.meta.dialects.Scala211
-//       val xss = List(List(q"x"))
-//       q"...$xss"
-//     """) === """
-//       |<macro>:5: rank mismatch when unquoting;
-//       | found   : ...
-//       | required: ..
-//       |      q"...$xss"
-//       |        ^
-//     """.trim.stripMargin)
-//   }
+  test("q\"...$xs\"") {
+    assert(typecheckError("""
+      import scala.meta._
+      import scala.meta.dialects.Scala211
+      val xss = List(List(q"x"))
+      q"...$xss"
+    """) === """
+      |<macro>:5: rank mismatch when unquoting;
+      | found   : ...
+      | required: ..
+      |      q"...$xss"
+      |        ^
+    """.trim.stripMargin)
+  }
 
-//   test("q\"foo[..$terms]\"") {
-//     assert(typecheckError("""
-//       import scala.meta._
-//       import scala.meta.dialects.Scala211
-//       val terms = List(q"T", q"U")
-//       q"foo[..$terms]"
-//     """) === """
-//       |<macro>:5: type mismatch when unquoting;
-//       | found   : List[scala.meta.Term.Name]
-//       | required: scala.collection.immutable.Seq[scala.meta.Type]
-//       |      q"foo[..$terms]"
-//       |              ^
-//     """.trim.stripMargin)
-//   }
+  test("q\"foo[..$terms]\"") {
+    assert(typecheckError("""
+      import scala.meta._
+      import scala.meta.dialects.Scala211
+      val terms = List(q"T", q"U")
+      q"foo[..$terms]"
+    """) === """
+      |<macro>:5: type mismatch when unquoting;
+      | found   : List[scala.meta.Term.Name]
+      | required: scala.collection.immutable.Seq[scala.meta.Type]
+      |      q"foo[..$terms]"
+      |              ^
+    """.trim.stripMargin)
+  }
 
-//   test("q\"foo($x, ..$ys, $z, ..$ts)\"") {
-//     assert(typecheckError("""
-//       import scala.meta._
-//       import scala.meta.dialects.Scala211
-//       val tree = q"foo(1, 2, 3)"
-//       tree match {
-//         case q"$_($x, ..$ys, $z, ..$ts)" =>
-//           println(x)
-//           println(ys)
-//           println(z)
-//       }
-//     """) === """
-//       |<macro>:6: rank mismatch when unquoting;
-//       | found   : ..
-//       | required: no dots
-//       |Note that you can extract a sequence into an unquote when pattern matching,
-//       |it just cannot follow another sequence either directly or indirectly.
-//       |        case q"$_($x, ..$ys, $z, ..$ts)" =>
-//       |                                 ^
-//     """.trim.stripMargin)
-//   }
+  test("q\"foo($x, ..$ys, $z, ..$ts)\"") {
+    assert(typecheckError("""
+      import scala.meta._
+      import scala.meta.dialects.Scala211
+      val tree = q"foo(1, 2, 3)"
+      tree match {
+        case q"$_($x, ..$ys, $z, ..$ts)" =>
+          println(x)
+          println(ys)
+          println(z)
+      }
+    """) === """
+      |<macro>:6: rank mismatch when unquoting;
+      | found   : ..
+      | required: no dots
+      |Note that you can extract a sequence into an unquote when pattern matching,
+      |it just cannot follow another sequence either directly or indirectly.
+      |        case q"$_($x, ..$ys, $z, ..$ts)" =>
+      |                                 ^
+    """.trim.stripMargin)
+  }
 
-//   test("q\"\"\" \"$x\" \"\"\"\"") {
-//     assert(typecheckError("""
-//       import scala.meta._
-//       import scala.meta.dialects.Scala211
-//       val x = "hello"
-//       qQQQ "$x" QQQ
-//     """) === """
-//       |<macro>:5: can't unquote into string literals
-//       |      qQQQ "$x" QQQ
-//       |            ^
-//     """.replace("QQQ", "\"\"\"").trim.stripMargin)
-//   }
-// }
+  test("q\"\"\" \"$x\" \"\"\"\"") {
+    assert(typecheckError("""
+      import scala.meta._
+      import scala.meta.dialects.Scala211
+      val x = "hello"
+      qQQQ "$x" QQQ
+    """) === """
+      |<macro>:5: can't unquote into string literals
+      |      qQQQ "$x" QQQ
+      |            ^
+    """.replace("QQQ", "\"\"\"").trim.stripMargin)
+  }
+}
