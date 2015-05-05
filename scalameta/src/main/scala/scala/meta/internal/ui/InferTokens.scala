3aea8c0a370d0bae76a6b2947ed41c597324dc14
Mathieu Demarne <mathieu.demarne@gmail.com>
2015-05-05 15:00:32 +0200

    Temporary hack for InterTokens
    In order to be able to test it locally.

--- scalameta/src/main/scala/scala/meta/internal/ui/InferTokens.scala
+++ scalameta/src/main/scala/scala/meta/internal/ui/InferTokens.scala
@@ -2,8 +2,14 @@ package scala.meta
 package internal
 package ui
 
+import scala.meta.dialects.Scala211
+
 private[meta] object inferTokens {
-  def apply(tree: Tree): Tokens = {
-    ???
+  def apply(tree: Tree): Tokens = { // TODO, dummy for now
+  	val code = tree.show[Code]
+  	(tree match {
+  		case _: Source => code.parse[Source]
+  		case _: Stat => code.parse[Stat]
+  	}).origin.tokens
   }
 }
