300195cd081624bdd16dab6f48fdfe4152eecd85
Eugene Burmako <xeno.by@gmail.com>
2015-05-15 17:33:08 +0200

    oops I shouldn've removed SemanticException.cause
    Looks like it's being used in scalahost macros.

--- scalameta/src/main/scala/scala/meta/Exceptions.scala
+++ scalameta/src/main/scala/scala/meta/Exceptions.scala
@@ -11,14 +11,18 @@ extends Exception(s"$message at ${pos.start.offset}..${pos.end.offset}") with Me
 @leaf class ParseException(pos: Position, message: String)
 extends Exception(s"$message at ${pos.start.offset}..${pos.end.offset}") with MetaException
 
-@leaf class SemanticException(pos: Option[Position], message: String)
-extends Exception(message) with MetaException {
-  def this(message: String) = this(None, message)
-  def this(pos: Position, message: String) = this(Some(pos), message)
+@leaf class SemanticException(pos: Option[Position], message: String, cause: Option[Throwable])
+extends Exception(message, cause.orNull) with MetaException {
+  def this(message: String) = this(None, message, None)
+  def this(message: String, cause: Throwable) = this(None, message, Some(cause))
+  def this(pos: Position, message: String) = this(Some(pos), message, None)
+  def this(pos: Position, message: String, cause: Throwable) = this(Some(pos), message, Some(cause))
 }
 
-@leaf class AbortException(pos: Option[Position], message: String)
-extends Exception(message) with MetaException {
-  def this(message: String) = this(None, message)
-  def this(pos: Position, message: String) = this(Some(pos), message)
+@leaf class AbortException(pos: Option[Position], message: String, cause: Option[Throwable])
+extends Exception(message, cause.orNull) with MetaException {
+  def this(message: String) = this(None, message, None)
+  def this(message: String, cause: Throwable) = this(None, message, Some(cause))
+  def this(pos: Position, message: String) = this(Some(pos), message, None)
+  def this(pos: Position, message: String, cause: Throwable) = this(Some(pos), message, Some(cause))
 }
