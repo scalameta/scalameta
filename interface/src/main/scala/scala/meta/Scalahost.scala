d42d7ef01a0d24035fa4ee87033efa38e8ccdf03
Eugene Burmako <xeno.by@gmail.com>
2015-05-15 17:36:07 +0200

    updates to recent changes in scalameta/scalameta

--- interface/src/main/scala/scala/meta/Scalahost.scala
+++ interface/src/main/scala/scala/meta/Scalahost.scala
@@ -4,15 +4,15 @@ import scala.tools.nsc.{Global => ScalaGlobal}
 import scala.meta.semantic.{Context => ScalametaSemanticContext}
 import scala.meta.internal.hosts.scalac.contexts.{SemanticContext => ScalahostSemanticContext}
 import scala.reflect.macros.contexts.{Context => ScalareflectMacroContext}
-import scala.meta.macros.{Context => ScalametaMacroContext}
-import scala.meta.internal.hosts.scalac.contexts.{MacroContext => ScalahostMacroContext}
+import scala.meta.semantic.{Context => ScalametaMacroContext}
+import scala.meta.internal.hosts.scalac.contexts.{SemanticContext => ScalahostMacroContext}
 import scala.meta.internal.hosts.scalac.contexts.{StandaloneContext => ScalametaStandaloneContext}
 
 object Scalahost {
   def mkGlobalContext[G <: ScalaGlobal](g: G): ScalametaSemanticContext with ScalahostSemanticContext[G] =
     new ScalahostSemanticContext[G](g)
   def mkMacroContext[G <: ScalaGlobal](rc: ScalareflectMacroContext): ScalametaMacroContext with ScalahostMacroContext[G] =
-    new ScalahostMacroContext[G](rc)
+    new ScalahostMacroContext[G](rc.universe.asInstanceOf[G])
   def mkStandaloneContext(options: String = ""): ScalametaStandaloneContext =
     new ScalametaStandaloneContext(options)
 }
