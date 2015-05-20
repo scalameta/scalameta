4cbc356f4bdb3e92e7929d9a8ec4f90d3c0f3907
Eugene Burmako <xeno.by@gmail.com>
2015-05-20 13:36:37 +0200

    moves some of the foundational stuff to scalameta/scalameta
    This is done to share this code with dottyhost.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   lazy val paradise ="org.scalamacros" % "paradise" % "2.1.0-M5" cross CrossVersion.full
 
   lazy val meta = "org.scalameta" %% "scalameta" % metaVersion
-  lazy val metafoundation = "org.scalameta" %% "scalameta-foundation" % metaVersion
+  lazy val metafoundation = "org.scalameta" %% "foundation" % metaVersion
   lazy val interpreter = "org.scalameta" %% "interpreter" % metaVersion
 
   lazy val scalatest = "org.scalatest" %% "scalatest" % "2.1.3" % "test"
