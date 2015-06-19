2174d49d182b3372f1918005f333c9b54abc6615
dveim <alexandrolgash@gmail.com>
2015-06-20 01:27:57 +0300

    added remaining tests, however, some of them are commented

--- tests/src/test/scala/quasiquotes/QuasiquoteSuite.scala
+++ tests/src/test/scala/quasiquotes/QuasiquoteSuite.scala
@@ -109,6 +109,140 @@ class QuasiquoteSuite extends FunSuite {
 //    val name = q"method"
 //    val tpes = List(t"T", t"U")
 //    val aexprs = List(q"1", q"b")
-//    assert(q"$expr $name[..$tpes] (..$aexprs)".show[Code] === "x method[T, U](1, b)") fixme test is broken, so even does not compile
+//    assert(q"$expr $name[..$tpes] (..$aexprs)".show[Code] === "x method[T, U](1, b)") // fixme test is broken, so even does not compile
 //  }
+
+  test("q\"!$expr\"") {
+    val q"!$x" = q"!foo"
+    assert(x.show[Code] === "foo")
+  }
+
+  test("q\"~$expr\"") {
+    val expr = q"foo"
+    assert(q"~$expr".show[Code] === "~foo")
+  }
+
+  test("q\"-$expr\"") {
+    val q"-$x" = q"-foo"
+    assert(x.show[Code] === "foo")
+  }
+
+  test("q\"+$expr\"") {
+    val q"+$x" = q"+foo"
+    assert(x.show[Code] === "foo")
+  }
+
+//  test("q\"$ref = $expr\"") {
+//    val q"$ref = $expr" = q"a = b" // fixme test is broken, so even does not compile
+//    assert(ref.show[Code] === "a")
+//    assert(expr.show[Code] === "b")
+//  }
+
+  test("q\"$expr(..$aexprs) = $expr\"") {
+    val q"$expr1(..$aexprs) = $expr2" = q"foo(a, b) = bar"
+    assert(expr1.show[Code] === "foo")
+    assert(aexprs(0).show[Code] === "a")
+    assert(aexprs(1).show[Code] === "b")
+    assert(expr2.show[Code] === "bar")
+  }
+
+  test("q\"return $expropt\"") {
+    val q"return $expropt" = q"return foo == bar"
+    assert(expropt.show[Code] === "foo == bar")
+  }
+
+  test("q\"throw $expr\"") {
+    val q"throw $expr" = q"throw new RuntimeException"
+    assert(expr.show[Code] === "new RuntimeException")
+  }
+
+  test("q\"$expr: $tpe\"") {
+    val q"$exp: $tpe" = q"1: Double"
+    assert(exp.show[Code] === "1")
+    assert(tpe.show[Code] === "Double")
+  }
+
+//  test("q\"$expr: ..@$expr\"") {
+//    val q"$a: ..@$b" = q"foo: @bar @baz"
+//  }
+
+//  test("q\"(..$exprs)\"") {
+//    val terms = List(q"y", q"z")
+//    assert(q"(..$terms)".show[Code] === "(y, z)") // fixme test is broken, so even does not compile
+//  }
+
+//  test("q\"{ ..$stats }\"") {
+//    val stats = List(q"val x = 1", q"val y = 2")
+//    q"{ ..$stats }"
+//  }
+
+  test("q\"if ($expr) $expr else $expr\"") {
+    val q"if ($expr1) $expr2 else $expr3" = q"if (1 > 2) a else b"
+    assert(expr1.show[Code] === "1 > 2")
+    assert(expr2.show[Code] === "a")
+    assert(expr3.show[Code] === "b")
+  }
+
+//  test("q\"$expr match { ..case $cases }\"") {
+//    val q"$expr match { ..case $cases }" =
+//      q"foo match { case bar => baz; case _ => foo }" // fixme test is broken, so even does not compile
+//  }
+
+//  test("q\"try $expr catch { ..case $cases } finally $expropt\"") {
+//    val q"try $expr catch { ..case $cases } finally $expropt" = // fixme test is broken, so even does not compile
+//      q"try foo catch { case _ => bar} finally baz"
+//  }
+
+//  test("q\"try $expr catch $expr finally $expropt\"") {
+//    val q"try $expr catch $expr finally $expropt" =
+//      q"try foo catch { case _ => } finally bar" // fixme test is broken, so even does not compile
+//  }
+
+//  test("q\"(..$params) => $expr\"") {
+//    val q"(..$params) => $expr" = q"(x: Int, y: String) => 42" // fixme test is broken, so even does not compile
+//  }
+
+  test("q\"{ ..case $cases }\"") {
+    val q"{ case ..$cases }" = q"{ case i: Int => i + 1 }"
+    assert(cases(0).show[Code] === "case i: Int => i + 1")
+  }
+
+  test("q\"while ($expr) $expr\"") {
+    val q"while ($expr1) $expr2" = q"while (foo) bar"
+    assert(expr1.show[Code] === "foo")
+    assert(expr2.show[Code] === "bar")
+  }
+
+  test("q\"do $expr while($expr)\"") {
+    val q"do $expr1 while($expr2)" = q"do foo while (bar)"
+    assert(expr1.show[Code] === "foo")
+    assert(expr2.show[Code] === "bar")
+  }
+
+//  test("q\"for (..$enumerators) $expr\"") {
+//    val q"for (..$enumerators) $expr" = q"for (x <- xs; y <- ys) foo(x, y)" // fixme test is broken, so even does not compile
+//  }
+
+//  test("q\"for (..$enumerators) yield $expr\"") {
+//    val q"for (..$enumerators) yield $expr" =  q"for (x <- xs; y <- ys) yield foo(x, y)" // fixme test is broken, so even does not compile
+//  }
+
+//  test("q\"new $template\"") {
+//    val q"new $template" = q"new Foo" // fixme test is broken, so even does not compile
+//  }
+
+  test("q\"$expr _\"") {
+    val q"$expr _" = q"foo _"
+    assert(expr.show[Code] === "foo")
+  }
+
+  test("q\"$lit\"") {
+    val lit = q"42"
+    assert(q"$lit".show[Code] === "42")
+  }
+
+  test("pattern matching q\"$lit\"") {
+    val q"$lit" = q"42"
+    assert(lit.show[Code] === "42") // fixme test produces warnings
+  }
 }
