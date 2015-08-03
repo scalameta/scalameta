cb6c052f9c3ddb3a9392e820289fead2ce2a2594
Eugene Burmako <xeno.by@gmail.com>
2015-08-03 12:02:00 +0200

    introduces Quasi.become
    See the diff in Parsers.scala to understand why this method is necessary.

--- foundation/src/main/scala/org/scalameta/ast/metadata.scala
+++ foundation/src/main/scala/org/scalameta/ast/metadata.scala
@@ -19,8 +19,16 @@ class AstMetadataMacros(val c: Context) {
   import c.universe._
   import c.internal._
   lazy val TreeClass = rootMirror.staticClass("scala.meta.Tree")
+  lazy val QuasiClass = rootMirror.staticClass("scala.meta.internal.ast.Quasi")
   def materialize[T](implicit T: c.WeakTypeTag[T]): c.Tree = {
-    if (T.tpe <:< TreeClass.toType) {
+    if (T.tpe <:< QuasiClass.toType) {
+      q"""
+        new _root_.org.scalameta.ast.AstMetadata[$T] {
+          def runtimeClass: _root_.java.lang.Class[$T] = implicitly[_root_.scala.reflect.ClassTag[$T]].runtimeClass.asInstanceOf[_root_.java.lang.Class[$T]]
+          def quasi(rank: Int, tree: Any): $T = ${T.tpe.typeSymbol.companion}.apply(rank, tree)
+        }
+      """
+    } else if (T.tpe <:< TreeClass.toType) {
       q"""
         new _root_.org.scalameta.ast.AstMetadata[$T] {
           def runtimeClass: _root_.java.lang.Class[$T] = implicitly[_root_.scala.reflect.ClassTag[$T]].runtimeClass.asInstanceOf[_root_.java.lang.Class[$T]]
---
+++ tests/src/test/scala/ast/InfrastructureSuite.scala
@@ -0,0 +1,21 @@
+package scala.meta.tests
+
+import org.scalatest._
+import scala.meta.XtensionQuasiquoteTokens
+import scala.meta.Structure
+import scala.meta.internal.ast._
+import scala.meta.dialects.Scala211
+
+class InfrastructureSuite extends FunSuite {
+  test("become for Quasi-0") {
+    val q = Term.Quasi(0, "hello").withTokens(toks"Hello")
+    assert(q.become[Type.Quasi].show[Structure] === """Type.Quasi(0, "hello")""")
+    assert(q.become[Type.Quasi].tokens.toString === "Synthetic(Vector(Hello (0..5)))")
+  }
+
+  test("become for Quasi-1") {
+    val q = Term.Quasi(1, Term.Quasi(0, "hello").withTokens(toks"HelloInner")).withTokens(toks"HelloOuter")
+    assert(q.become[Type.Quasi].show[Structure] === """Type.Quasi(1, Type.Quasi(0, "hello"))""")
+    assert(q.become[Type.Quasi].tokens.toString === "Synthetic(Vector(HelloOuter (0..10)))")
+  }
+}
