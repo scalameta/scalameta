7555e19cf4279a16e2c41d1a2c27a95c3cbe6e3c
Eugene Burmako <xeno.by@gmail.com>
2015-08-10 08:36:54 +0200

    (wip) brings contexts in line with the updated scalameta/scalameta

--- scalahost/src/main/scala/scala/meta/Exceptions.scala
+++ scalahost/src/main/scala/scala/meta/Exceptions.scala
@@ -3,14 +3,9 @@ package scala.meta
 import org.scalameta.adt._
 import org.scalameta.unreachable
 
-@root trait ScalahostException extends Exception
+@root trait ScalahostException extends ScalametaException
 
 @leaf class ConvertException(culprit: Any, message: String, cause: Option[Throwable] = None)
 extends Exception(message, cause.orNull) with ScalahostException {
-  override def toString = super.toString
-}
-
-@leaf class StandaloneException(message: String)
-extends Exception(message) with ScalahostException {
   override def toString = super.toString
 }
--- scalahost/src/main/scala/scala/meta/Scalahost.scala
+++ scalahost/src/main/scala/scala/meta/Scalahost.scala
@@ -6,7 +6,6 @@ import scala.{meta => m}
 import scala.meta.semantic.{Context => ScalametaSemanticContext}
 import scala.meta.projects.{Context => ScalametaProjectContext}
 import scala.meta.internal.hosts.scalac.contexts.{GlobalContext => ScalahostGlobalContextImpl}
-import scala.meta.internal.hosts.scalac.contexts.{StandaloneContext => ScalahostStandaloneContextImpl}
 import scala.meta.internal.hosts.scalac.contexts.{ProjectContext => ScalahostProjectContextImpl}
 
 trait ScalahostGlobalContext[G <: ScalaGlobal] extends ScalametaSemanticContext { self =>
@@ -19,22 +18,12 @@ trait ScalahostGlobalContext[G <: ScalaGlobal] extends ScalametaSemanticContext 
   }
 }
 
-trait ScalahostStandaloneContext extends ScalametaSemanticContext {
-  def define(code: String): Source
-}
-
 trait ScalahostProjectContext extends ScalametaProjectContext with ScalametaSemanticContext {
 }
 
 object Scalahost {
-  // TODO: we probably need mkGlobalContext to become private[meta]
-  // there should preferably be only one way to do it
-  // where "it" means creating a scala.meta-enabled context for/from scalac artifacts
   def mkGlobalContext[G <: ScalaGlobal](g: G): ScalahostGlobalContext[G] =
     new ScalahostGlobalContextImpl[G](g)
-  // TODO: replace all usages of mkStandaloneContext with mkProjectContext and then delete it
-  def mkStandaloneContext(options: String = ""): ScalahostStandaloneContext =
-    new ScalahostStandaloneContextImpl(options)
   def mkProjectContext(sourcepath: String, classpath: String): ScalahostProjectContext =
     new ScalahostProjectContextImpl(sourcepath, classpath)
 }
