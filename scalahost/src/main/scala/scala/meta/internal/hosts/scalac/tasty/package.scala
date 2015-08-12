579f516d10893b5876fe7774b5938a0a59093389
Eugene Burmako <xeno.by@gmail.com>
2015-08-12 18:48:34 +0200

    also serializes hash of the source code next to Source
    This is done to avoid source code mismatches during deserialization.
    Visual Studio can detect this, why can't we?

--- scalahost/src/main/scala/scala/meta/internal/hosts/scalac/tasty/package.scala
+++ scalahost/src/main/scala/scala/meta/internal/hosts/scalac/tasty/package.scala
@@ -1,41 +1,59 @@
 package scala.meta.internal.hosts.scalac
 
+import java.io._
+import java.security.MessageDigest
 import scala.meta.Source
+import scala.meta.syntactic.Content
 
 package object tasty {
   implicit class XtensionTastyWriteTree(source: Source) {
     def toTasty: Array[Byte] = {
-      import java.io._
       val baos = new ByteArrayOutputStream()
       val oos = new ObjectOutputStream(baos)
       oos.writeObject(source)
       oos.close()
       baos.close()
-      val metaBlob = baos.toByteArray()
+      val sourceBlob = baos.toByteArray()
+
       val pickler = new TastyPickler
-      val buf = new TastyBuffer(metaBlob.length)
-      pickler.newSection("Scalameta", buf)
-      buf.writeNat(metaBlob.length)
-      buf.writeBytes(metaBlob, metaBlob.length)
+      val sourceBuf = new TastyBuffer(sourceBlob.length)
+      pickler.newSection("ScalametaSource", sourceBuf)
+      sourceBuf.writeNat(sourceBlob.length)
+      sourceBuf.writeBytes(sourceBlob, sourceBlob.length)
+
+      val hashBuf = new TastyBuffer(20)
+      pickler.newSection("ScalametaHash", hashBuf)
+      val text = source.position.input match { case input: Content => new String(input.chars); case _ => "" }
+      val sha1 = MessageDigest.getInstance("SHA-1")
+      sha1.reset()
+      sha1.update(text.getBytes("UTF-8"))
+      val digest = sha1.digest()
+      hashBuf.writeNat(digest.length)
+      hashBuf.writeBytes(digest, digest.length)
+
       pickler.assembleParts()
     }
   }
+
   implicit class XtensionTastyReadTree(dummy: Source.type) {
-    def fromTasty(tastyBlob: Array[Byte]): Option[Source] = {
+    def fromTasty(tastyBlob: Array[Byte]): Option[(Source, String)] = {
       if (tastyBlob.length == 0) return None
       val unpickler = new TastyUnpickler(tastyBlob)
-      class ScalametaUnpickler extends TastyUnpickler.SectionUnpickler[Array[Byte]]("Scalameta") {
+      class ArrayUnpickler(name: String) extends TastyUnpickler.SectionUnpickler[Array[Byte]](name) {
         def unpickle(reader: TastyReader, tastyName: TastyName.Table) = {
           val length = reader.readNat()
           reader.readBytes(length)
         }
       }
-      val metaBlob = unpickler.unpickle(new ScalametaUnpickler)
-      metaBlob.map(metaBlob => {
-        import java.io._
-        val bais = new ByteArrayInputStream(metaBlob)
+      class SourceUnpickler extends TastyUnpickler.SectionUnpickler[Array[Byte]]("ScalametaSource")
+      class HashUnpickler extends TastyUnpickler.SectionUnpickler[Array[Byte]]("ScalametaHash")
+      val sourceBlob = unpickler.unpickle(new SourceUnpickler)
+      sourceBlob.flatMap(sourceBlob => {
+        val bais = new ByteArrayInputStream(sourceBlob)
         val ois = new ObjectInputStream(bais)
-        ois.readObject.asInstanceOf[Source]
+        val source = ois.readObject.asInstanceOf[Source]
+        val hash = unpickler.unpickle(new HashUnpickler)
+        hash.map(hash => (source, hash.map(b => "%2X".format(b)).mkString))
       })
     }
   }
