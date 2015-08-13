46bc9446be39d7d9beb831ba1ea9e06b9d427b2c
Eugene Burmako <xeno.by@gmail.com>
2015-08-13 10:59:45 +0200

    also saves dialect when serializing trees to TASTY
    In the future, this can probably be avoided if the TASTY spec is updated
    to include the name of the TASTY serializer.

--- scalameta/src/main/scala/scala/meta/internal/tasty/package.scala
+++ scalameta/src/main/scala/scala/meta/internal/tasty/package.scala
@@ -1,24 +1,30 @@
 package scala.meta.internal
 
 import java.io._
+import java.nio.charset.Charset
 import java.security.MessageDigest
+import scala.meta.Dialect
 import scala.meta.Source
 import scala.meta.syntactic.Content
+import org.scalameta.data._
 
 package object tasty {
-  implicit class XtensionTastyWriteTree(source: Source) {
+  implicit class XtensionTastyWriteTree(source: SemanticSource) {
     def toTasty: Array[Byte] = {
       val pickler = new TastyPickler
 
       val syntacticBuf = new TastyBuffer(20)
       pickler.newSection("ScalametaSyntactic", syntacticBuf)
+      val dialectBytes = source.dialect.name.getBytes("UTF-8")
       val text = source.position.input match { case input: Content => new String(input.chars); case _ => "" }
       val sha1 = MessageDigest.getInstance("SHA-1")
       sha1.reset()
       sha1.update(text.getBytes("UTF-8"))
-      val digest = sha1.digest()
-      syntacticBuf.writeNat(digest.length)
-      syntacticBuf.writeBytes(digest, digest.length)
+      val hashBytes = sha1.digest()
+      syntacticBuf.writeNat(dialectBytes.length)
+      syntacticBuf.writeBytes(dialectBytes, dialectBytes.length)
+      syntacticBuf.writeNat(hashBytes.length)
+      syntacticBuf.writeBytes(hashBytes, hashBytes.length)
 
       val baos = new ByteArrayOutputStream()
       val oos = new ObjectOutputStream(baos)
@@ -36,32 +42,40 @@ package object tasty {
   }
 
   implicit class XtensionTastyReadTree(dummy: Source.type) {
-    def fromTasty(tastyBlob: Array[Byte]): Option[(String, Source)] = {
+    def fromTasty(tastyBlob: Array[Byte]): Option[(SyntacticDigest, SemanticSource)] = {
+      class SyntacticUnpickler extends TastyUnpickler.SectionUnpickler[SyntacticDigest]("ScalametaSyntactic") {
+        def unpickle(reader: TastyReader, tastyName: TastyName.Table) = {
+          val dialectLength = reader.readNat()
+          val dialectName = new String(reader.readBytes(dialectLength), Charset.forName("UTF-8"))
+          val dialect = Dialect.forName(dialectName)
+          val hashLength = reader.readNat()
+          val hashBytes = reader.readBytes(hashLength)
+          val hash = hashBytes.map(b => "%2X".format(b)).mkString
+          SyntacticDigest(dialect, hash)
+        }
+      }
+
+      class SemanticUnpickler extends TastyUnpickler.SectionUnpickler[SemanticSource]("ScalametaSemantic") {
+        def unpickle(reader: TastyReader, tastyName: TastyName.Table) = {
+          val sourceLength = reader.readNat()
+          val sourceBlob = reader.readBytes(sourceLength)
+          val bais = new ByteArrayInputStream(sourceBlob)
+          val ois = new ObjectInputStream(bais)
+          ois.readObject.asInstanceOf[SemanticSource]
+        }
+      }
+
       if (tastyBlob.length == 0) return None
       val unpickler = new TastyUnpickler(tastyBlob)
-      class SyntacticUnpickler extends TastyUnpickler.SectionUnpickler[String]("ScalametaSyntactic") {
-        def unpickle(reader: TastyReader, tastyName: TastyName.Table) = {
-          val length = reader.readNat()
-          val bytes = reader.readBytes(length)
-          bytes.map(b => "%2X".format(b)).mkString
-        }
+      for {
+        syntacticDigest <- unpickler.unpickle(new SyntacticUnpickler)
+        semanticSource <- unpickler.unpickle(new SemanticUnpickler)
+      } yield {
+        (syntacticDigest, semanticSource)
       }
-      class SemanticUnpickler extends TastyUnpickler.SectionUnpickler[Array[Byte]]("ScalametaSemantic") {
-        def unpickle(reader: TastyReader, tastyName: TastyName.Table) = {
-          val length = reader.readNat()
-          reader.readBytes(length)
-        }
-      }
-      val syntacticDigest = unpickler.unpickle(new SyntacticUnpickler)
-      syntacticDigest.flatMap(syntacticDigest => {
-        val semanticBlob = unpickler.unpickle(new SemanticUnpickler)
-        semanticBlob.flatMap(semanticBlob => {
-          val bais = new ByteArrayInputStream(semanticBlob)
-          val ois = new ObjectInputStream(bais)
-          val semanticSource = ois.readObject.asInstanceOf[Source]
-          Some((syntacticDigest, semanticSource))
-        })
-      })
     }
   }
+
+  @data class SyntacticDigest(dialect: Dialect, hash: String)
+  type SemanticSource = Source
 }
