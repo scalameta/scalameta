fbcf313110fe84b085cf402b1821d07039887e4b
Eugene Burmako <xeno.by@gmail.com>
2015-10-13 16:12:51 +0200

    makes -Dconvert.debug and -Dmerge.debug logging sensitive
    This is going to prevent spurious crashes that look like this
    without hampering our debugging capabilities:
    
    Caused by: org.scalameta.invariants.InvariantFailedException: invariant failed:
    when verifying Caches.this.indicesWriteOnly.`unary_!`
    found that Caches.this.indicesWriteOnly.`unary_!` is true
    where Caches = scala.meta.internal.hosts.scalac.contexts.Proxy@17d9adc3
    	at org.scalameta.invariants.InvariantFailedException$.raise(Exceptions.scala:15)
    	at scala.meta.internal.hosts.scalac.converters.Caches$class.ldenotToMmemberIndex(Caches.scala:42)
    	at scala.meta.internal.hosts.scalac.converters.Api.ldenotToMmemberIndex(Api.scala:8)
    	at scala.meta.internal.hosts.scalac.converters.ToMmember$XtensionLsymbolToMmember.toMmember(ToMmember.scala:100)

--- scalameta/trees/src/main/scala/scala/meta/internal/ast/package.scala
+++ scalameta/trees/src/main/scala/scala/meta/internal/ast/package.scala
@@ -4,13 +4,13 @@ package internal
 package object ast {
   implicit class XtensionConvertDebug(debug: org.scalameta.debug.Debug.type) {
     def logConvert(op: => Unit): Unit = {
-      if (sys.props("convert.debug") != null) debug.log(op)
+      if (sys.props("convert.debug") != null) debug.sensitiveLog(op)
     }
   }
 
   implicit class XtensionMergeDebug(debug: org.scalameta.debug.Debug.type) {
     def logMerge(op: => Unit): Unit = {
-      if (sys.props("merge.debug") != null) debug.log(op)
+      if (sys.props("merge.debug") != null) debug.sensitiveLog(op)
     }
   }
 }
