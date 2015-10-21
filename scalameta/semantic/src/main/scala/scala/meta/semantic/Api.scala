43e7845e8db94df201585e8c41194b34704595b1
Eugene Burmako <xeno.by@gmail.com>
2015-10-21 16:49:45 +0200

    fixes a typo in the implementation of Type.=:=

--- scalameta/semantic/src/main/scala/scala/meta/semantic/Api.scala
+++ scalameta/semantic/src/main/scala/scala/meta/semantic/Api.scala
@@ -163,7 +163,7 @@ private[meta] trait Api {
 
   implicit class XtensionSemanticType(tree: Type) {
     @hosted def <:<(other: Type): Boolean = implicitly[SemanticContext].isSubtype(tree, other)
-    @hosted def =:=(other: Type): Boolean = (tree =:= other) && (other =:= tree)
+    @hosted def =:=(other: Type): Boolean = (tree <:< other) && (other <:< tree)
     @hosted def widen: Type = implicitly[SemanticContext].widen(tree)
     @hosted def dealias: Type = implicitly[SemanticContext].dealias(tree)
     @hosted def companion: Type.Ref = ???
