9be6563797ea1540f3b0fc4d62942bd4f46e2527
Eugene Burmako <xeno.by@gmail.com>
2016-04-13 08:17:27 +0300

    this is something I wanted for a while
    The ability to write `transformer(tree)` instead of `transformer.transform(tree)`.
    It's a shame that you can't write `super(tree)` though.

--- foundation/src/main/scala/scala/meta/internal/transversers/transformer.scala
+++ foundation/src/main/scala/scala/meta/internal/transversers/transformer.scala
@@ -19,26 +19,26 @@ class TransformerMacros(val c: Context) extends TransverserMacros {
 
   def generatedMethods(cases: List[CaseDef]): Tree = {
     q"""
-      def transform(tree: $TreeClass): $TreeClass = {
+      def apply(tree: $TreeClass): $TreeClass = {
         tree match { case ..$cases }
       }
 
-      def transform(treeopt: $OptionClass[$TreeClass]): $OptionClass[$TreeClass] = treeopt match {
+      def apply(treeopt: $OptionClass[$TreeClass]): $OptionClass[$TreeClass] = treeopt match {
         case $SomeModule(tree) =>
-          val tree1 = transform(tree)
+          val tree1 = apply(tree)
           if (tree eq tree1) treeopt
           else $SomeModule(tree1)
         case $NoneModule =>
           $NoneModule
       }
 
-      def transform(trees: $SeqClass[$TreeClass]): $SeqClass[$TreeClass] = {
+      def apply(trees: $SeqClass[$TreeClass]): $SeqClass[$TreeClass] = {
         var same = true
         val buf = $ListBufferModule[$TreeClass]()
         val it = trees.iterator
         while (it.hasNext) {
           val tree = it.next
-          val tree1 = transform(tree)
+          val tree1 = apply(tree)
           if (tree ne tree1) same = false
           buf += tree1
         }
@@ -46,22 +46,22 @@ class TransformerMacros(val c: Context) extends TransverserMacros {
         else buf.toList
       }
 
-      def transform(treesopt: $OptionClass[$SeqClass[$TreeClass]])(implicit hack: $Hack1Class): $OptionClass[$SeqClass[$TreeClass]] = treesopt match {
+      def apply(treesopt: $OptionClass[$SeqClass[$TreeClass]])(implicit hack: $Hack1Class): $OptionClass[$SeqClass[$TreeClass]] = treesopt match {
         case $SomeModule(trees) =>
-          val trees1 = transform(trees)
+          val trees1 = apply(trees)
           if (trees eq trees1) treesopt
           else $SomeModule(trees1)
         case $NoneModule =>
           $NoneModule
       }
 
-      def transform(treess: $SeqClass[$SeqClass[$TreeClass]])(implicit hack: $Hack2Class): $SeqClass[$SeqClass[$TreeClass]] = {
+      def apply(treess: $SeqClass[$SeqClass[$TreeClass]])(implicit hack: $Hack2Class): $SeqClass[$SeqClass[$TreeClass]] = {
         var same = true
         val buf = $ListBufferModule[$SeqClass[$TreeClass]]()
         val it = treess.iterator
         while (it.hasNext) {
           val trees = it.next
-          val trees1 = transform(trees)
+          val trees1 = apply(trees)
           if (trees ne trees1) same = false
           buf += trees1
         }
--- foundation/src/main/scala/scala/meta/internal/transversers/traverser.scala
+++ foundation/src/main/scala/scala/meta/internal/transversers/traverser.scala
@@ -15,37 +15,37 @@ class TraverserMacros(val c: Context) extends TransverserMacros {
 
   def leafHandler(l: Leaf): Tree = {
     val relevantFields = l.fields.filter(f => !(f.tpe =:= typeOf[Any]) && !(f.tpe =:= typeOf[String]))
-    val recursiveTraversals = relevantFields.map(f => q"this.traverse(${f.name})")
+    val recursiveTraversals = relevantFields.map(f => q"this.apply(${f.name})")
     q"..$recursiveTraversals"
   }
 
   def generatedMethods(cases: List[CaseDef]): Tree = {
     q"""
-      def traverse(tree: $TreeClass): $UnitClass = {
+      def apply(tree: $TreeClass): $UnitClass = {
         tree match { case ..$cases }
       }
 
-      def traverse(treeopt: $OptionClass[$TreeClass]): $UnitClass = treeopt match {
-        case $SomeModule(tree) => traverse(tree)
+      def apply(treeopt: $OptionClass[$TreeClass]): $UnitClass = treeopt match {
+        case $SomeModule(tree) => apply(tree)
         case $NoneModule => // do nothing
       }
 
-      def traverse(trees: $SeqClass[$TreeClass]): $UnitClass = {
+      def apply(trees: $SeqClass[$TreeClass]): $UnitClass = {
         val it = trees.iterator
         while (it.hasNext) {
-          traverse(it.next)
+          apply(it.next)
         }
       }
 
-      def traverse(treesopt: $OptionClass[$SeqClass[$TreeClass]])(implicit hack: $Hack1Class): $UnitClass = treesopt match {
-        case $SomeModule(trees) => traverse(trees)
+      def apply(treesopt: $OptionClass[$SeqClass[$TreeClass]])(implicit hack: $Hack1Class): $UnitClass = treesopt match {
+        case $SomeModule(trees) => apply(trees)
         case $NoneModule => // do nothing
       }
 
-      def traverse(treess: $SeqClass[$SeqClass[$TreeClass]])(implicit hack: $Hack2Class): $UnitClass = {
+      def apply(treess: $SeqClass[$SeqClass[$TreeClass]])(implicit hack: $Hack2Class): $UnitClass = {
         val it = treess.iterator
         while (it.hasNext) {
-          traverse(it.next)
+          apply(it.next)
         }
       }
     """
