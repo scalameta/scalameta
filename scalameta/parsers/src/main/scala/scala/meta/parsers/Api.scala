3a25e728cc7ac2f57570fd2c7aa6316faee39a43
Eugene Burmako <xeno.by@gmail.com>
2016-04-20 16:06:17 +0300

    alternative way to parse when dialect needs to be specified explicitly

--- scalameta/parsers/src/main/scala/scala/meta/parsers/Api.scala
+++ scalameta/parsers/src/main/scala/scala/meta/parsers/Api.scala
@@ -4,11 +4,23 @@ package parsers
 import org.scalameta.convert._
 import scala.meta.inputs._
 
+class InputWithDialect(input: Input, dialect: Dialect) {
+  def parse[U](implicit parse: Parse[U]): Parsed[U] = {
+    parse.apply(input)(dialect)
+  }
+}
+
 private[meta] trait Api {
   implicit class XtensionParseInputLike[T](inputLike: T) {
     def parse[U](implicit convert: Convert[T, Input], parse: Parse[U], dialect: Dialect): Parsed[U] = {
       val input = convert(inputLike)
-      parse(input)(dialect)
+      parse.apply(input)(dialect)
+    }
+  }
+  implicit class XtensionDialectParseInputLike(dialect: Dialect) {
+    def apply[T](inputLike: T)(implicit convert: Convert[T, Input]): InputWithDialect = {
+      val input = convert(inputLike)
+      new InputWithDialect(input, dialect)
     }
   }
 }
