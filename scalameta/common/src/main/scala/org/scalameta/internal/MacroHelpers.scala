e1bb608854e424c4ecaf019862ec8e67829e78ae
Eugene Burmako <xeno.by@gmail.com>
2016-04-24 08:31:09 +0300

    this is no longer necessary

--- scalameta/common/src/main/scala/org/scalameta/internal/MacroHelpers.scala
+++ scalameta/common/src/main/scala/org/scalameta/internal/MacroHelpers.scala
@@ -50,7 +50,6 @@ trait MacroHelpers extends DebugFinder
   val AstTyperMacrosBundle = hygienicRef[scala.meta.internal.ast.AstTyperMacrosBundle]
   val AstInfoClass = hygienicRef[scala.meta.internal.ast.AstInfo[_]]
   val TokenMetadataModule = hygienicRef(scala.meta.internal.tokens.Metadata)
-  val InstanceTagClass = hygienicRef[scala.meta.internal.parsers.InstanceTag[_]]
   val BooleanClass = hygienicRef[scala.Boolean]
 
   private def fqRef(fqName: String, isTerm: Boolean): Tree = {
