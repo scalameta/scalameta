b8fecf38a9ac46033d7695ac2d1002c816048d01
Eugene Burmako <xeno.by@gmail.com>
2016-05-27 13:41:19 +0200

    special treatment for ...$ in Term.Apply (fixes #277, illustrates #405)

--- scalameta/trees/src/main/scala/scala/meta/internal/ast/SyntacticTrees.scala
+++ scalameta/trees/src/main/scala/scala/meta/internal/ast/SyntacticTrees.scala
@@ -2,9 +2,22 @@ package scala.meta
 package internal
 package ast
 
-object SyntacticApply {
-  // NOTE: Brings up good memories of thousands of lines of SyntacticXXX in scala.reflect.
-  // It looks like we can't completely get away from this unsatisfying pattern even in scala.meta.
-  // See #277 for details.
-  def
+import scala.{Seq => _}
+import scala.collection.immutable.Seq
+
+// NOTE: Brings up great memories of thousands of lines of SyntacticXXX in scala.reflect.
+// It looks like we can't completely get away from this unsatisfying pattern even in scala.meta.
+// See #277 and #405 for details.
+object Syntactic {
+  object Term {
+    object Apply {
+      def apply(fun: scala.meta.Term, argss: Seq[Seq[scala.meta.Term.Arg]]): scala.meta.Term = {
+        ???
+      }
+
+      def unapply(tree: Tree): Option[(scala.meta.Term, Seq[Seq[scala.meta.Term.Arg]])] = {
+        ???
+      }
+    }
+  }
 }
