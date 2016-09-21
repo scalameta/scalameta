2c0e102027b55265a9e8b1c070cc710c0164f231
Eugene Burmako <xeno.by@gmail.com>
2016-09-21 15:25:46 +0200

    2.0.0-SNAPSHOT

--- project/build.scala
+++ project/build.scala
@@ -11,7 +11,7 @@ import scala.compat.Platform.EOL
 
 object build extends Build {
   lazy val ScalaVersions = Seq("2.11.8")
-  lazy val LibraryVersion = "1.2.0-SNAPSHOT"
+  lazy val LibraryVersion = "2.0.0-SNAPSHOT"
 
   lazy val scalametaRoot = Project(
     id = "scalametaRoot",
