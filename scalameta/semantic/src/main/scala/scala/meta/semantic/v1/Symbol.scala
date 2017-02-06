971925383086ece7348bc7be7209297f0b514295
Eugene Burmako <eburmako@twitter.com>
2017-02-06 14:15:17 -0800

    fix parsing of Symbol.Multi

--- scalameta/semantic/src/main/scala/scala/meta/semantic/v1/Symbol.scala
+++ scalameta/semantic/src/main/scala/scala/meta/semantic/v1/Symbol.scala
@@ -5,7 +5,9 @@ package v1
 import scala.{Seq => _}
 import scala.collection.immutable.Seq
 import scala.collection.mutable
+import scala.compat.Platform.EOL
 import org.scalameta.adt._
+import org.scalameta.invariants._
 import scala.meta.common._
 import scala.meta.internal.semantic.v1._
 
@@ -38,9 +40,9 @@ object Symbol {
     override def toString = syntax
   }
 
-  @leaf class Multi(ids: Seq[Symbol]) extends Symbol {
-    override def syntax = ids.map(_.syntax).mkString(";")
-    override def structure = s"""Symbol.Multi(${ids.map(_.structure).mkString(", ")})"""
+  @leaf class Multi(symbols: Seq[Symbol] @nonEmpty) extends Symbol {
+    override def syntax = symbols.map(_.syntax).mkString(";")
+    override def structure = s"""Symbol.Multi(${symbols.map(_.structure).mkString(", ")})"""
     override def toString = syntax
   }
 
@@ -48,77 +50,131 @@ object Symbol {
   // It'll do for prototyping, but in the future we'll have to replace it.
   // upd. Ugh, I should've started with fastparse in the first place!!
   def apply(s: String): Symbol = {
-    if (s == "") {
-      Symbol.None
-    } else if (s.startsWith("_")) {
+    object naiveParser {
       var i = 0
-
-      var currChar = '\u0000'
-      def readChar(): Char = {
-        if (i >= s.length) sys.error(s"invalid format: $s")
-        currChar = s(i)
-        i += 1
-        currChar
+      def fail() = {
+        val message = "invalid symbol format"
+        val caret = " " * (i - 1) + "^"
+        sys.error(s"$message$EOL$s$EOL$caret")
       }
 
-      def readName(): String = {
+      val BOF = '\u0000'
+      val EOF = '\u001A'
+      var currChar = BOF
+      def readChar(): Char = {
+        if (i >= s.length) {
+          if (i == s.length) {
+            currChar = EOF
+            i += 1
+            currChar
+          } else {
+            fail()
+          }
+        } else {
+          currChar = s(i)
+          i += 1
+          currChar
+        }
+      }
+
+      def parseName(): String = {
         val buf = new StringBuilder()
         if (currChar == '`') {
           while (readChar() != '`') buf += currChar
           readChar()
         } else {
-          if (!Character.isJavaIdentifierStart(currChar)) sys.error(s"invalid format: $s")
+          if (!Character.isJavaIdentifierStart(currChar)) fail()
           buf += currChar
           while (Character.isJavaIdentifierPart(readChar())) buf += currChar
         }
         buf.toString
       }
 
-      def parse(curr: Symbol): Symbol = {
-        if (i == s.length) return curr
-        readChar()
-
-        if (currChar == '[') {
+      def parseGlobal(curr: Symbol): Symbol = {
+        if (currChar == EOF) {
+          curr
+        } else if (currChar == ';') {
+          curr
+        } else if (currChar == '[') {
           readChar()
-          val name = readName()
-          if (currChar != ']') sys.error(s"invalid format: $s")
-          parse(Symbol.Global(curr, Signature.TypeParameter(name)))
+          val name = parseName()
+          if (currChar != ']') fail()
+          else readChar()
+          parseGlobal(Symbol.Global(curr, Signature.TypeParameter(name)))
         } else if (currChar == '(') {
           readChar()
-          val name = readName()
-          if (currChar != ')') sys.error(s"invalid format: $s")
-          parse(Symbol.Global(curr, Signature.TermParameter(name)))
+          val name = parseName()
+          if (currChar != ')') fail()
+          else readChar()
+          parseGlobal(Symbol.Global(curr, Signature.TermParameter(name)))
         } else {
-          val name = readName()
+          val name = parseName()
           if (currChar == '#') {
-            parse(Symbol.Global(curr, Signature.Type(name)))
+            readChar()
+            parseGlobal(Symbol.Global(curr, Signature.Type(name)))
           } else if (currChar == '.') {
-            parse(Symbol.Global(curr, Signature.Term(name)))
+            readChar()
+            parseGlobal(Symbol.Global(curr, Signature.Term(name)))
           } else if (currChar == '(') {
             val buf = new StringBuilder()
             buf += currChar
             while (readChar() != '.') buf += currChar
-            parse(Symbol.Global(curr, Signature.Method(name, buf.toString)))
+            readChar()
+            parseGlobal(Symbol.Global(curr, Signature.Method(name, buf.toString)))
           } else if (currChar == '=') {
             readChar()
-            if (currChar != '>') sys.error(s"invalid format: $s")
-            parse(Symbol.Global(curr, Signature.Self(name)))
+            if (currChar != '>') fail()
+            else readChar()
+            parseGlobal(Symbol.Global(curr, Signature.Self(name)))
           } else {
-            sys.error(s"invalid format: $s")
+            fail()
           }
         }
       }
+      def parseLocal(): Symbol = {
+        val addrBuf = new StringBuilder
+        addrBuf += currChar
+        while (readChar() != '@') addrBuf += currChar
 
-      parse(Symbol.None)
-    } else {
-      val LocalSymbol = """^(.*?)@(\d+)\.\.(\d+)$""".r
-      s match {
-        case LocalSymbol(s_addr, s_start, s_end) =>
-          Symbol.Local(Address(s_addr), s_start.toInt, s_end.toInt)
-        case _ =>
-          sys.error(s"invalid format: $s")
+        val startBuf = new StringBuilder
+        while (Character.isDigit(readChar())) startBuf += currChar
+
+        if (currChar != '.') fail()
+        readChar()
+        if (currChar != '.') fail()
+
+        val endBuf = new StringBuilder
+        while (Character.isDigit(readChar())) endBuf += currChar
+
+        Symbol.Local(Address(addrBuf.toString), startBuf.toString.toInt, endBuf.toString.toInt)
+      }
+
+      def parseMulti(symbols: List[Symbol]): Symbol = {
+        if (currChar == EOF) {
+          symbols match {
+            case Nil => Symbol.None
+            case List(symbol) => symbol
+            case symbols => Symbol.Multi(symbols)
+          }
+        } else {
+          val symbol = {
+            if (currChar == '_') parseGlobal(Symbol.None)
+            else parseLocal()
+          }
+          if (currChar == ';') {
+            readChar()
+            if (currChar == EOF) fail()
+          }
+          parseMulti(symbols :+ symbol)
+        }
+      }
+
+      def entryPoint(): Symbol = {
+        readChar()
+        parseMulti(Nil)
       }
     }
+    naiveParser.entryPoint()
   }
 }
 
