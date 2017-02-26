f985ac260f8896a16d10ed67b0430f9515fa4fb7
David Dudson <DavidDudson@users.noreply.github.com>
2017-02-27 05:26:09 +1300

    Fix bug where corpus would break (#704)

--- scalameta/contrib/src/test/scala/scala/meta/contrib/EqualSuite.scala
+++ scalameta/contrib/src/test/scala/scala/meta/contrib/EqualSuite.scala
@@ -32,10 +32,19 @@ class EqualSuite extends FunSuite {
   test("structural property") {
     val errors = SyntaxAnalysis.onParsed[Tree](ContribSuite.corpus) { ast =>
       // empty transformation preserve structural equality
-      val a = ast.transform { case Term.Name(x) => Term.Name(x) }
-      val b = ast.transform { case Type.Name(x) => Type.Name(x) }
-      val refEqual = a.equals(b) // should be false
+      val termNameTransformer: PartialFunction[Tree, Tree] = { case Term.Name(x) => Term.Name(x) }
+      val typeNameTransformer: PartialFunction[Tree, Tree] = { case Type.Name(x) => Type.Name(x) }
+
+      // We need to make sure that the tree will actually be transformed. or ref equality will be true
+      val transformWillChangeRef = termNameTransformer.isDefinedAt(ast) || typeNameTransformer
+          .isDefinedAt(ast)
+
+      val a = ast.transform(termNameTransformer)
+      val b = ast.transform(typeNameTransformer)
+
+      val refEqual = a.equals(b) && transformWillChangeRef // should be false
       val structuralEqual = a.equal[Structurally](b) // should be true
+
       if (refEqual || !structuralEqual) Seq(a)
       else Nil
     }
