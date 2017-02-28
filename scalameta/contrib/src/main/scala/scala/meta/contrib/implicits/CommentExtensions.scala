d4104bea75ea91de5edfd3aeb6ffa7956ee23408
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2017-02-28 13:01:30 +0100

    Move Comment pimps logic to a class named 'CommentOps'

--- scalameta/contrib/src/main/scala/scala/meta/contrib/implicits/CommentExtensions.scala
+++ scalameta/contrib/src/main/scala/scala/meta/contrib/implicits/CommentExtensions.scala
@@ -1,41 +1,20 @@
 package scala.meta.contrib.implicits
 
-import scala.meta._
-import scala.meta.contrib.{DocToken, ScaladocParser}
+import scala.meta.contrib.{CommentOps, DocToken}
 import scala.meta.tokens.Token.Comment
 
 trait CommentExtensions {
 
-  private[this] val scaladocSymbols = Seq('/', '*', ' ')
-
   implicit class XtensionCommentOps(c: Comment) {
 
-    private[this] val rawSyntax: String = c.show[Syntax].trim
+    @inline
+    def isScaladoc: Boolean = CommentOps.isScaladoc(c)
 
     @inline
-    def isScaladoc: Boolean = {
-      rawSyntax.startsWith("/**") && rawSyntax.endsWith("*/")
-    }
+    def content: String = CommentOps.content(c)
 
     @inline
-    def content: String = {
-      if (isScaladoc) {
-        rawSyntax.lines
-          .map(_.dropWhile(scaladocSymbols.contains)) // Removes leading comments symbols
-          .map(l => l.take(l.lastIndexWhere(!scaladocSymbols.contains(_)) + 1)) // Remove trailing comments symbols
-          .map(_.trim)
-          .toSeq
-          .mkString("\n")
-          .trim
-      } else {
-        rawSyntax
-          .take(rawSyntax.lastIndexWhere(!scaladocSymbols.contains(_)) + 1) // Remove trailing comments symbols
-          .dropWhile(scaladocSymbols.contains) // Removes leading comments symbols
-      }
-    }
-
-    @inline
-    def structure: Seq[DocToken] = ScaladocParser.parseScaladoc(c)
+    def docTokens: Seq[DocToken] = CommentOps.docTokens(c)
   }
 
 }
