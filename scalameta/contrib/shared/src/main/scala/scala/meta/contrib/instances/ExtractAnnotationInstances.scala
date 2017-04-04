a30c2e2316160e76e1d228c17ba23b28e2c2f561
David Dudson <davidjohndudson@gmail.com>
2017-04-05 09:21:42 +1200

    Fix formatting

--- scalameta/contrib/shared/src/main/scala/scala/meta/contrib/instances/ExtractAnnotationInstances.scala
+++ scalameta/contrib/shared/src/main/scala/scala/meta/contrib/instances/ExtractAnnotationInstances.scala
@@ -4,8 +4,7 @@ import scala.meta._
 import scala.meta.contrib._
 
 trait ExtractAnnotationInstances {
-  implicit def extractAnnotationsFromMods[A](
-      implicit ev: Extract[A, Mod]): Extract[A, Mod.Annot] =
+  implicit def extractAnnotationsFromMods[A](implicit ev: Extract[A, Mod]): Extract[A, Mod.Annot] =
     Extract(a => ev.extract(a).collect { case m: Mod.Annot => m })
 }
 
--- scalameta/contrib/shared/src/main/scala/scala/meta/contrib/instances/ExtractStatSubtypeInstances.scala
+++ scalameta/contrib/shared/src/main/scala/scala/meta/contrib/instances/ExtractStatSubtypeInstances.scala
@@ -4,64 +4,49 @@ import scala.meta._
 import scala.meta.contrib._
 
 trait ExtractStatSubtypeInstances {
-  implicit def extractDefsFromStats[A](
-      implicit ev: Extract[A, Stat]): Extract[A, Defn.Def] =
+  implicit def extractDefsFromStats[A](implicit ev: Extract[A, Stat]): Extract[A, Defn.Def] =
     Extract(a => ev.extract(a).collect { case d: Defn.Def => d })
 
-  implicit def extractValsFromStats[A](
-      implicit ev: Extract[A, Stat]): Extract[A, Defn.Val] =
+  implicit def extractValsFromStats[A](implicit ev: Extract[A, Stat]): Extract[A, Defn.Val] =
     Extract(a => ev.extract(a).collect { case v: Defn.Val => v })
 
-  implicit def extractVarsFromStats[A](
-      implicit ev: Extract[A, Stat]): Extract[A, Defn.Var] =
+  implicit def extractVarsFromStats[A](implicit ev: Extract[A, Stat]): Extract[A, Defn.Var] =
     Extract(a => ev.extract(a).collect { case v: Defn.Var => v })
 
-  implicit def extractTraitsFromStats[A](
-      implicit ev: Extract[A, Stat]): Extract[A, Defn.Trait] =
+  implicit def extractTraitsFromStats[A](implicit ev: Extract[A, Stat]): Extract[A, Defn.Trait] =
     Extract(a => ev.extract(a).collect { case t: Defn.Trait => t })
 
-  implicit def extractObjectsFromStats[A](
-      implicit ev: Extract[A, Stat]): Extract[A, Defn.Object] =
+  implicit def extractObjectsFromStats[A](implicit ev: Extract[A, Stat]): Extract[A, Defn.Object] =
     Extract(a => ev.extract(a).collect { case o: Defn.Object => o })
 
-  implicit def extractClassesFromStats[A](
-      implicit ev: Extract[A, Stat]): Extract[A, Defn.Class] =
+  implicit def extractClassesFromStats[A](implicit ev: Extract[A, Stat]): Extract[A, Defn.Class] =
     Extract(a => ev.extract(a).collect { case c: Defn.Class => c })
 
-  implicit def extractTypesFromStats[A](
-      implicit ev: Extract[A, Stat]): Extract[A, Defn.Type] =
+  implicit def extractTypesFromStats[A](implicit ev: Extract[A, Stat]): Extract[A, Defn.Type] =
     Extract(a => ev.extract(a).collect { case t: Defn.Type => t })
 
-  implicit def extractTermsFromStats[A](
-       implicit ev: Extract[A, Stat]): Extract[A, Term] =
+  implicit def extractTermsFromStats[A](implicit ev: Extract[A, Stat]): Extract[A, Term] =
     Extract(a => ev.extract(a).collect { case t: Term => t })
 
-  implicit def extractMembersFromStats[A](
-       implicit ev: Extract[A, Stat]): Extract[A, Member] =
+  implicit def extractMembersFromStats[A](implicit ev: Extract[A, Stat]): Extract[A, Member] =
     Extract(a => ev.extract(a).collect { case d: Member => d })
 
-  implicit def extractDefnsFromStats[A](
-       implicit ev: Extract[A, Stat]): Extract[A, Defn] =
+  implicit def extractDefnsFromStats[A](implicit ev: Extract[A, Stat]): Extract[A, Defn] =
     Extract(a => ev.extract(a).collect { case d: Defn => d })
 
-  implicit def extractDeclsFromStats[A](
-      implicit ev: Extract[A, Stat]): Extract[A, Decl] =
+  implicit def extractDeclsFromStats[A](implicit ev: Extract[A, Stat]): Extract[A, Decl] =
     Extract(a => ev.extract(a).collect { case d: Decl => d })
 
-  implicit def extractDeclDefsFromStats[A](
-      implicit ev: Extract[A, Stat]): Extract[A, Decl.Def] =
+  implicit def extractDeclDefsFromStats[A](implicit ev: Extract[A, Stat]): Extract[A, Decl.Def] =
     Extract(a => ev.extract(a).collect { case d: Decl.Def => d })
 
-  implicit def extractDeclVarsFromStats[A](
-      implicit ev: Extract[A, Stat]): Extract[A, Decl.Var] =
+  implicit def extractDeclVarsFromStats[A](implicit ev: Extract[A, Stat]): Extract[A, Decl.Var] =
     Extract(a => ev.extract(a).collect { case v: Decl.Var => v })
 
-  implicit def extractDeclValsFromStats[A](
-      implicit ev: Extract[A, Stat]): Extract[A, Decl.Val] =
+  implicit def extractDeclValsFromStats[A](implicit ev: Extract[A, Stat]): Extract[A, Decl.Val] =
     Extract(a => ev.extract(a).collect { case v: Decl.Val => v })
 
-  implicit def extractDeclTypesFromStats[A](
-      implicit ev: Extract[A, Stat]): Extract[A, Decl.Type] =
+  implicit def extractDeclTypesFromStats[A](implicit ev: Extract[A, Stat]): Extract[A, Decl.Type] =
     Extract(a => ev.extract(a).collect { case v: Decl.Type => v })
 }
 
