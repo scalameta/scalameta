f3a7d83a95d439fd654480517398a949152958c7
David Dudson <davidjohndudson@gmail.com>
2017-04-08 13:31:58 +1200

    Support syntactically and structurally for Set[Tree]

---
+++ scalameta/contrib/shared/src/main/scala/scala/meta/contrib/implicits/SetExtensions.scala
@@ -0,0 +1,15 @@
+package scala.meta.contrib.implicits
+
+import scala.meta._
+import scala.meta.contrib._
+import scala.meta.contrib.equality.{Structurally, Syntactically}
+
+trait SetExtensions {
+  implicit class SetEnrichments[A <: Tree](set: Set[A]) {
+    def structurally: Set[Structurally[A]] =
+      set.map(Structurally(_))
+
+    def syntactically: Set[Syntactically[A]] =
+      set.map(Syntactically(_))
+  }
+}
