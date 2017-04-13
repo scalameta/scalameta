55c1a546283662f182e8e4fdc087b69761aaa3d5
Eugene Burmako <eburmako@twitter.com>
2017-04-13 12:01:18 -0700

    support for OBJECT, PACKAGE and PACKAGEOBJECT

--- scalahost/core/src/main/scala/scala/meta/internal/scalahost/v1/online/DenotationOps.scala
+++ scalahost/core/src/main/scala/scala/meta/internal/scalahost/v1/online/DenotationOps.scala
@@ -7,13 +7,20 @@ import scala.{meta => m}
 import scala.meta.semantic.v1._
 
 trait DenotationOps { self: Mirror =>
+  import g._
 
   implicit class XtensionGSymbolMDenotation(sym: g.Symbol) {
     def toDenotation: m.Denotation = {
+      if (sym.isModuleClass) return sym.asClass.module.toDenotation
+
       var flags: Long = 0
-      // TODO: support other flags
+
+      if (sym.isModule && !sym.hasPackageFlag && sym.name != nme.PACKAGE) flags |= OBJECT
+      if (sym.hasPackageFlag) flags |= PACKAGE
+      if (sym.isModule && sym.name == nme.PACKAGE) flags |= PACKAGEOBJECT
       if (sym.isClass && !sym.isTrait) flags |= CLASS
       if (sym.isTrait) flags |= TRAIT
+
       m.Denotation(flags)
     }
   }
