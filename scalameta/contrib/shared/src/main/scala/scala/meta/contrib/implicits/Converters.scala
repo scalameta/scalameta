e01a388e2c590a70a2844d8534ca3faccf7c51be
Lloyd <lloydmeta@users.noreply.github.com>
2017-05-11 18:08:59 +0900

    Close #801 by adding Type.Arg#toType (#855)
    * Close #801 by adding Type.Arg#toType
    
    * Add a simple test

--- scalameta/contrib/shared/src/main/scala/scala/meta/contrib/implicits/Converters.scala
+++ scalameta/contrib/shared/src/main/scala/scala/meta/contrib/implicits/Converters.scala
@@ -9,6 +9,14 @@ trait Converters {
     def asCtorRef: Ctor.Ref.Name = Ctor.Ref.Name(n.value)
     def asPat: Pat.Var.Term = Pat.Var.Term(n.asTerm)
   }
+
+  implicit class XtensionClassTypeArg(arg: Type.Arg) {
+    def toType: Type = arg match {
+      case Type.Arg.Repeated(tpe) => tpe
+      case Type.Arg.ByName(tpe) => tpe
+      case tpe: Type => tpe
+    }
+  }
 }
 
 object Converters extends Converters
