8b39d0626433175f7ad4ae9da8e49b3d51be9779
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-05-27 15:30:31 +0200

    Fix #897, handle BOF and EOF tokens.

--- scalameta/contrib/shared/src/test/scala/scala/meta/contrib/AssociatedCommentsTest.scala
+++ scalameta/contrib/shared/src/test/scala/scala/meta/contrib/AssociatedCommentsTest.scala
@@ -6,23 +6,20 @@ import scala.meta.tokens.Token
 import org.scalatest.FunSuite
 
 class AssociatedCommentsTest extends FunSuite {
-  val input: Source =
-    """|import a.b
-       |/** leading docstring */
-       |object a  {
-       |  // leading 2
-       |  val x = 2 // trailing
-       |}
-       |""".stripMargin.parse[Source].get
-  val comments = AssociatedComments(input)
   test("leading") {
+    val input: Source =
+      """|import a.b
+         |/** leading docstring */
+         |object a  {
+         |  // leading 2
+         |  val x = 2 // trailing
+         |}
+         |""".stripMargin.parse[Source].get
+    val comments = AssociatedComments(input)
     val defnObject = input.find(_.is[Defn.Object]).get
     val defnVal = input.find(_.is[Defn.Val]).get
     val lit = input.find(_.is[Lit]).get
-    comments.leading(defnVal).head match {
-      case Token.Comment(a) =>
-        println(s"'$a'")
-    }
+    val Token.Comment(a) = comments.leading(defnVal).head
     val Seq(Token.Comment("* leading docstring ")) =
       comments.leading(defnObject).to[Seq]
     val Seq() = comments.trailing(defnObject).to[Seq]
@@ -32,5 +29,28 @@ class AssociatedCommentsTest extends FunSuite {
       comments.trailing(defnVal).to[Seq]
     val Seq(Token.Comment(" trailing")) =
       comments.trailing(lit).to[Seq]
+  }
+
+  test("#897 first comment in file") {
+    val input = """|/** Scaladoc for class A
+                   |  */
+                   |class A
+                   |/** Scaladoc for object A
+                   |  */
+                   |object A""".stripMargin.parse[Source].get
+    val comments = AssociatedComments(input.tokens)
+    val source @ Source(stats) = input
+    val result = stats.map(comments.leading)
+    assert(result.forall(_.nonEmpty))
+    assert(
+      comments.syntax ===
+        """AssociatedComments(
+          |  Leading =
+          |    class [30..35) => List(/**∙Scaladoc∙for∙class∙A¶∙∙*/)
+          |    object [69..75) => List(/**∙Scaladoc∙for∙object∙A¶∙∙*/)
+          |
+          |  Trailing =
+          |
+          |)""".stripMargin)
   }
 }
