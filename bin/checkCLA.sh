62a2d9725f8ba3e91b41a1a336eefbe9032bf3b9
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-07-12 14:49:13 +0200

    Fix checkCLA.

--- bin/checkCLA.sh
+++ bin/checkCLA.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-if [ -n "$CI_PULL_REQUEST" ] ; then
-  echo "Incoming pull request from https://github.com/$CI_REPO/pull/$CI_PULL_REQUEST";
-  author=$(curl -s "https://api.github.com/repos/$CI_REPO/pulls/$CI_PULL_REQUEST" | jq -r ".user.login");
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
+  echo "Incoming pull request from https://github.com/$TRAVIS_REPO_SLUG/pull/$TRAVIS_PULL_REQUEST";
+  author=$(curl -u dummy4dummy:dummy2dummy -s "https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST" | jq -r ".user.login");
   if [ $? -ne 0 ] ; then exit 1; fi;
   echo "Pull request submitted by $author";
-  signed=$(curl -s http://www.lightbend.com/contribute/cla/scala/check/$author | jq -r ".signed");
+  signed=$(curl -s https://www.lightbend.com/contribute/cla/scala/check/$author | jq -r ".signed");
   if [ $? -ne 0 ] ; then exit 1; fi;
   if [ "$signed" = "true" ] ; then
     echo "CLA check for $author successful";
   else
     echo "CLA check for $author failed";
-    echo "Please sign the Scala CLA to contribute to scala.meta";
+    echo "Please sign the Scala CLA to contribute to Scalameta";
     echo "Go to https://www.lightbend.com/contribute/cla/scala and then resubmit this pull request";
     exit 1;
   fi;
-fi
-
+fi;
