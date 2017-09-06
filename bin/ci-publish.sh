10a2c60b043f40f0b1cf2319d6576f60fee99908
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-09-06 11:41:56 +0200

    Remove unnecessary code in ci-publish.sh

--- bin/ci-publish.sh
+++ bin/ci-publish.sh
@@ -6,14 +6,7 @@ SECURE_VAR=${TRAVIS_SECURE_ENV_VARS:-false}
 if [[ "$TRAVIS_SECURE_ENV_VARS" == true && -n "$TRAVIS_TAG" && "$CI_PUBLISH" == true ]]; then
   git log | head -n 20
   echo "$PGP_SECRET" | base64 --decode | gpg --import
-  mkdir -p $HOME/.bintray
-  cat > $HOME/.bintray/.credentials <<EOF
-realm = Bintray API Realm
-host = api.bintray.com
-user = ${BINTRAY_USERNAME}
-password = ${BINTRAY_API_KEY}
-EOF
-  sbt "sonatypeOpen scalameta-$TRAVIS_TAG" ci-publish sonatypeReleaseAll
+  sbt ci-publish sonatypeReleaseAll
 else
   echo "Skipping publish, branch=$TRAVIS_BRANCH publish=$PUBLISH test=$CI_TEST"
 fi
