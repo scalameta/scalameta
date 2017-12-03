8bba1671804fcc83efa4491cf3c4c4b0ee27bb3f
Eugene Burmako <eburmako@twitter.com>
2017-12-02 21:30:22 -0800

    Skip non-fatal errors for individual files

--- langmeta-sqlite/src/main/scala/org/langmeta/sqlite/Main.scala
+++ langmeta-sqlite/src/main/scala/org/langmeta/sqlite/Main.scala
@@ -7,6 +7,7 @@ import scala.Array
 import scala.collection.mutable
 import scala.io.Codec
 import scala.reflect.classTag
+import scala.util.control.NonFatal
 import org.langmeta.internal.semanticdb.schema._
 
 object Main {
@@ -67,113 +68,119 @@ object Main {
 
           semanticdbFilenames.foreach { semanticdbFilename =>
             val path = Paths.get(semanticdbFilename)
-            val bytes = Files.readAllBytes(path)
-            val db = Database.parseFrom(bytes)
-            db.documents.foreach { document =>
-              documentsToPaths.get(document.filename) match {
-                case Some(existingPath) =>
-                  val what = document.filename
-                  val details = "both $existingPath and $path"
-                  println(s"Duplicate document filename $what in $details")
-                case None =>
-                  genuineDocuments += 1
-                  documentsToPaths(document.filename) = path
+            try {
+              val bytes = Files.readAllBytes(path)
+              val db = Database.parseFrom(bytes)
+              db.documents.foreach { document =>
+                documentsToPaths.get(document.filename) match {
+                  case Some(existingPath) =>
+                    val what = document.filename
+                    val details = "both $existingPath and $path"
+                    println(s"Duplicate document filename $what in $details")
+                  case None =>
+                    genuineDocuments += 1
+                    documentsToPaths(document.filename) = path
 
-                  val documentRef = documentId.next
-                  documentStmt.setInt(1, documentRef)
-                  documentStmt.setString(2, document.filename)
-                  documentStmt.setString(3, document.contents)
-                  documentStmt.setString(4, document.language)
-                  documentStmt.executeUpdate()
+                    val documentRef = documentId.next
+                    documentStmt.setInt(1, documentRef)
+                    documentStmt.setString(2, document.filename)
+                    documentStmt.setString(3, document.contents)
+                    documentStmt.setString(4, document.language)
+                    documentStmt.executeUpdate()
 
-                  document.names.foreach { name =>
-                    nameStmt.setInt(1, nameId.next)
-                    nameStmt.setInt(2, documentRef)
-                    nameStmt.setInt(3, name.position.get.start)
-                    nameStmt.setInt(4, name.position.get.end)
-                    nameStmt.setInt(5, symbolId(name.symbol))
-                    nameStmt.setBoolean(6, name.isDefinition)
-                    nameStmt.executeUpdate()
-                  }
-
-                  document.messages.foreach { message =>
-                    messageStmt.setInt(1, messageId.next)
-                    messageStmt.setInt(2, documentRef)
-                    messageStmt.setInt(3, message.position.get.start)
-                    messageStmt.setInt(4, message.position.get.end)
-                    messageStmt.setInt(5, message.severity.value)
-                    messageStmt.setString(6, message.text)
-                    messageStmt.executeUpdate()
-                  }
-
-                  document.symbols.foreach { symbol =>
-                    val symbolRef = symbolId(symbol.symbol)
-                    if (!symbolDone.contains(symbolRef)) {
-                      symbolStmt.setInt(1, symbolRef)
-                      symbolStmt.setString(2, symbol.symbol)
-                      symbolStmt.setLong(3, symbol.denotation.get.flags)
-                      symbolStmt.setString(4, symbol.denotation.get.name)
-                      val signatureDocumentRef = {
-                        documentStmt.setInt(1, documentId.next)
-                        documentStmt.setString(2, null)
-                        documentStmt.setString(3, symbol.denotation.get.signature)
-                        documentStmt.setString(4, null)
-                        documentStmt.executeUpdate()
-                        documentId.value
-                      }
-                      symbolStmt.setInt(5, signatureDocumentRef)
-                      symbol.denotation.get.names.foreach { name =>
-                        nameStmt.setInt(1, nameId.next)
-                        nameStmt.setInt(2, signatureDocumentRef)
-                        nameStmt.setInt(3, name.position.get.start)
-                        nameStmt.setInt(4, name.position.get.end)
-                        nameStmt.setInt(5, symbolId(name.symbol))
-                        nameStmt.setBoolean(6, name.isDefinition)
-                        nameStmt.executeUpdate()
-                      }
-                      symbolStmt.executeUpdate()
-                      symbolTodo.remove(symbolRef)
-                      symbolDone.add(symbolRef)
-                    }
-                  }
-
-                  document.synthetics.foreach { synthetic =>
-                    syntheticStmt.setInt(1, syntheticId.next)
-                    syntheticStmt.setInt(2, synthetic.pos.get.start)
-                    syntheticStmt.setInt(3, synthetic.pos.get.end)
-                    val syntheticDocumentRef = {
-                      documentStmt.setInt(1, documentId.next)
-                      documentStmt.setString(2, null)
-                      documentStmt.setString(3, synthetic.text)
-                      documentStmt.setString(4, null)
-                      documentStmt.executeUpdate()
-                      documentId.value
-                    }
-                    syntheticStmt.setInt(4, syntheticDocumentRef)
-                    synthetic.names.foreach { name =>
+                    document.names.foreach { name =>
                       nameStmt.setInt(1, nameId.next)
-                      nameStmt.setInt(2, syntheticDocumentRef)
+                      nameStmt.setInt(2, documentRef)
                       nameStmt.setInt(3, name.position.get.start)
                       nameStmt.setInt(4, name.position.get.end)
                       nameStmt.setInt(5, symbolId(name.symbol))
                       nameStmt.setBoolean(6, name.isDefinition)
                       nameStmt.executeUpdate()
                     }
-                    syntheticStmt.executeUpdate()
-                  }
 
-                  if ((genuineDocuments % 1000) == 0) {
-                    val semanticdbElapsed = System.nanoTime() - semanticdbStart
-                    val buf = new StringBuilder
-                    buf.append(s"$genuineDocuments documents: ")
-                    buf.append(s"${nameId.value} names, ")
-                    buf.append(s"${messageId.value} messages, ")
-                    buf.append(s"${_symbolId.value} symbols, ")
-                    buf.append(s"${syntheticId.value} synthetics")
-                    buf.append(s" (~${semanticdbElapsed / 1000000000}s) ")
-                    println(buf.toString)
-                  }
+                    document.messages.foreach { message =>
+                      messageStmt.setInt(1, messageId.next)
+                      messageStmt.setInt(2, documentRef)
+                      messageStmt.setInt(3, message.position.get.start)
+                      messageStmt.setInt(4, message.position.get.end)
+                      messageStmt.setInt(5, message.severity.value)
+                      messageStmt.setString(6, message.text)
+                      messageStmt.executeUpdate()
+                    }
+
+                    document.symbols.foreach { symbol =>
+                      val symbolRef = symbolId(symbol.symbol)
+                      if (!symbolDone.contains(symbolRef)) {
+                        symbolStmt.setInt(1, symbolRef)
+                        symbolStmt.setString(2, symbol.symbol)
+                        symbolStmt.setLong(3, symbol.denotation.get.flags)
+                        symbolStmt.setString(4, symbol.denotation.get.name)
+                        val signatureDocumentRef = {
+                          documentStmt.setInt(1, documentId.next)
+                          documentStmt.setString(2, null)
+                          documentStmt.setString(3, symbol.denotation.get.signature)
+                          documentStmt.setString(4, null)
+                          documentStmt.executeUpdate()
+                          documentId.value
+                        }
+                        symbolStmt.setInt(5, signatureDocumentRef)
+                        symbol.denotation.get.names.foreach { name =>
+                          nameStmt.setInt(1, nameId.next)
+                          nameStmt.setInt(2, signatureDocumentRef)
+                          nameStmt.setInt(3, name.position.get.start)
+                          nameStmt.setInt(4, name.position.get.end)
+                          nameStmt.setInt(5, symbolId(name.symbol))
+                          nameStmt.setBoolean(6, name.isDefinition)
+                          nameStmt.executeUpdate()
+                        }
+                        symbolStmt.executeUpdate()
+                        symbolTodo.remove(symbolRef)
+                        symbolDone.add(symbolRef)
+                      }
+                    }
+
+                    document.synthetics.foreach { synthetic =>
+                      syntheticStmt.setInt(1, syntheticId.next)
+                      syntheticStmt.setInt(2, synthetic.pos.get.start)
+                      syntheticStmt.setInt(3, synthetic.pos.get.end)
+                      val syntheticDocumentRef = {
+                        documentStmt.setInt(1, documentId.next)
+                        documentStmt.setString(2, null)
+                        documentStmt.setString(3, synthetic.text)
+                        documentStmt.setString(4, null)
+                        documentStmt.executeUpdate()
+                        documentId.value
+                      }
+                      syntheticStmt.setInt(4, syntheticDocumentRef)
+                      synthetic.names.foreach { name =>
+                        nameStmt.setInt(1, nameId.next)
+                        nameStmt.setInt(2, syntheticDocumentRef)
+                        nameStmt.setInt(3, name.position.get.start)
+                        nameStmt.setInt(4, name.position.get.end)
+                        nameStmt.setInt(5, symbolId(name.symbol))
+                        nameStmt.setBoolean(6, name.isDefinition)
+                        nameStmt.executeUpdate()
+                      }
+                      syntheticStmt.executeUpdate()
+                    }
+
+                    if ((genuineDocuments % 1000) == 0) {
+                      val semanticdbElapsed = System.nanoTime() - semanticdbStart
+                      val buf = new StringBuilder
+                      buf.append(s"$genuineDocuments documents: ")
+                      buf.append(s"${nameId.value} names, ")
+                      buf.append(s"${messageId.value} messages, ")
+                      buf.append(s"${_symbolId.value} symbols, ")
+                      buf.append(s"${syntheticId.value} synthetics")
+                      buf.append(s" (~${semanticdbElapsed / 1000000000}s) ")
+                      println(buf.toString)
+                    }
+                }
               }
+            } catch {
+              case NonFatal(ex) =>
+                println(s"Error processing $path")
+                ex.printStackTrace
             }
           }
 
