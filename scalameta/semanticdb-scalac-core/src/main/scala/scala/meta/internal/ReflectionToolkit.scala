b62794ecf64d6391c79af673e5824e4fde129ca5
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-12-10 19:51:30 +0100

    Disable semanticdb-scalac in presentation compiler
    It has taken me close to a full day to track down an infinite loop
    in the pc to this change. Lesson learned, the presentation compiler
    is a fragile beast.

--- scalameta/semanticdb-scalac-core/src/main/scala/scala/meta/internal/ReflectionToolkit.scala
+++ scalameta/semanticdb-scalac-core/src/main/scala/scala/meta/internal/ReflectionToolkit.scala
@@ -16,7 +16,10 @@ trait ReflectionToolkit {
   lazy val isDocCompiler = global.isInstanceOf[ScaladocGlobal]
   lazy val isReplCompiler = global.isInstanceOf[ReplGlobal]
   lazy val isInteractiveCompiler = global.isInstanceOf[InteractiveGlobal]
-  lazy val isSupportedCompiler = !isDocCompiler && !isReplCompiler
+  // NOTE: InteractiveGlobal does not work with semanticdb-scalac, in scalameta/language-server
+  // we tried to enable semanticdb-scalac with the presentation compiler and it resulted
+  // in cryptic infinite while loops while completing scope members.
+  lazy val isSupportedCompiler = !isDocCompiler && !isReplCompiler && !isInteractiveCompiler
 
   // NOTE: this boilerplate is unfortunately necessary, because we don't expose Attachable in the public API
   trait Attachable[-T] {
