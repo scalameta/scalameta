2d973c66d20c140075fccc397e97af9dec7bc5d4
Eugene Burmako <eburmako@twitter.com>
2018-01-18 11:45:40 -0800

    Upgrade to Scala 2.10.7

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ package org.scalameta
 package build
 
 object Versions {
-  val LatestScala210 = "2.10.6"
+  val LatestScala210 = "2.10.7"
   val LatestScala211 = "2.11.12"
   val LatestScala212 = "2.12.4"
 }
