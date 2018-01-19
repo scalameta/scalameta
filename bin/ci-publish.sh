65addba315d9f5f826b80d44502952eca429234a
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-01-19 17:52:49 +0100

    Setup publish -SNAPSHOT on merge to master.
    To make it easier to consume latest fixes without triggering a stable
    release. We should still be unafraid to cut stable releases, but this
    is a nice way to give a pre-release a try before pushing the tag.

--- bin/ci-publish.sh
+++ bin/ci-publish.sh
@@ -3,10 +3,14 @@ set -eu
 PUBLISH=${CI_PUBLISH:-false}
 SECURE_VAR=${TRAVIS_SECURE_ENV_VARS:-false}
 
-if [[ "$TRAVIS_SECURE_ENV_VARS" == true && -n "$TRAVIS_TAG" && "$CI_PUBLISH" == true ]]; then
+if [[ "$TRAVIS_SECURE_ENV_VARS" == true && "$CI_PUBLISH" == true ]]; then
   git log | head -n 20
   echo "$PGP_SECRET" | base64 --decode | gpg --import
-  sbt ci-publish
+  if [ -n "$TRAVIS_TAG" ]; then
+    sbt ci-publish
+  else
+    sbt -Dscalameta.snapshot=true ci-publish
+  fi
 else
   echo "Skipping publish, branch=$TRAVIS_BRANCH publish=$PUBLISH test=$CI_TEST"
 fi
