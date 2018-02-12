c4785b9c105674fe5fc29f889839a2868e27d01d
Eugene Burmako <eburmako@twitter.com>
2018-02-12 06:04:18 -0800

    Address feedback

--- semanticdb/metacp/src/main/scala/scala/meta/cli/Metacp.scala
+++ semanticdb/metacp/src/main/scala/scala/meta/cli/Metacp.scala
@@ -4,13 +4,10 @@ import scala.meta.internal.metacp._
 
 object Metacp {
   def main(args: Array[String]): Unit = {
-    Settings.parse(args.toList) match {
-      case Some(settings) => sys.exit(process(settings))
-      case None => sys.exit(1)
-    }
+    sys.exit(process(args))
   }
 
-  def process(settings: Settings): Int = {
-    Main.process(settings)
+  def process(args: Array[String]): Int = {
+    Main.process(args)
   }
 }
