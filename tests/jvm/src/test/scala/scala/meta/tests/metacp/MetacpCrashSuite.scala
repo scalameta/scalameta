3ed8cdc5b0ff970c72e6762a8ac0155051f203b4
Eugene Burmako <eburmako@twitter.com>
2018-03-06 17:07:55 -0800

    Remove a potentially staleable comment

--- tests/jvm/src/test/scala/scala/meta/tests/metacp/MetacpCrashSuite.scala
+++ tests/jvm/src/test/scala/scala/meta/tests/metacp/MetacpCrashSuite.scala
@@ -1,7 +1,6 @@
 package scala.meta.tests.metacp
 
-// TODO(olafur): move to slow/integration tests. Each run takes ~20s on my laptop
-// and produces ~60mb of artifacts in a temporary directory.
+// TODO(olafur): move to slow/integration tests.
 class MetacpCrashSuite extends BaseMetacpSuite {
 
   checkMetacp("scala-library", () => scalaLibraryClasspath)
