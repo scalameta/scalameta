2db2218f1701febe2e29e8f437c9ae6f3c7f81e5
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-04-06 19:15:42 +0200

    Preserve URI encoding when reading path.
    ```scala
    val space = Paths.get("a b")
    space.toUri.getPath // 'a b'
    space.toUri         // 'a%20b'
    ```

---
+++ tests/jvm/src/test/scala/scala/meta/tests/metacp/VisitClasspathSuite.scala
@@ -0,0 +1,11 @@
+package scala.meta.tests.metacp
+
+import java.nio.file.FileVisitResult
+import java.nio.file.Path
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.attribute.BasicFileAttributes
+
+class VisitClasspathSuite extends BaseMetacpSuite {
+
+
+}
