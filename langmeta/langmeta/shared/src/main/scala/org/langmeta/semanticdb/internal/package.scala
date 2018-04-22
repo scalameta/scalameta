48a0b10669e8db6b8d18ab2404ab7d65334d1ba5
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-04-22 12:38:38 +0200

    Fix typo in deprecation message

--- langmeta/langmeta/shared/src/main/scala/org/langmeta/semanticdb/internal/package.scala
+++ langmeta/langmeta/shared/src/main/scala/org/langmeta/semanticdb/internal/package.scala
@@ -8,7 +8,7 @@ import scala.meta.internal.{semanticdb3 => s}
 
 package object semanticdb {
   final val DeprecationMessage =
-    "This API is no longer suppported. Use scala.meta.internal.semanticdb3._ instead, but note that this API is subject to breaking changes without notice."
+    "This API is no longer supported. Use scala.meta.internal.semanticdb3._ instead, but note that this API is subject to binary and source breaking changes without notice."
   implicit class XtensionSchemaTextDocuments(sdocuments: s.TextDocuments) {
     def toVfs(targetroot: AbsolutePath): v.Database = {
       val ventries = sdocuments.documents.toIterator.map { sentry =>
