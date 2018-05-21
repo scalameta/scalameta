82b91d3c326dc4e815d22b89e09648029a98986c
Loïc Girault <lgirault@mediarithmics.com>
2018-05-21 22:22:21 +0200

    Relax RelevantMessage condition

--- semanticdb/scalac/library/src/main/scala/scala/meta/internal/semanticdb/scalac/ReporterOps.scala
+++ semanticdb/scalac/library/src/main/scala/scala/meta/internal/semanticdb/scalac/ReporterOps.scala
@@ -14,7 +14,6 @@ trait ReporterOps { self: SemanticdbOps =>
         case r: StoreReporter =>
           object RelevantMessage {
             def unapply(info: r.Info): Option[(gPosition, Int, String)] = {
-              if (!info.pos.isRange) return None
               if (info.pos.source != unit.source) return None
               Some((info.pos, info.severity.id, info.msg))
             }
