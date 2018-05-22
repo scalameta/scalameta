4df11f6d4dc711cc7d95ea919a54e604428d4479
Loïc Girault <lgirault@mediarithmics.com>
2018-05-22 18:39:27 +0200

    Make different tests for 2.11 and 2.12

--- tests/jvm/src/test/scala/scala/meta/tests/semanticdb/TargetedSuite.scala
+++ tests/jvm/src/test/scala/scala/meta/tests/semanticdb/TargetedSuite.scala
@@ -513,29 +513,6 @@ class TargetedSuite extends SemanticdbSuite() {
     """.stripMargin.trim
   )
 
-  diagnostics(
-    """
-      |package l2
-      |class A {
-      |  private val a = 1
-      |}
-      |object B {
-      |  private val a = 1
-      |}
-      |package object C {
-      |  private val a = 1
-      |}
-      |trait D {
-      |  private val a = 1
-      |}
-    """.stripMargin.trim,
-    """|[2:14..2:14)[warning] private val a in class A is never used
-       |[5:14..5:14)[warning] private val a in object B is never used
-       |[8:14..8:14)[warning] private val a in package object C is never used
-       |[11:2..11:19)[warning] private val a in trait D is never used
-    """.stripMargin.trim
-  )
-
   occurrences(
     s"""
        |package m
