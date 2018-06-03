430494095d6a70b4411457445fc685cac42dd962
Eugene Burmako <eburmako@twitter.com>
2018-06-03 12:56:21 -0700

    Refactor metap to avoid passing doc everywhere
    This not only makes the code simpler, but also avoids the need to
    maintain weak hashmaps (which was, frankly speaking, insane).
    
    The only place where getting rid of explicit doc passing was annoying
    is prettyprinting of ranges, however I figured that out by modularizing
    range printing into smaller components.

--- semanticdb/metap/src/main/scala/scala/meta/internal/semanticdb3/Print.scala
+++ semanticdb/metap/src/main/scala/scala/meta/internal/semanticdb3/Print.scala
@@ -3,15 +3,16 @@ package scala.meta.internal.semanticdb3
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 import scala.meta.cli._
-import scala.meta.internal.metap.Main
+import scala.meta.internal.metap.Printer
 import scala.meta.metap.Settings
 
 object Print {
 
   def document(doc: TextDocument): String = {
     val baos = new ByteArrayOutputStream()
-    val main = new Main(Settings(), Reporter().withOut(new PrintStream(baos)))
-    main.pprint(doc)
+    val ps = new PrintStream(baos)
+    val printer = new Printer(ps, doc)
+    printer.print()
     baos.toString()
   }
 
