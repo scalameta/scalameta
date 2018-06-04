16093da483769b02398cda4bfbb2bf404a0ca30e
Eugene Burmako <eburmako@twitter.com>
2018-06-03 21:02:13 -0700

    This part of NameOps is no longer necessary
    I remember deleting this bit yesterday, but I have probably messed up
    some rebase, so it came back.

--- semanticdb/scalac/library/src/main/scala/scala/meta/internal/semanticdb/scalac/NameOps.scala
+++ semanticdb/scalac/library/src/main/scala/scala/meta/internal/semanticdb/scalac/NameOps.scala
@@ -13,13 +13,6 @@ trait NameOps { self: SemanticdbOps =>
         n.EmptyPackage
       } else if (gname == g.nme.CONSTRUCTOR) {
         n.Constructor
-      } else if (gname == g.tpnme.REFINE_CLASS_NAME) {
-        // See https://github.com/scalameta/scalameta/pull/1109#discussion_r137194314
-        // for a motivation why <refinement> symbols should have $anon as names.
-        // This may be the wrong encoding of the symbol, but with the current
-        // implementation it makes the use-site symbols of this refinement
-        // decl match with the definition-site of the refinement decl.
-        g.nme.ANON_CLASS_NAME.decoded
       } else {
         gname.decoded.stripSuffix(g.nme.LOCAL_SUFFIX_STRING)
       }
