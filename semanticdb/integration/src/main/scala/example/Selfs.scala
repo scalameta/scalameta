1075b3996d58d06f29fbf4d020de65dd52ce028d
Eugene Burmako <eburmako@twitter.com>
2018-06-21 08:32:24 -0400

    More tests for selfs

--- semanticdb/integration/src/main/scala/example/Selfs.scala
+++ semanticdb/integration/src/main/scala/example/Selfs.scala
@@ -10,3 +10,12 @@ class C2 extends B { self: B =>
 
 class C3 extends B { self: B with Int =>
 }
+
+class C4 extends B { _ =>
+}
+
+class C5 extends B { _: B =>
+}
+
+class C6 extends B { this: B =>
+}
