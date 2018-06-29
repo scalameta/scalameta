a469479fca76ace2797ab64e68adb7b835afbc0f
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-06-29 10:20:24 +0200

    Undo config style formatting

--- semanticdb/metacp/src/main/scala/scala/meta/internal/metacp/ToplevelClassfile.scala
+++ semanticdb/metacp/src/main/scala/scala/meta/internal/metacp/ToplevelClassfile.scala
@@ -4,10 +4,6 @@ import scala.meta.internal.io._
 import scala.meta.io._
 import scala.tools.asm.tree._
 
-final case class ToplevelClassfile(
-    base: AbsolutePath,
-    path: AbsolutePath,
-    node: ClassNode
-) {
+final case class ToplevelClassfile(base: AbsolutePath, path: AbsolutePath, node: ClassNode) {
   def uri: String = PathIO.toUnix(path.toRelative(base).toString)
 }
