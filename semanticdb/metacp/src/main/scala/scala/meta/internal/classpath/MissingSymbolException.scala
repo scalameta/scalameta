8b21db9a566aa22b1d0c45d2e3574fb08857f8e7
Eugene Burmako <eburmako@twitter.com>
2018-07-20 15:59:02 -0700

    Drop NoStackTrace from MissingSymbolException

--- semanticdb/metacp/src/main/scala/scala/meta/internal/classpath/MissingSymbolException.scala
+++ semanticdb/metacp/src/main/scala/scala/meta/internal/classpath/MissingSymbolException.scala
@@ -1,7 +1,5 @@
 package scala.meta.internal.classpath
 
-import scala.util.control.NoStackTrace
-
-final case class MissingSymbolException(symbol: String) extends Exception with NoStackTrace {
+final case class MissingSymbolException(symbol: String) extends Exception {
   override def getMessage: String = s"missing symbol: $symbol"
 }
