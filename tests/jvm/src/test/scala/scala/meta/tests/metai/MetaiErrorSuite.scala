222bcbfd13d6416abb5f7ce96289c4b734cb550c
Eugene Burmako <eburmako@twitter.com>
2018-07-29 23:27:47 -0700

    Fix AppVeyor failures

--- tests/jvm/src/test/scala/scala/meta/tests/metai/MetaiErrorSuite.scala
+++ tests/jvm/src/test/scala/scala/meta/tests/metai/MetaiErrorSuite.scala
@@ -8,11 +8,12 @@ import scala.meta.internal.io.FileIO
 import scala.meta.io.AbsolutePath
 import scala.meta.io.Classpath
 import scala.meta.metai.Settings
+import scala.meta.testkit.DiffAssertions
 import scala.meta.tests.BuildInfo
 import scala.meta.tests.cli.CliSuite
 import scala.meta.tests.metacp.Library
 
-class MetaiErrorSuite extends FunSuite {
+class MetaiErrorSuite extends FunSuite with DiffAssertions {
   test("no META-INF/semanticdb should error") {
     val scalaLibrary = Library.scalaLibrary.classpath().entries.head
     val tmp1 = Files.createTempFile("metai", "_scala-library.jar")
@@ -36,8 +37,8 @@ class MetaiErrorSuite extends FunSuite {
     val expectedStderr =
       s"""|No META-INF/semanticdb found in $tmp1
           |""".stripMargin
-    assert(out == expectedStdout, out)
-    assert(err == expectedStderr, err)
+    assertNoDiffOrPrintExpected(out, expectedStdout)
+    assertNoDiffOrPrintExpected(err, expectedStderr)
     FileIO.withJarFileSystem(AbsolutePath(tmp1), create = false, close = true) { root =>
       val semanticidx1 = root.resolve("META-INF/semanticdb.semanticidx")
       assert(!semanticidx1.isFile, semanticidx1)
