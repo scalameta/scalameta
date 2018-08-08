3428d76e3fc65bb08e04b0bf4238b7f8341179be
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-08-08 14:19:56 +0200

    Follow LSP conventions for line/character positions (fixes #1728)
    The previous implementation was not consistent with LSP on how columns
    that exceeded the maximum line length were treated. This commit makes
    the implementation consistent with LSP in that `column=Int.MaxValue`
    is treated as the highest possible column value for that line. This
    makes it easy to manually construct positions that cover a full line.

--- tests/shared/src/test/scala/scala/meta/tests/parsers/PositionSuite.scala
+++ tests/shared/src/test/scala/scala/meta/tests/parsers/PositionSuite.scala
@@ -62,4 +62,16 @@ class PositionSuite extends ParseSuite {
         | List(Term.Name{8..9}("c")))
     """.trim.stripMargin.split("\n").mkString)
   }
+
+  test("apply(startLine,startColumn,endLine,endColumn)") {
+    val input = Input.String(
+      """|  val x = 2 // line 0
+         |            // line 1""".stripMargin
+    )
+    val x = Position.Range(input, 0, 2, 0, 11)
+    assert(x.text == "val x = 2")
+    val last = Position.Range(input, 1, 0, 1, Int.MaxValue)
+    assert(last.text == "            // line 1")
+  }
+
 }
