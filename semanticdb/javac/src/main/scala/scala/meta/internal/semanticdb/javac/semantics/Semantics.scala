3cca3fc5c1e677cb7082bb5e45e11c4f0e04880e
Alexander Myltsev <alexander@myltsev.com>
2018-12-20 17:19:16 +0300

    Minor code cleanup: remove unused values and `not-implemented` exceptions

--- semanticdb/javac/src/main/scala/scala/meta/internal/semanticdb/javac/semantics/Semantics.scala
+++ semanticdb/javac/src/main/scala/scala/meta/internal/semanticdb/javac/semantics/Semantics.scala
@@ -9,60 +9,60 @@ import scala.compat.java8.OptionConverters._
 import scala.meta.internal.semanticdb.Scala.{Descriptor => d, _}
 
 trait Semantics extends Elements with TypeMirrors {
-  type SymbolsTable = mutable.Map[String, Option[jp.Range]]
-  def emptySymbolsTable: SymbolsTable =
+  type SymbolTable = mutable.Map[String, Option[jp.Range]]
+  def emptySymbolTable: SymbolTable =
     mutable.Map.empty[String, Option[jp.Range]]
 
-  class SymbolTableGenerator extends jp.ast.visitor.VoidVisitorAdapter[SymbolsTable] {
-    override def visit(n: jp.ast.PackageDeclaration, arg: SymbolsTable): Unit = {
+  class SymbolTableGenerator extends jp.ast.visitor.VoidVisitorAdapter[SymbolTable] {
+    override def visit(n: jp.ast.PackageDeclaration, arg: SymbolTable): Unit = {
       super.visit(n, arg)
       val range = n.getName.getRange
       arg += n.sym -> range.asScala
     }
 
-    override def visit(vd: jp.ast.body.VariableDeclarator, arg: SymbolsTable): Unit = {
+    override def visit(vd: jp.ast.body.VariableDeclarator, arg: SymbolTable): Unit = {
       super.visit(vd, arg)
       val range = vd.getName.getRange
       arg += vd.sym -> range.asScala
     }
 
-    override def visit(cid: jp.ast.body.ClassOrInterfaceDeclaration, arg: SymbolsTable): Unit = {
+    override def visit(cid: jp.ast.body.ClassOrInterfaceDeclaration, arg: SymbolTable): Unit = {
       super.visit(cid, arg)
       val range = cid.getName.getRange
       arg += cid.sym -> range.asScala
     }
 
-    override def visit(p: jp.ast.body.Parameter, arg: SymbolsTable): Unit = {
+    override def visit(p: jp.ast.body.Parameter, arg: SymbolTable): Unit = {
       super.visit(p, arg)
       val range = p.getName.getRange
       arg += p.sym -> range.asScala
     }
 
-    override def visit(md: jp.ast.body.MethodDeclaration, arg: SymbolsTable): Unit = {
+    override def visit(md: jp.ast.body.MethodDeclaration, arg: SymbolTable): Unit = {
       super.visit(md, arg)
       val range = md.getName.getRange
       arg += md.sym -> range.asScala
     }
 
-    override def visit(ad: jp.ast.body.AnnotationDeclaration, arg: SymbolsTable): Unit = {
+    override def visit(ad: jp.ast.body.AnnotationDeclaration, arg: SymbolTable): Unit = {
       super.visit(ad, arg)
       val range = ad.getName.getRange
       arg += ad.sym -> range.asScala
     }
 
-    override def visit(tp: jp.ast.`type`.TypeParameter, arg: SymbolsTable): Unit = {
+    override def visit(tp: jp.ast.`type`.TypeParameter, arg: SymbolTable): Unit = {
       super.visit(tp, arg)
       val range = tp.getName.getRange
       arg += tp.sym -> range.asScala
     }
 
-    override def visit(ecd: jp.ast.body.EnumConstantDeclaration, arg: SymbolsTable): Unit = {
+    override def visit(ecd: jp.ast.body.EnumConstantDeclaration, arg: SymbolTable): Unit = {
       super.visit(ecd, arg)
       val range = ecd.getName.getRange
       arg += ecd.sym -> range.asScala
     }
 
-    override def visit(ed: jp.ast.body.EnumDeclaration, arg: SymbolsTable): Unit = {
+    override def visit(ed: jp.ast.body.EnumDeclaration, arg: SymbolTable): Unit = {
       super.visit(ed, arg)
 
       // add synthetic `values` method
@@ -102,25 +102,23 @@ trait Semantics extends Elements with TypeMirrors {
             case _ =>
               parent.owner
           }
-        case _ => ???
+        case _ =>
+          throw new RuntimeException("Unexpected kind of node. Please, submit the issue")
       }
     }
 
     def symbolName: String = node match {
-      case elem: jp.ast.body.ConstructorDeclaration =>
+      case _: jp.ast.body.ConstructorDeclaration =>
         "<init>"
-
       case elem: jp.ast.nodeTypes.NodeWithSimpleName[_] =>
         elem.getNameAsString
-
-      case elem =>
-        ???
+      case _ =>
+        throw new RuntimeException("Unexpected kind of node. Please, submit the issue")
     }
 
     def sym: String = node match {
       case elem: jp.ast.CompilationUnit =>
         elem.getPackageDeclaration.asScala.map { _.sym }.getOrElse("")
-
       case elem: jp.ast.PackageDeclaration =>
         val qualName = elem.getName.asString
         if (qualName == "") {
@@ -128,16 +126,12 @@ trait Semantics extends Elements with TypeMirrors {
         } else {
           qualName.replace('.', '/') + "/"
         }
-
-      case elem: jp.ast.body.TypeDeclaration[_] =>
+      case _: jp.ast.body.TypeDeclaration[_] =>
         Symbols.Global(owner, d.Type(symbolName))
-
-      case elem: jp.ast.stmt.LocalClassDeclarationStmt =>
+      case _: jp.ast.stmt.LocalClassDeclarationStmt =>
         ""
-
-      case elem: jp.ast.body.Parameter =>
+      case _: jp.ast.body.Parameter =>
         Symbols.Global(owner, d.Parameter(symbolName))
-
       case elem: jp.ast.body.CallableDeclaration[_] =>
         elem.getParentNode.asScala match {
           case Some(owner: jp.ast.body.TypeDeclaration[_]) =>
@@ -160,42 +154,37 @@ trait Semantics extends Elements with TypeMirrors {
               }
               val methodPlace = siblingMethods.indexOf(elem)
               (methodPlace: @switch) match {
-                case -1 => ???
+                case -1 =>
+                  throw new RuntimeException("Unexpected state. Please, submit the issue")
                 case 0 => "()"
                 case x => s"(+$x)"
               }
             }
             Symbols.Global(owner.sym, d.Method(symbolName, disambig))
-
-          case Some(owner: jp.ast.expr.ObjectCreationExpr) =>
+          case Some(_: jp.ast.expr.ObjectCreationExpr) =>
             ""
-
-          case x =>
-            ???
+          case _ =>
+            throw new RuntimeException("Unexpected kind of node. Please, submit the issue")
         }
-
-      case elem: jp.ast.body.EnumConstantDeclaration =>
+      case _: jp.ast.body.EnumConstantDeclaration =>
         Symbols.Global(owner, d.Term(symbolName))
-
-      case elem: jp.ast.body.VariableDeclarator =>
+      case _: jp.ast.body.VariableDeclarator =>
         val sn = symbolName
         Symbols.Global(owner, d.Term(sn))
-
-      case elem: jp.ast.`type`.TypeParameter =>
+      case _: jp.ast.`type`.TypeParameter =>
         Symbols.Global(owner, d.TypeParameter(symbolName))
-
-      case x =>
-        ???
+      case _ =>
+        throw new RuntimeException("Unexpected kind of node. Please, submit the issue")
     }
   }
 
   val sourceFile: JavaFileObject
 
-  val symbolTable: SymbolsTable = {
+  val symbolTable: SymbolTable = {
     val content = sourceFile.getCharContent(true).toString
     val cu = jp.JavaParser.parse(content)
     val stg = new SymbolTableGenerator()
-    val symbolTable = emptySymbolsTable
+    val symbolTable = emptySymbolTable
     stg.visit(cu, symbolTable)
     symbolTable
   }
