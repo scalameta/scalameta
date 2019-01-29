a9f2e28a2a13fdfbbcdba0d0bec2fa29c179ddad
Ólafur Páll Geirsson <olafurpg@gmail.com>
2019-01-29 20:53:11 +0100

    Add one more sanity test for for comprehensions.
    Everything looks good!

--- tests/jvm/src/test/resources/example/ForComprehension.scala
+++ tests/jvm/src/test/resources/example/ForComprehension.scala
@@ -7,4 +7,39 @@ class ForComprehension/*example.ForComprehension#*/ {
     if b/*local1*/ >/*scala.Int#`>`(+3).*/ 1
     c/*local3*/ = a/*local0*/ +/*scala.Int#`+`(+4).*/ b/*local1*/
   } yield (a/*local0*/, b/*local1*/, c/*local3*/)
+  for {
+    a/*local4*/ <- List/*scala.collection.immutable.List.*/(1)
+    b/*local5*/ <- List/*scala.collection.immutable.List.*/(a/*local4*/)
+    if (
+      a/*local4*/,
+      b/*local5*/
+    ) ==/*java.lang.Object#`==`().*/ (1, 2)
+    (
+      c/*local11*/,
+      d/*local12*/
+      ) <- List/*scala.collection.immutable.List.*/((a/*local4*/, b/*local5*/))
+    if (
+      a/*local4*/,
+      b/*local5*/,
+      c/*local11*/,
+      d/*local12*/
+    ) ==/*java.lang.Object#`==`().*/ (1, 2, 3, 4)
+    e/*local14*/ = (
+      a/*local4*/,
+      b/*local5*/,
+      c/*local11*/,
+      d/*local12*/
+    )
+    if e/*local14*/ ==/*java.lang.Object#`==`().*/ (1, 2, 3, 4)
+    f/*local15*/ <- List/*scala.collection.immutable.List.*/(e/*local14*/)
+  } yield {
+    (
+      a/*local4*/,
+      b/*local5*/,
+      c/*local11*/,
+      d/*local12*/,
+      e/*local14*/,
+      f/*local15*/
+    )
+  }
 }
