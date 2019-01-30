fc4ed724663f6a0b47a8a77a985accd392c1775d
Ólafur Páll Geirsson <olafurpg@gmail.com>
2019-01-30 18:26:43 +0100

    Fail to reproduce #1774.

---
+++ tests/jvm/src/test/resources/example/Local.scala
@@ -0,0 +1,8 @@
+package example
+
+class Local/*<=example.Local#*/ {
+  def a/*<=example.Local#a().*/() = {
+    def id/*<=local0*/[A/*<=local2*/](a/*<=local1*/: A/*=>local2*/): A/*=>local2*/ = a/*=>local1*/
+    id/*=>local0*/(1)
+  }
+}
