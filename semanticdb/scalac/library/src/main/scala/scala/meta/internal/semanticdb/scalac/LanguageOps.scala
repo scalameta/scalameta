d4abc9b86c7fcc34e4da888eb4fcb35f6e224bc9
Tim Nieradzik <tim@sparse.tech>
2019-03-18 23:33:43 +0100

    LanguageOps: Detect Typelevel compiler
    This allows to use Typelevel-specific features like literal types.

--- semanticdb/scalac/library/src/main/scala/scala/meta/internal/semanticdb/scalac/LanguageOps.scala
+++ semanticdb/scalac/library/src/main/scala/scala/meta/internal/semanticdb/scalac/LanguageOps.scala
@@ -8,8 +8,12 @@ trait LanguageOps { self: SemanticdbOps =>
   lazy val language: String = {
     val version = Properties.versionNumberString
     if (version.startsWith("2.10")) "Scala210"
-    else if (version.startsWith("2.11")) "Scala211"
-    else if (version.startsWith("2.12")) "Scala212"
-    else sys.error(s"unsupported Scala version $version")
+    else if (version.startsWith("2.11")) {
+      if (!version.endsWith("-bin-typelevel-4")) "Scala211"
+      else "Typelevel211"
+    } else if (version.startsWith("2.12")) {
+      if (!version.endsWith("-bin-typelevel-4")) "Scala212"
+      else "Typelevel212"
+    } else sys.error(s"unsupported Scala version $version")
   }
 }
