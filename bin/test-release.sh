76bf36aefc4d77e531c432948f35f60f498bb268
Ólafur Páll Geirsson <olafurpg@gmail.com>
2019-04-26 08:42:12 +0200

    Update test-release script

--- bin/test-release.sh
+++ bin/test-release.sh
@@ -11,8 +11,27 @@ coursier fetch \
   org.scalameta:scalameta_sjs0.6_2.11:$version \
   org.scalameta:metac_2.12.8:$version \
   org.scalameta:metac_2.11.12:$version \
+  org.scalameta:semanticdb-scalac-core_2.12.8:$version \
   org.scalameta:semanticdb-scalac-core_2.12.7:$version \
   org.scalameta:semanticdb-scalac-core_2.11.12:$version \
   org.scalameta:semanticdb-scalac_2.12.8:$version \
+  org.scalameta:semanticdb-scalac_2.12.7:$version \
   org.scalameta:semanticdb-scalac_2.11.12:$version \
+  -r sonatype:public
+
+# These artifacts were published manually, they are not published by the CI.
+# These artfacts will be dropped in the next release.
+coursier fetch \
+  org.scalameta:semanticdb-scalac-core_2.12.6:$version \
+  org.scalameta:semanticdb-scalac-core_2.12.5:$version \
+  org.scalameta:semanticdb-scalac-core_2.12.4:$version \
+  org.scalameta:semanticdb-scalac-core_2.11.11:$version \
+  org.scalameta:semanticdb-scalac-core_2.11.10:$version \
+  org.scalameta:semanticdb-scalac-core_2.11.9:$version \
+  org.scalameta:semanticdb-scalac_2.12.6:$version \
+  org.scalameta:semanticdb-scalac_2.12.5:$version \
+  org.scalameta:semanticdb-scalac_2.12.4:$version \
+  org.scalameta:semanticdb-scalac_2.11.11:$version \
+  org.scalameta:semanticdb-scalac_2.11.10:$version \
+  org.scalameta:semanticdb-scalac_2.11.9:$version \
   -r sonatype:public
