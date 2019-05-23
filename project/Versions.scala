cfa073fd9d3b6b385631b10ceb7d67677f385676
Ólafur Páll Geirsson <olafurpg@gmail.com>
2019-05-23 08:26:00 +0200

    Remove 2.12.7 from the build.
    Let's see if this reduces release times so we don't timeout on Travis.

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,7 +6,5 @@ object Versions {
   val LatestScala211 = "2.11.12"
   val LatestScala212 = "2.12.8"
   val LatestScala213 = "2.13.0-RC1"
-  val LegacyScalaVersions = List(
-    "2.12.7"
-  )
+  val LegacyScalaVersions = List[String]()
 }
