2f1e7435330c272082d29449ed58c0f59bc0c62c
Gabriele Petronella <gabriele@buildo.io>
2019-06-14 21:23:07 +0200

    Cross build for Scala 2.13

--- project/Versions.scala
+++ project/Versions.scala
@@ -5,6 +5,6 @@ object Versions {
   val LatestScala210 = "2.10.7"
   val LatestScala211 = "2.11.12"
   val LatestScala212 = "2.12.8"
-  val LatestScala213 = "2.13.0-RC1"
+  val LatestScala213 = "2.13.0"
   val LegacyScalaVersions = List[String]()
 }
