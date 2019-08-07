06fb5359b477cd51d96daa23cdf2d039567effac
Gabriele Petronella <gabriele@buildo.io>
2019-08-07 14:42:57 +0200

    Remove unused scala 2.10 version

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,6 @@ package org.scalameta
 package build
 
 object Versions {
-  val LatestScala210 = "2.10.7"
   val LatestScala211 = "2.11.12"
   val LatestScala212 = "2.12.9"
   val LatestScala213 = "2.13.0"
