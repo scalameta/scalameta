7fd6720a6ac90f2ec84fa79b71a78a033538b044
Gabriele Petronella <gabriele@buildo.io>
2019-08-07 14:41:06 +0200

    Add support for 2.12.9

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ package build
 object Versions {
   val LatestScala210 = "2.10.7"
   val LatestScala211 = "2.11.12"
-  val LatestScala212 = "2.12.8"
+  val LatestScala212 = "2.12.9"
   val LatestScala213 = "2.13.0"
-  val LegacyScalaVersions = List[String]()
+  val LegacyScalaVersions = List("2.12.8")
 }
