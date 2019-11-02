7c5c020cc0ff9b97d9029b315fb8659590236a2c
Olafur Pall Geirsson <lgeirsson@twitter.com>
2019-11-02 11:10:11 +0000

    Update test-release.sh script

--- bin/test-release.sh
+++ bin/test-release.sh
@@ -4,15 +4,20 @@ set -eux
 version=$1
 
 coursier fetch \
+  org.scalameta:scalameta_2.13:$version \
   org.scalameta:scalameta_2.12:$version \
   org.scalameta:scalameta_2.11:$version \
+  org.scalameta:scalameta_sjs0.6_2.13:$version \
   org.scalameta:scalameta_sjs0.6_2.12:$version \
   org.scalameta:scalameta_sjs0.6_2.11:$version \
-  org.scalameta:metac_2.12.9:$version \
+  org.scalameta:metac_2.13.1:$version \
+  org.scalameta:metac_2.12.10:$version \
   org.scalameta:metac_2.11.12:$version \
-  org.scalameta:semanticdb-scalac-core_2.12.9:$version \
+  org.scalameta:semanticdb-scalac-core_2.13.1:$version \
+  org.scalameta:semanticdb-scalac-core_2.12.10:$version \
   org.scalameta:semanticdb-scalac-core_2.11.12:$version \
-  org.scalameta:semanticdb-scalac_2.12.9:$version \
+  org.scalameta:semanticdb-scalac_2.13.1:$version \
+  org.scalameta:semanticdb-scalac_2.12.10:$version \
   org.scalameta:semanticdb-scalac_2.11.12:$version \
   -r sonatype:public
 
@@ -22,8 +27,10 @@ coursier fetch \
 # These artifacts were published manually, they are not published by the CI.
 # These artfacts will be dropped in the next release.
 coursier fetch \
-  org.scalameta:semanticdb-scalac-core_2.12.7:$version \
-  org.scalameta:semanticdb-scalac_2.12.7:$version \
+  org.scalameta:semanticdb-scalac-core_2.13.0:$version \
+  org.scalameta:semanticdb-scalac-core_2.12.9:$version \
   org.scalameta:semanticdb-scalac-core_2.12.8:$version \
+  org.scalameta:semanticdb-scalac_2.13.1:$version \
+  org.scalameta:semanticdb-scalac_2.12.9:$version \
   org.scalameta:semanticdb-scalac_2.12.8:$version \
   -r sonatype:public
