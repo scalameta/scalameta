2c37f756711989b71f46a5e6bc677703539631e4
Scala Steward <me@scala-steward.org>
2019-12-08 13:05:06 +0100

    Update scalap to 2.13.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ package build
 
 object Versions {
   val LatestScala211 = "2.11.12"
-  val LatestScala212 = "2.12.10"
+  val LatestScala212 = "2.13.1"
   val LatestScala213 = "2.13.1"
   val LegacyScalaVersions = List("2.12.8", "2.12.9", "2.13.0")
 }
