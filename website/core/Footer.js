9e562dab3afa2a0b2f5b7d738b45ea5a515f6aa1
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-04-18 11:20:57 +0000

    List Scalameta projects in the scalameta.org footer

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -7,7 +7,7 @@ class Footer extends React.Component {
     const currentYear = new Date().getFullYear();
     const {
       copyright,
-      colors: { secondaryColor }
+      colors: { secondaryColor },
     } = this.props.config;
     return (
       <footer
@@ -19,9 +19,7 @@ class Footer extends React.Component {
           {this.props.config.footerIcon && (
             <a href={this.props.config.baseUrl} className="nav-home">
               <img
-                src={`${this.props.config.baseUrl}${
-                  this.props.config.footerIcon
-                }`}
+                src={`${this.props.config.baseUrl}${this.props.config.footerIcon}`}
                 alt={this.props.config.title}
                 width="66"
                 height="58"
@@ -29,7 +27,7 @@ class Footer extends React.Component {
             </a>
           )}
           <div>
-            <h5>Docs</h5>
+            <h5>Scalameta Docs</h5>
             <a href={` ${this.props.config.baseUrl}docs/trees/guide.html`}>
               Trees Guide
             </a>
@@ -39,23 +37,42 @@ class Footer extends React.Component {
               Quasiquotes
             </a>
             <a
-              href={` ${
-                this.props.config.baseUrl
-              }docs/semanticdb/specification.html`}
+              href={` ${this.props.config.baseUrl}docs/semanticdb/specification.html`}
             >
               SemanticDB
             </a>
           </div>
           <div>
-            <h5>Community</h5>
-            <a href={siteConfig.gitterUrl} target="_blank">
-              Chat on Gitter
+            <h5>Scalameta Projects</h5>
+            <a href="https://scalameta.org/metals">Metals: language server</a>
+            <a href="https://scalameta.org/scalafmt">
+              Scalafmt: code formatter
+            </a>
+            <a href="https://scalacenter.github.io/scalafix">
+              Scalafix: linting and refactoring tool
+            </a>
+            <a href="https://scalameta.org/munit">MUnit: testing library</a>
+            <a href="https://scalameta.org/mdoc">MDoc: documentation tool</a>
+            <a href="https://github.com/scalameta/metabrowse">
+              Metabrowse: online code browser
             </a>
           </div>
           <div>
-            <h5>More</h5>
+            <h5>Community</h5>
             <a href={siteConfig.repoUrl} target="_blank">
               GitHub
+            </a>
+            <a href="https://github.com/scalameta/scalameta" target="_blank">
+              <img src="https://img.shields.io/github/stars/scalameta/metals.svg?color=%23087e8b&label=stars&logo=github&style=social" />
+            </a>
+            <a href="https://discord.gg/RFpSVth" target="_blank">
+              <img src="https://img.shields.io/discord/632642981228314653?logo=discord&style=social" />
+            </a>
+            <a href="https://gitter.im/scalameta/scalameta" target="_blank">
+              <img src="https://img.shields.io/gitter/room/scalameta/scalameta.svg?logo=gitter&style=social" />
+            </a>
+            <a href="https://twitter.com/scalameta" target="_blank">
+              <img src="https://img.shields.io/twitter/follow/scalameta.svg?logo=twitter&style=social" />
             </a>
           </div>
         </section>
