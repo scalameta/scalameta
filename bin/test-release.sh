a48a34159230299c5efd8eb7e54652c8b4e537b6
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-05-02 17:17:29 +0000

    Update test-release.sh script to latest versions

--- bin/test-release.sh
+++ bin/test-release.sh
@@ -10,29 +10,15 @@ coursier resolve \
   org.scalameta:scalameta_sjs0.6_2.13:$version \
   org.scalameta:scalameta_sjs0.6_2.12:$version \
   org.scalameta:scalameta_sjs0.6_2.11:$version \
-  org.scalameta:metac_2.13.1:$version \
+  org.scalameta:metac_2.13.2:$version \
   org.scalameta:metac_2.12.11:$version \
   org.scalameta:metac_2.11.12:$version \
+  org.scalameta:semanticdb-scalac-core_2.13.2:$version \
   org.scalameta:semanticdb-scalac-core_2.13.1:$version \
   org.scalameta:semanticdb-scalac-core_2.12.11:$version \
   org.scalameta:semanticdb-scalac-core_2.11.12:$version \
+  org.scalameta:semanticdb-scalac_2.13.2:$version \
   org.scalameta:semanticdb-scalac_2.13.1:$version \
   org.scalameta:semanticdb-scalac_2.12.11:$version \
   org.scalameta:semanticdb-scalac_2.11.12:$version \
-  -r sonatype:public
-
-# Scala Native is disabled until v0.4
-  # org.scalameta:scalameta_native0.3_2.11:$version \
-
-# These artifacts were published manually, they are not published by the CI.
-# These artfacts will be dropped in the next release.
-coursier resolve \
-  org.scalameta:semanticdb-scalac-core_2.13.0:$version \
-  org.scalameta:semanticdb-scalac-core_2.12.10:$version \
-  org.scalameta:semanticdb-scalac-core_2.12.9:$version \
-  org.scalameta:semanticdb-scalac-core_2.12.8:$version \
-  org.scalameta:semanticdb-scalac_2.13.1:$version \
-  org.scalameta:semanticdb-scalac_2.12.10:$version \
-  org.scalameta:semanticdb-scalac_2.12.9:$version \
-  org.scalameta:semanticdb-scalac_2.12.8:$version \
   -r sonatype:public
