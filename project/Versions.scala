6bfb82da57802d9b775bd184a37336209836edf1
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-06-26 04:43:03 +0200

    Update to Scala 2.13.3

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,6 +4,6 @@ package build
 object Versions {
   val LatestScala211 = "2.11.12"
   val LatestScala212 = "2.12.11"
-  val LatestScala213 = "2.13.2"
-  val LegacyScalaVersions = List("2.12.8", "2.12.9", "2.12.10", "2.13.0", "2.13.1")
+  val LatestScala213 = "2.13.3"
+  val LegacyScalaVersions = List("2.12.8", "2.12.9", "2.12.10", "2.13.0", "2.13.1", "2.13.2")
 }
