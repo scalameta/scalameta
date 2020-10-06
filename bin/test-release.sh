fe39d957a0cfbd4854da0d9eeee5a7053500d63b
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-10-06 13:40:34 +0200

    Update test-release.sh

--- bin/test-release.sh
+++ bin/test-release.sh
@@ -9,15 +9,15 @@ coursier resolve \
   org.scalameta:scalameta_2.11:$version \
   org.scalameta:scalameta_sjs1_2.13:$version \
   org.scalameta:scalameta_sjs1_2.12:$version \
-  org.scalameta:metac_2.13.2:$version \
-  org.scalameta:metac_2.12.11:$version \
+  org.scalameta:metac_2.13.3:$version \
+  org.scalameta:metac_2.12.12:$version \
   org.scalameta:metac_2.11.12:$version \
   org.scalameta:semanticdb-scalac-core_2.13.2:$version \
   org.scalameta:semanticdb-scalac-core_2.13.1:$version \
-  org.scalameta:semanticdb-scalac-core_2.12.11:$version \
+  org.scalameta:semanticdb-scalac-core_2.12.12:$version \
   org.scalameta:semanticdb-scalac-core_2.11.12:$version \
   org.scalameta:semanticdb-scalac_2.13.2:$version \
   org.scalameta:semanticdb-scalac_2.13.1:$version \
-  org.scalameta:semanticdb-scalac_2.12.11:$version \
+  org.scalameta:semanticdb-scalac_2.12.12:$version \
   org.scalameta:semanticdb-scalac_2.11.12:$version \
-  -r sonatype:public
+  -r sonatype:staging
