c0ac0d1fcd7e669437037f0fb6e8d42bcd39a9f5
Tomasz Godzik <tomek.godzik@gmail.com>
2020-10-07 18:03:46 +0200

    Remove files from Dotty community tests and update mima

--- project/Mima.scala
+++ project/Mima.scala
@@ -30,6 +30,8 @@ object Mima {
     ProblemFilters.exclude[MissingClassProblem]("scala.meta.testkit.DiffAssertions"),
     ProblemFilters.exclude[MissingClassProblem]("scala.meta.testkit.DiffAssertions$DiffFailure"),
     ProblemFilters.exclude[MissingClassProblem]("scala.meta.testkit.DiffAssertions$DiffFailure$"),
-    ProblemFilters.exclude[MissingTypesProblem]("scala.meta.testkit.DiffAssertions")
+    ProblemFilters.exclude[MissingTypesProblem]("scala.meta.testkit.DiffAssertions"),
+    ProblemFilters.exclude[ReversedMissingMethodProblem]("scala.meta.Template.getDerives"),
+    ProblemFilters.exclude[ReversedMissingMethodProblem]("scala.meta.Template.setDerives")
   )
 }
