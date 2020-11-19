1606b52c408e3473c1a1a75f064cb433a5f5e7a7
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-11-19 13:36:28 +0100

    Upgrade to Scala 2.13.4

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ package build
 object Versions {
   val LatestScala211 = "2.11.12"
   val LatestScala212 = "2.12.12"
-  val LatestScala213 = "2.13.3"
+  val LatestScala213 = "2.13.4"
   val LegacyScalaVersions =
-    List("2.12.8", "2.12.9", "2.12.10", "2.12.11", "2.13.0", "2.13.1", "2.13.2")
+    List("2.12.8", "2.12.9", "2.12.10", "2.12.11", "2.13.0", "2.13.1", "2.13.2", "2.13.3")
 }
