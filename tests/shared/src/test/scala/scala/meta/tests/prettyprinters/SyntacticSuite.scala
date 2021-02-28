scalameta/scalameta

commit a5ec732af4c3518ebdf33cbcf706919041605d24

[Scala 3] Add support for using `as` for renames and `*` for wildcards
In Scala 3.0.0-RC1 two new syntaxes were introduced:
- `as` soft keyword to use for renames, which also allows to specify renames without brace
- `*` for wildcard imports/exports

More info here: https://dotty.epfl.ch/docs/reference/changed-features/imports.html

scalameta/trees/shared/src/main/scala/scala/meta/internal/prettyprinters/TreeSyntax.scala
@@ -267,13 +267,16 @@ object TreeSyntax {
         looksLikePatVar && thisLocationAlsoAcceptsPatVars
       }
       // soft keywords might need to be written with backquotes in some places
-      def isEscapableSoftKeyword(t: Term.Name, parent: Tree): Boolean = {
+      def isEscapableSoftKeyword(t: Name, parent: Tree): Boolean = {
         t.value match {
           case "extension" if dialect.allowExtensionMethods =>
             parent.is[Term.Apply] || parent.is[Term.ApplyUsing]
           case "inline" if dialect.allowInlineMods =>
             parent.is[Term.Apply] || parent.is[Term.ApplyUsing] || parent.is[Term.ApplyInfix]
-          case _ => false
+          case "*" if dialect.allowStarWildcardImport =>
+            parent.is[Importee]
+          case _ =>
+            false
         }
       }
       (t, t.parent) match {
@@ -283,6 +286,8 @@ object TreeSyntax {
             isEscapableSoftKeyword(t, p)
         case (t: Type.Name, Some(p: Tree)) =>
           cantBeWrittenWithoutBackquotes(t)
+        case (t: Name, Some(p: Tree)) =>
+          isEscapableSoftKeyword(t, p)
         case _ => cantBeWrittenWithoutBackquotes(t)
       }
     }
@@ -328,6 +333,7 @@ object TreeSyntax {
 
       // Name
       case t: Name.Anonymous => s("_")
+      case t: Term.Anonymous => s("")
       case t: Name.Indeterminate => if (guessIsBackquoted(t)) s("`", t.value, "`") else s(t.value)
 
       // Term
@@ -1133,10 +1139,26 @@ object TreeSyntax {
       case t: Importee.Name => s(t.name)
       case t: Importee.Given => s(kw("given"), " ", t.tpe)
       case t: Importee.GivenAll => s(kw("given"))
-      case t: Importee.Rename => s(t.name, " ", kw("=>"), " ", t.rename)
-      case t: Importee.Unimport => s(t.name, " ", kw("=>"), " ", kw("_"))
-      case _: Importee.Wildcard => kw("_")
-      case t: Importer => s(t.ref, ".", t.importees)
+      case t: Importee.Rename =>
+        if (dialect.allowAsForImportRename)
+          s(t.name, " ", kw("as"), " ", t.rename)
+        else
+          s(t.name, " ", kw("=>"), " ", t.rename)
+      case t: Importee.Unimport =>
+        if (dialect.allowAsForImportRename)
+          s(t.name, " ", kw("as"), " ", kw("_"))
+        else
+          s(t.name, " ", kw("=>"), " ", kw("_"))
+      case _: Importee.Wildcard =>
+        if (dialect.allowStarWildcardImport)
+          kw("*")
+        else
+          kw("_")
+      case t: Importer =>
+        if (t.ref.isNot[Term.Anonymous])
+          s(t.ref, ".", t.importees)
+        else
+          s(t.importees)
       case t: Import => s(kw("import"), " ", r(t.importers, ", "))
       case t: Export =>
         s(kw("export"), " ", r(t.importers, ", "))
@@ -1238,6 +1260,8 @@ object TreeSyntax {
       case (t: Importee.Name) :: Nil => s(t)
       case (t: Importee.Wildcard) :: Nil => s(t)
       case (t: Importee.GivenAll) :: Nil => s(t)
+      case (t: Importee.Rename) :: Nil if dialect.allowAsForImportRename => s(t)
+      case (t: Importee.Unimport) :: Nil if dialect.allowAsForImportRename => s(t)
       case (t: Importee.Rename) :: Nil => s("{", t, "}")
       case importees => s("{ ", r(importees, ", "), " }")
     }

scalameta/dialects/shared/src/main/scala/scala/meta/Dialect.scala
@@ -150,7 +150,11 @@ final class Dialect private (
      * `case tp @ OrNull(tp1): OrType`
      * the last section after : was not allowed previously.
      */
-    val allowAllTypedPatterns: Boolean
+    val allowAllTypedPatterns: Boolean,
+    // Scala 3 import renames can use as soft keyword `import a.b.C as D`
+    val allowAsForImportRename: Boolean,
+    // Scala 3 wildcard imports can be specified as `import a.b.*`
+    val allowStarWildcardImport: Boolean
 ) extends Product with Serializable {
 
   // NOTE(olafur) checklist for adding a new dialect field in a binary compatible way:
@@ -241,7 +245,9 @@ final class Dialect private (
       allowSymbolLiterals = true,
       allowDependentFunctionTypes = false,
       allowPostfixStarVarargSplices = false,
-      allowAllTypedPatterns = false
+      allowAllTypedPatterns = false,
+      allowAsForImportRename = false,
+      allowStarWildcardImport = false
       // NOTE(olafur): declare the default value for new fields above this comment.
     )
   }
@@ -431,6 +437,12 @@ final class Dialect private (
   def withAllowAllTypedPatterns(newValue: Boolean): Dialect = {
     privateCopy(allowAllTypedPatterns = newValue)
   }
+  def withAllowAsForImportRename(newValue: Boolean): Dialect = {
+    privateCopy(allowAsRenames = newValue)
+  }
+  def withAllowStarWildcardImport(newValue: Boolean): Dialect = {
+    privateCopy(allowStarWildcardImport = newValue)
+  }
   // NOTE(olafur): add the next `withX()` method above this comment. Please try
   // to use consistent formatting, use `newValue` as the parameter name and wrap
   // the body inside curly braces.
@@ -492,7 +504,9 @@ final class Dialect private (
       allowSymbolLiterals: Boolean = this.allowSymbolLiterals,
       allowDependentFunctionTypes: Boolean = this.allowDependentFunctionTypes,
       allowPostfixStarVarargSplices: Boolean = this.allowPostfixStarVarargSplices,
-      allowAllTypedPatterns: Boolean = this.allowAllTypedPatterns
+      allowAllTypedPatterns: Boolean = this.allowAllTypedPatterns,
+      allowAsRenames: Boolean = this.allowAsForImportRename,
+      allowStarWildcardImport: Boolean = this.allowStarWildcardImport
       // NOTE(olafur): add the next parameter above this comment.
   ): Dialect = {
     new Dialect(
@@ -551,7 +565,9 @@ final class Dialect private (
       allowSymbolLiterals,
       allowDependentFunctionTypes,
       allowPostfixStarVarargSplices,
-      allowAllTypedPatterns
+      allowAllTypedPatterns,
+      allowAsRenames,
+      allowStarWildcardImport
       // NOTE(olafur): add the next argument above this comment.
     )
   }

scalameta/dialects/shared/src/main/scala/scala/meta/dialects/package.scala
@@ -126,6 +126,8 @@ package object dialects {
     .withAllowSymbolLiterals(false)
     .withAllowDependentFunctionTypes(true)
     .withAllowAllTypedPatterns(true)
+    .withAllowAsForImportRename(true)
+    .withAllowStarWildcardImport(true)
 
   @deprecated("Use Scala3 instead", "4.4.2")
   implicit val Dotty = Scala3

scalameta/parsers/shared/src/main/scala/scala/meta/internal/parsers/ScalametaParser.scala
@@ -3807,6 +3807,9 @@ class ScalametaParser(input: Input)(implicit dialect: Dialect) { parser =>
     Import(commaSeparated(importer()))
   }
 
+  def isStarWildcard: Boolean = isStarWildcard(token)
+  def isStarWildcard(token: Token): Boolean = dialect.allowStarWildcardImport && token.syntax == "*"
+
   def importer(): Importer = autoPos {
     val sid = stableId() match {
       case q: Quasi => q.become[Term.Ref.Quasi]
@@ -3815,14 +3818,21 @@ class ScalametaParser(input: Input)(implicit dialect: Dialect) { parser =>
       case path => path
     }
     def dotselectors = { accept[Dot]; Importer(sid, importees()) }
+    def name(tn: Term.Name) = atPos(tn, tn)(Name.Indeterminate(tn.value))
     sid match {
       case Term.Select(sid: Term.Ref, tn: Term.Name) if sid.isStableId =>
         if (token.is[Dot]) dotselectors
-        else
-          Importer(
-            sid,
-            atPos(tn, tn)(Importee.Name(atPos(tn, tn)(Name.Indeterminate(tn.value)))) :: Nil
-          )
+        else if (token.is[soft.KwAs]) {
+          next()
+          Importer(sid, importeeRename(name(tn)) :: Nil)
+        } else if (isStarWildcard(tn.tokens.head)) {
+          Importer(sid, atPos(tn, tn)(Importee.Wildcard()) :: Nil)
+        } else {
+          Importer(sid, atPos(tn, tn)(Importee.Name(name(tn))) :: Nil)
+        }
+      case tn: Term.Name if token.is[soft.KwAs] =>
+        next()
+        Importer(autoPos(Term.Anonymous()), importeeRename(name(tn)) :: Nil)
       case _ =>
         dotselectors
     }
@@ -3854,7 +3864,7 @@ class ScalametaParser(input: Input)(implicit dialect: Dialect) { parser =>
     }
 
   def importWildcardOrName(): Importee = autoPos {
-    if (token.is[Underscore]) {
+    if (token.is[Underscore] || isStarWildcard) {
       next(); Importee.Wildcard()
     } else if (token.is[KwGiven]) {
       next();
@@ -3867,17 +3877,23 @@ class ScalametaParser(input: Input)(implicit dialect: Dialect) { parser =>
     }
   }
 
+  def importeeRename(from: Name) = atPos(from, auto) {
+    importWildcardOrName() match {
+      case to: Importee.Name => Importee.Rename(from, to.name)
+      case to: Importee.Wildcard => Importee.Unimport(from)
+      case other => unreachable(debug(other, other.structure))
+    }
+  }
+
   def importee(): Importee = autoPos {
     importWildcardOrName() match {
-      case from: Importee.Name if token.is[RightArrow] =>
+      case from: Importee.Name if token.is[RightArrow] || token.is[soft.KwAs] =>
         next()
-        importWildcardOrName() match {
-          case to: Importee.Name => Importee.Rename(from.name, to.name)
-          case to: Importee.Wildcard => Importee.Unimport(from.name)
-          case other => unreachable(debug(other, other.structure))
-        }
+        importeeRename(from.name)
       // NOTE: this is completely nuts
-      case from: Importee.Wildcard if token.is[RightArrow] && ahead(token.is[Underscore]) =>
+      case from: Importee.Wildcard
+          if (token.is[RightArrow] || token.is[soft.KwAs]) &&
+            ahead(token.is[Underscore] || isStarWildcard) =>
         nextTwice()
         from
       case other =>

scalameta/parsers/shared/src/main/scala/scala/meta/internal/parsers/SoftKeywords.scala
@@ -15,6 +15,14 @@ class SoftKeywords(dialect: Dialect) {
   }
 
   @classifier
+  trait KwAs {
+    val name = "as"
+    def unapply(token: Token): Boolean = {
+      matches(token, name, dialect.allowAsForImportRename)
+    }
+  }
+
+  @classifier
   trait KwUsing {
     val name = "using"
     def unapply(token: Token): Boolean = {

scalameta/trees/shared/src/main/scala/scala/meta/Trees.scala
@@ -74,6 +74,10 @@ object Term {
   @ast class This(qual: scala.meta.Name) extends Term.Ref
   @ast class Super(thisp: scala.meta.Name, superp: scala.meta.Name) extends Term.Ref
   @ast class Name(value: Predef.String @nonEmpty) extends scala.meta.Name with Term.Ref with Pat
+  @ast class Anonymous() extends scala.meta.Name with Term.Ref {
+    def value = ""
+    checkParent(ParentChecks.AnonymousImport)
+  }
   @ast class Select(qual: Term, name: Term.Name) extends Term.Ref with Pat
   @ast class Interpolate(prefix: Name, parts: List[Lit] @nonEmpty, args: List[Term]) extends Term {
     checkFields(parts.length == args.length + 1)

scalameta/trees/shared/src/main/scala/scala/meta/internal/trees/ParentChecks.scala
@@ -71,6 +71,10 @@ object ParentChecks {
     bindRhs || extractArgs || extractInfixArgs || extractInterpolate || extractXml
   }
 
+  def AnonymousImport(tree: Term.Anonymous, parent: Tree, destination: String): Boolean = {
+    parent.is[Importer]
+  }
+
   def NameAnonymous(tree: Name.Anonymous, parent: Tree, destination: String): Boolean = {
     def primaryCtorName = parent.is[Ctor.Primary] && destination == "name"
     def secondaryCtorName = parent.is[Ctor.Secondary] && destination == "name"

scalameta/trees/shared/src/main/scala/scala/meta/internal/trees/package.scala
@@ -99,7 +99,7 @@ package object trees {
     }
     def isStableId: Boolean = tree match {
       case _: Term.Ref.Quasi => true
-      case _: Term.Name | Term.Select(_: Term.Super, _) => true
+      case _: Term.Name | _: Term.Anonymous | Term.Select(_: Term.Super, _) => true
       case Term.Select(qual: Term.Quasi, _) => true
       case Term.Select(qual: Term.Ref, _) => qual.isPath
       case _ => false

commit 5457bb98f6854ee1809f5fbaee66f2edc15fbe1d

pretty printer: backquote importees when needed
fix regression introduced in a5ec732

scalameta/trees/shared/src/main/scala/scala/meta/internal/prettyprinters/TreeSyntax.scala
@@ -279,17 +279,17 @@ object TreeSyntax {
             false
         }
       }
-      (t, t.parent) match {
-        case (t: Term.Name, Some(p: Tree)) =>
-          isAmbiguousWithPatVarTerm(t, p) ||
-            cantBeWrittenWithoutBackquotes(t) ||
-            isEscapableSoftKeyword(t, p)
-        case (t: Type.Name, Some(p: Tree)) =>
-          cantBeWrittenWithoutBackquotes(t)
-        case (t: Name, Some(p: Tree)) =>
-          isEscapableSoftKeyword(t, p)
-        case _ => cantBeWrittenWithoutBackquotes(t)
+      def isAmbiguousInParent(t: Tree, parent: Tree): Boolean = {
+        t match {
+          case t: Term.Name =>
+            isAmbiguousWithPatVarTerm(t, parent) || isEscapableSoftKeyword(t, parent)
+          case t: Name =>
+            isEscapableSoftKeyword(t, parent)
+          case _ =>
+            false
+        }
       }
+      cantBeWrittenWithoutBackquotes(t) || t.parent.exists(isAmbiguousInParent(t, _))
     }
     def guessIsPostfix(t: Term.Select): Boolean = false
     def guessHasExpr(t: Term.Return): Boolean = t.expr match {

tests/shared/src/test/scala/scala/meta/tests/prettyprinters/SyntacticSuite.scala
@@ -807,6 +807,11 @@ class SyntacticSuite extends scala.meta.tests.parsers.ParseSuite {
     assert(q"import a.{b=>c}".syntax == "import a.{b => c}")
   }
 
+  test("backquote importees when needed - scalafix #1337") {
+    assert(q"import a.`{ q }`".syntax == "import a.`{ q }`")
+    assert(q"import a.`macro`".syntax == "import a.`macro`")
+  }
+
   test("show[Structure] should uppercase long literals suffix: '2l' -> '2L'") {
     assert(
       templStat("foo(1l, 1L)").structure ==