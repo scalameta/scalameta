69e8380a89fd892c160c2efd72c5cac7116ffbb6
Tomasz Godzik <tomek.godzik@gmail.com>
2021-06-24 15:11:56 +0200

    [Scala 3] Fix issues with type lambda in type bounds

--- scalameta/trees/shared/src/main/scala/scala/meta/internal/trees/ParentChecks.scala
+++ scalameta/trees/shared/src/main/scala/scala/meta/internal/trees/ParentChecks.scala
@@ -114,7 +114,7 @@ object ParentChecks {
   }
 
   def TypeLambda(tree: Type.Lambda, parent: Tree, destination: String): Boolean = {
-    parent.is[Type] || parent.is[Defn.Type] || parent.is[Term.ApplyType]
+    parent.is[Type] || parent.is[Defn.Type] || parent.is[Type.Bounds] || parent.is[Term.ApplyType]
   }
 
   def TypeMethod(tree: Type.Method, parent: Tree, destination: String): Boolean = {
