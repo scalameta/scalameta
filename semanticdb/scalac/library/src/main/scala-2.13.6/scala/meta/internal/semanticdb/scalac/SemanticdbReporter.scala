6d7e321bf6107ff003c130df8ed8eb5f6422064f
Meriam Lachkar <meriam.lachkar@gmail.com>
2021-07-05 18:12:31 +0200

    Fix SemanticdbReporter and set Xmaxwarns and Xmaxerrs for this reporter
    - in previous version from 2.12 up to 2.13.0, we instantiate a new settings() for semanticdbRepoter
    which have a Xmaxwarns to the default value (100 max warnings), which implies that the reporter wont report more than 100 diagnostics
    - in 2.13.1+, by overloading def filter we keep the same the same setting Xmaxwarns set by the user.
    This setting is supposed to modify the number of warning printed, but should not modify the number of
    warning stored in semanticdb file.

--- semanticdb/scalac/library/src/main/scala-2.13.6/scala/meta/internal/semanticdb/scalac/SemanticdbReporter.scala
+++ semanticdb/scalac/library/src/main/scala-2.13.6/scala/meta/internal/semanticdb/scalac/SemanticdbReporter.scala
@@ -2,21 +2,26 @@ package scala.meta.internal.semanticdb.scalac
 
 import scala.reflect.internal.util.Position
 import scala.tools.nsc.reporters.{FilteringReporter, StoreReporter}
+import scala.tools.nsc.Settings
 
-class SemanticdbReporter(underlying: FilteringReporter) extends StoreReporter {
+class SemanticdbReporter(underlying: FilteringReporter)
+    extends StoreReporter(SemanticdbReporter.defaultSettings(underlying.settings)) {
   override def doReport(pos: Position, msg: String, severity: Severity): Unit = {
     super.doReport(pos, msg, severity)
     underlying.doReport(pos, msg, severity)
   }
 
-  // overriding increment and filter is enough so make sure that error/warning
+  // overriding increment is enough so make sure that error/warning
   // counts are the same as in underlying reporter
 
   override def increment(severity: Severity): Unit = {
     super.increment(severity)
     underlying.increment(severity)
   }
-
-  override def filter(pos: Position, msg: String, severity: Severity): Int =
-    underlying.filter(pos, msg, severity)
+}
+object SemanticdbReporter {
+  def defaultSettings(s: Settings): Settings = {
+    s.processArguments(List("-Xmaxwarns", "-1", "-Xmaxerrs", "-1"), true)
+    s
+  }
 }
