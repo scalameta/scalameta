10892c850d8c66c6b3638717e428b31d44cb6528
adampauls <adpauls@microsoft.com>
2022-01-25 11:00:41 -0800

    Permit package declarations when allowToplevelTerms enabled.
    Just change the old behavior.

--- tests/shared/src/test/scala/scala/meta/tests/parsers/ParseSuite.scala
+++ tests/shared/src/test/scala/scala/meta/tests/parsers/ParseSuite.scala
@@ -26,7 +26,8 @@ class ParseSuite extends FunSuite with CommonTrees {
   def term(code: String)(implicit dialect: Dialect) = code.parseRule(_.expr())
   def pat(code: String)(implicit dialect: Dialect) = code.parseRule(_.pattern())
   def tpe(code: String)(implicit dialect: Dialect) = code.parseRule(_.typ())
-  def topStat(code: String)(implicit dialect: Dialect) = code.parseRule(_.topStatSeq().head)
+  def topStat(code: String)(implicit dialect: Dialect) =
+    code.parseRule(p => p.statSeq(p.topStat).head)
   def templStat(code: String)(implicit dialect: Dialect) = code.parseRule(_.templateStats().head)
   def blockStat(code: String)(implicit dialect: Dialect) = code.parseRule(_.blockStatSeq().head)
   def caseClause(code: String)(implicit dialect: Dialect) = code.parseRule(_.caseClause())
