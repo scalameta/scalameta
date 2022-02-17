f60036b77ec6e687e4306326e4497043b1952775
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2022-02-17 07:48:29 -0800

    ParentChecks: use match, remove redundant checks

--- scalameta/trees/shared/src/main/scala/scala/meta/internal/trees/ParentChecks.scala
+++ scalameta/trees/shared/src/main/scala/scala/meta/internal/trees/ParentChecks.scala
@@ -3,56 +3,42 @@ package internal
 package trees
 
 import org.scalameta.invariants._
+
+import scala.annotation.tailrec
 import scala.meta.classifiers._
-import scala.meta.Defn.Given
-import scala.meta.Defn.GivenAlias
-import scala.meta.Defn.ExtensionGroup
 
 object ParentChecks {
-  private def termArgument(parent: Tree, destination: String): Boolean = {
-    def applyArgument = parent.is[Term.Apply] && destination == "args"
-    def applyInfixArgument = parent.is[Term.ApplyInfix] && destination == "args"
-    def applyUnaryArgument = parent.is[Term.ApplyUnary] && destination == "arg"
-    def assignArgument = parent.is[Term.Assign] && destination == "rhs"
-    def interpolateArgument = parent.is[Term.Interpolate] && destination == "args"
-    def xmlArgument = parent.is[Term.Interpolate] && destination == "args"
-    def initArgument = parent.is[Init] && destination == "argss"
-    def namedArgument = parent.is[Term.Assign] && destination == "rhs"
-    applyArgument || applyInfixArgument || applyUnaryArgument || assignArgument ||
-    interpolateArgument || xmlArgument || initArgument || namedArgument
+  private def termArgument(parent: Tree, destination: String): Boolean = parent match {
+    case _: Term.Apply | _: Term.ApplyInfix | _: Term.Interpolate => destination == "args"
+    case _: Term.ApplyUnary => destination == "arg"
+    case _: Term.Assign => destination == "rhs"
+    case _: Init => destination == "argss"
+    case _ => false
   }
 
   def TermAssign(tree: Term.Assign, parent: Tree, destination: String): Boolean = {
-    def namedRepeatedArgument = tree.rhs.is[Term.Repeated]
-    !namedRepeatedArgument || termArgument(parent, destination)
+    !tree.rhs.is[Term.Repeated] || termArgument(parent, destination)
   }
 
   def TermRepeated(tree: Term.Repeated, parent: Tree, destination: String): Boolean = {
     termArgument(parent, destination)
   }
 
-  def PatVar(tree: Pat.Var, parent: Tree, destination: String): Boolean = {
-    val Pat.Var(Term.Name(value)) = tree
-    def capitalized = value.nonEmpty && value(0).isUpper
-    def isInPatternBinding = parent.is[Pat.Bind]
-    def declValPat = parent.is[Decl.Val] && destination == "pats"
-    def declVarPat = parent.is[Decl.Var] && destination == "pats"
-    def defnValPat = parent.is[Defn.Val] && destination == "pats"
-    def defnVarPat = parent.is[Defn.Var] && destination == "pats"
-    def enumeratorGeneratorPat = parent.is[Enumerator.Generator] && destination == "pat"
-    def enumeratorValPat = parent.is[Enumerator.Val] && destination == "pat"
-    isInPatternBinding || !capitalized || declValPat || declVarPat || defnValPat || defnVarPat ||
-    enumeratorGeneratorPat || enumeratorValPat
+  def PatVar(tree: Pat.Var, parent: Tree, destination: String): Boolean = parent match {
+    case _: Pat.Bind => true
+    case _: Decl.Val | _: Decl.Var | _: Defn.Val | _: Defn.Var => destination == "pats"
+    case _: Enumerator.Generator | _: Enumerator.Val => destination == "pat"
+    case _ =>
+      val value = tree.name.value
+      value.isEmpty || !value(0).isUpper
   }
 
-  private def typeArgument(tree: Type, parent: Tree, destination: String): Boolean = {
-    def termParamDecltpe = parent.is[Term.Param] && destination == "decltpe"
-    def typeFunctionArgument =
-      (parent.is[Type.Function] || parent.is[Type.ContextFunction]) && destination == "params"
-    def byNameType = parent.is[Type.ByName] && destination == "tpe"
-    def byNameRepeatedType =
-      tree.is[Type.ByName] && parent.is[Type.Repeated] && destination == "tpe"
-    termParamDecltpe || typeFunctionArgument || byNameType || byNameRepeatedType
+  private def typeArgument(tree: Type, parent: Tree, destination: String): Boolean = parent match {
+    case _: Term.Param => destination == "decltpe"
+    case _: Type.FunctionType => destination == "params"
+    case _: Type.ByName => destination == "tpe"
+    case _: Type.Repeated => tree.is[Type.ByName] && destination == "tpe"
+    case _ => false
   }
 
   def TypeByName(tree: Type.ByName, parent: Tree, destination: String): Boolean = {
@@ -63,43 +49,33 @@ object ParentChecks {
     typeArgument(tree, parent, destination)
   }
 
-  def PatSeqWildcard(tree: Pat.SeqWildcard, parent: Tree, destination: String): Boolean = {
-    def bindRhs = parent.is[Pat.Bind] && destination == "rhs"
-    def extractArgs = parent.is[Pat.Extract] && destination == "args"
-    def extractInfixArgs = parent.is[Pat.ExtractInfix] && destination == "args"
-    def extractInterpolate = parent.is[Pat.Interpolate] && destination == "args"
-    def extractXml = parent.is[Pat.Xml] && destination == "args"
-    bindRhs || extractArgs || extractInfixArgs || extractInterpolate || extractXml
-  }
+  def PatSeqWildcard(tree: Pat.SeqWildcard, parent: Tree, destination: String): Boolean =
+    parent match {
+      case _: Pat.Bind => destination == "rhs"
+      case _: Pat.Extract | _: Pat.ExtractInfix | _: Pat.Interpolate | _: Pat.Xml =>
+        destination == "args"
+      case _ => false
+    }
 
   def AnonymousImport(tree: Term.Anonymous, parent: Tree, destination: String): Boolean = {
     parent.is[Importer]
   }
 
-  def NameAnonymous(tree: Name.Anonymous, parent: Tree, destination: String): Boolean = {
-    def primaryCtorName = parent.is[Ctor.Primary] && destination == "name"
-    def secondaryCtorName = parent.is[Ctor.Secondary] && destination == "name"
-    def termParamName = parent.is[Term.Param] && destination == "name"
-    def typeParamName = parent.is[Type.Param] && destination == "name"
-    def initName = parent.is[Init] && destination == "name"
-    def selfName = parent.is[Self] && destination == "name"
-    def privateWithin = parent.is[Mod.Private] && destination == "within"
-    def protectedWithin = parent.is[Mod.Protected] && destination == "within"
-    def thisQualifier = parent.is[Term.This]
-    def givenName = parent.is[Given] || parent.is[GivenAlias]
-    def extensionName = parent.is[ExtensionGroup]
-    def repeatedCase = parent.is[Defn.RepeatedEnumCase]
-    def superQualifier = parent.is[Term.Super]
-    primaryCtorName || secondaryCtorName || termParamName || typeParamName ||
-    initName || selfName || privateWithin || protectedWithin || thisQualifier ||
-    superQualifier || givenName || extensionName || repeatedCase
-  }
+  def NameAnonymous(tree: Name.Anonymous, parent: Tree, destination: String): Boolean =
+    parent match {
+      case _: Ctor | _: Init | _: Self | _: Term.Param | _: Type.Param => destination == "name"
+      case _: Mod.Private | _: Mod.Protected => destination == "within"
+      case _: Term.This | _: Term.Super | _: Defn.Given | _: Defn.GivenAlias => true
+      case _: Defn.ExtensionGroup | _: Defn.RepeatedEnumCase => true
+      case _ => false
+    }
 
   def TypeVar(tree: Type.Var, parent: Tree, destination: String): Boolean = {
+    @tailrec
     def loop(tree: Option[Tree]): Boolean = tree match {
       case Some(tree: Type) => loop(tree.parent)
       case Some(_: Pat.Typed) => true
-      case Some(tree: Term.ApplyType) => tree.parent.map(_.is[Pat.Extract]).getOrElse(true)
+      case Some(tree: Term.ApplyType) => tree.parent.forall(_.is[Pat.Extract])
       case Some(_) => false
       case None => true
     }
