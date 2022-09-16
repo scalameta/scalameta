99693c2a9ac73b03c420668cc3efabe5c3cf66eb
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2022-09-15 21:21:36 -0700

    TreeSyntax: fix floats and doubles (#2862)

--- tests/shared/src/test/scala/scala/meta/tests/parsers/Scala213Suite.scala
+++ tests/shared/src/test/scala/scala/meta/tests/parsers/Scala213Suite.scala
@@ -1,8 +1,7 @@
 package scala.meta.tests
 package parsers
 
-import scala.meta.Dialect
-import scala.meta.dialects
+import scala.meta._
 import scala.meta.dialects.Scala213
 
 class Scala213Suite extends ParseSuite {
@@ -11,89 +10,126 @@ class Scala213Suite extends ParseSuite {
 
   test("literal-types") {
     // https://docs.scala-lang.org/sips/42.type.html
-    runAssert(
-      "val a: 42 = 42",
-      """Defn.Val(Nil, List(Pat.Var(Term.Name("a"))), Some(Lit.Int(42)), Lit.Int(42))"""
+    runAssert("val a: 42 = 42")(
+      Defn.Val(Nil, List(Pat.Var(Term.Name("a"))), Some(Lit.Int(42)), Lit.Int(42))
     )
 
-    runAssert(
-      "val a: -2 = -2",
-      """Defn.Val(Nil, List(Pat.Var(Term.Name("a"))), Some(Lit.Int(-2)), Lit.Int(-2))"""
+    runAssert("val a: -2 = -2")(
+      Defn.Val(Nil, List(Pat.Var(Term.Name("a"))), Some(Lit.Int(-2)), Lit.Int(-2))
     )
 
-    runAssert(
-      "def foo(a: 3.14159f): .1",
-      """Decl.Def(Nil, Term.Name("foo"), Nil, List(List(Term.Param(Nil, Term.Name("a"), Some(Lit.Float(3.14159f)), None))), Lit.Double(.1))"""
+    runAssert("def foo(a: 3.14159f): .1d")(
+      Decl.Def(
+        Nil,
+        Term.Name("foo"),
+        Nil,
+        List(List(Term.Param(Nil, Term.Name("a"), Some(Lit.Float("3.14159f")), None))),
+        Lit.Double(".1")
+      )
     )
 
-    runAssert(
-      "def foo(x: 'a'): Option['z']",
-      """Decl.Def(Nil, Term.Name("foo"), Nil, List(List(Term.Param(Nil, Term.Name("x"), Some(Lit.Char('a')), None))), Type.Apply(Type.Name("Option"), List(Lit.Char('z'))))"""
+    runAssert("def foo(x: 'a'): Option['z']")(
+      Decl.Def(
+        Nil,
+        Term.Name("foo"),
+        Nil,
+        List(List(Term.Param(Nil, Term.Name("x"), Some(Lit.Char('a')), None))),
+        Type.Apply(Type.Name("Option"), List(Lit.Char('z')))
+      )
     )
 
-    runAssert(
-      "def bar[T <: 1](t: T): T = t",
-      """Defn.Def(Nil, Term.Name("bar"), List(Type.Param(Nil, Type.Name("T"), Nil, Type.Bounds(None, Some(Lit.Int(1))), Nil, Nil)), List(List(Term.Param(Nil, Term.Name("t"), Some(Type.Name("T")), None))), Some(Type.Name("T")), Term.Name("t"))"""
+    runAssert("def bar[T <: 1](t: T): T = t")(
+      Defn.Def(
+        Nil,
+        Term.Name("bar"),
+        List(Type.Param(Nil, Type.Name("T"), Nil, Type.Bounds(None, Some(Lit.Int(1))), Nil, Nil)),
+        List(List(Term.Param(Nil, Term.Name("t"), Some(Type.Name("T")), None))),
+        Some(Type.Name("T")),
+        Term.Name("t")
+      )
     )
   }
 
   test("identifier-types") {
     // "x" is a class with def =>>(x: Int): Int, in dotty ==> is keyword and it will be error here
-    runAssert(
-      "val c = x =>> 3",
-      """Defn.Val(Nil, List(Pat.Var(Term.Name("c"))), None, Term.ApplyInfix(Term.Name("x"), Term.Name("=>>"), Nil, List(Lit.Int(3))))"""
+    runAssert("val c = x =>> 3")(
+      Defn.Val(
+        Nil,
+        List(Pat.Var(Term.Name("c"))),
+        None,
+        Term.ApplyInfix(Term.Name("x"), Term.Name("=>>"), Nil, List(Lit.Int(3)))
+      )
     )
 
-    runAssert(
-      "val given = 3",
-      """Defn.Val(Nil, List(Pat.Var(Term.Name("given"))), None, Lit.Int(3))"""
+    runAssert("val given = 3")(
+      Defn.Val(Nil, List(Pat.Var(Term.Name("given"))), None, Lit.Int(3))
     )
 
-    runAssert(
-      "val extension = 3",
-      """Defn.Val(Nil, List(Pat.Var(Term.Name("extension"))), None, Lit.Int(3))"""
+    runAssert("val extension = 3")(
+      Defn.Val(Nil, List(Pat.Var(Term.Name("extension"))), None, Lit.Int(3))
     )
 
-    runAssert(
-      "val enum = 3",
-      """Defn.Val(Nil, List(Pat.Var(Term.Name("enum"))), None, Lit.Int(3))"""
+    runAssert("val enum = 3")(
+      Defn.Val(Nil, List(Pat.Var(Term.Name("enum"))), None, Lit.Int(3))
     )
   }
 
   test("try with any expr") {
-    runAssert(
-      "try (1 + 2).toString",
-      """Term.Try(Term.Select(Term.ApplyInfix(Lit.Int(1), Term.Name("+"), Nil, List(Lit.Int(2))), Term.Name("toString")), Nil, None)"""
+    runAssert("try (1 + 2).toString")(
+      Term.Try(
+        Term.Select(
+          Term.ApplyInfix(Lit.Int(1), Term.Name("+"), Nil, List(Lit.Int(2))),
+          Term.Name("toString")
+        ),
+        Nil,
+        None
+      )
     )
-    runAssert(
-      "try { 1 + 2 }.toString",
-      """Term.Try(Term.Select(Term.Block(List(Term.ApplyInfix(Lit.Int(1), Term.Name("+"), Nil, List(Lit.Int(2))))), Term.Name("toString")), Nil, None)"""
+    runAssert("try { 1 + 2 }.toString")(
+      Term.Try(
+        Term.Select(
+          Term.Block(List(Term.ApplyInfix(Lit.Int(1), Term.Name("+"), Nil, List(Lit.Int(2))))),
+          Term.Name("toString")
+        ),
+        Nil,
+        None
+      )
     )
-    runAssert(
-      "try (1 :: Nil) map fn",
-      """Term.Try(Term.ApplyInfix(Term.ApplyInfix(Lit.Int(1), Term.Name("::"), Nil, List(Term.Name("Nil"))), Term.Name("map"), Nil, List(Term.Name("fn"))), Nil, None)"""
+    runAssert("try (1 :: Nil) map fn")(
+      Term.Try(
+        Term.ApplyInfix(
+          Term.ApplyInfix(Lit.Int(1), Term.Name("::"), Nil, List(Term.Name("Nil"))),
+          Term.Name("map"),
+          Nil,
+          List(Term.Name("fn"))
+        ),
+        Nil,
+        None
+      )
     )
-    runAssert(
-      "try (true, false)",
-      """Term.Try(Term.Tuple(List(Lit.Boolean(true), Lit.Boolean(false))), Nil, None)"""
+    runAssert("try (true, false)")(
+      Term.Try(Term.Tuple(List(Lit.Boolean(true), Lit.Boolean(false))), Nil, None)
     )
-    runAssert("try ()", """Term.Try(Lit.Unit(()), Nil, None)""")
+    runAssert("try ()")(Term.Try(Lit.Unit(), Nil, None))
   }
 
   test("infix-at-line-start") {
     runAssert(
       """|val x = "hello"
          |  ++ "world"
-         |""".stripMargin,
-      """Defn.Val(Nil, List(Pat.Var(Term.Name("x"))), None, Term.ApplyInfix(Lit.String("hello"), Term.Name("++"), Nil, List(Lit.String("world"))))"""
+         |""".stripMargin
+    )(
+      Defn.Val(
+        Nil,
+        List(Pat.Var(Term.Name("x"))),
+        None,
+        Term.ApplyInfix(Lit.String("hello"), Term.Name("++"), Nil, List(Lit.String("world")))
+      )
     )(dialects.Scala213Source3)
   }
 
-  private def runAssert(code: String, expected: String)(implicit d: Dialect): Unit = {
-    assertNoDiff(
-      templStat(code)(d).structure,
-      expected
-    )
+  private def runAssert(code: String)(expected: Tree)(implicit d: Dialect): Unit = {
+    assertTree(templStat(code)(d))(expected)
   }
 
 }
