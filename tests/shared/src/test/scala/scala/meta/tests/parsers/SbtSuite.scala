4ed08fde5978612cadae0e36ec8050432b8ab04d
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2022-10-22 16:05:47 -0700

    SbtSuite: cosmetic, reformat the test string (#2920)

--- tests/shared/src/test/scala/scala/meta/tests/parsers/SbtSuite.scala
+++ tests/shared/src/test/scala/scala/meta/tests/parsers/SbtSuite.scala
@@ -9,8 +9,8 @@ import scala.meta.dialects.Sbt0137
 class SbtSuite extends FunSuite {
   test("\"...\".parse[Source]") {
     val tree = simpleBuildSyntax.parse[Source].get
-    assert(tree.syntax == simpleBuildSyntax)
-    assert(tree.structure == simpleBuildStructure)
+    assertEquals(tree.syntax, simpleBuildSyntax)
+    assertEquals(tree.structure, simpleBuildStructure)
   }
 
   test("\"...\".parse[Stat]") {
@@ -32,13 +32,14 @@ class SbtSuite extends FunSuite {
         )
     """
     // NOTE: not checking against simpleBuildSyntax because quasiquotes don't retain tokens
-    assert(
-      tree.syntax == """
+    assertEquals(
+      tree.syntax,
+      """
       |lazy val commonSettings = Seq(organization := "com.example", version := "0.1.0", scalaVersion := "2.11.7")
       |lazy val root = (project in file(".")).settings(commonSettings: _*).settings(name := "hello")
     """.trim.stripMargin.split('\n').mkString(EOL)
     )
-    assert(tree.structure == simpleBuildStructure)
+    assertEquals(tree.structure, simpleBuildStructure)
   }
 
   private def simpleBuildSyntax = """
@@ -58,28 +59,65 @@ class SbtSuite extends FunSuite {
   private def simpleBuildStructure = {
     """
       |Source(List(
-        |Defn.Val(
-          |List(Mod.Lazy()),
-          | List(Pat.Var(Term.Name("commonSettings"))),
-          | None,
-          | Term.Apply(
-              |Term.Name("Seq"),
-              | List(
-                  |Term.ApplyInfix(Term.Name("organization"), Term.Name(":="), Nil, List(Lit.String("com.example"))),
-                  | Term.ApplyInfix(Term.Name("version"), Term.Name(":="), Nil, List(Lit.String("0.1.0"))),
-                  | Term.ApplyInfix(Term.Name("scalaVersion"), Term.Name(":="), Nil, List(Lit.String("2.11.7")))))),
-        | Defn.Val(
-          |List(Mod.Lazy()),
-          | List(Pat.Var(Term.Name("root"))),
-          | None,
-          | Term.Apply(
-              |Term.Select(
-                |Term.Apply(
-                  |Term.Select(Term.ApplyInfix(Term.Name("project"), Term.Name("in"), Nil, List(Term.Apply(Term.Name("file"), List(Lit.String("."))))), Term.Name("settings")),
-                  | List(Term.Repeated(Term.Name("commonSettings")))),
-                | Term.Name("settings")),
-              | List(
-                  |Term.ApplyInfix(Term.Name("name"), Term.Name(":="), Nil, List(Lit.String("hello"))))))))
-    """.trim.stripMargin.split('\n').mkString("")
+      |  Defn.Val(
+      |    List(Mod.Lazy()),
+      |    List(Pat.Var(Term.Name("commonSettings"))),
+      |    None,
+      |    Term.Apply(
+      |      Term.Name("Seq"),
+      |      List(
+      |        Term.ApplyInfix(
+      |          Term.Name("organization"),
+      |          Term.Name(":="),
+      |          Nil,
+      |          List(Lit.String("com.example"))
+      |        ),
+      |        Term.ApplyInfix(
+      |          Term.Name("version"),
+      |          Term.Name(":="),
+      |          Nil,
+      |          List(Lit.String("0.1.0"))
+      |        ),
+      |        Term.ApplyInfix(
+      |          Term.Name("scalaVersion"),
+      |          Term.Name(":="),
+      |          Nil,
+      |          List(Lit.String("2.11.7"))
+      |        )
+      |      )
+      |    )
+      |  ),
+      |  Defn.Val(
+      |    List(Mod.Lazy()),
+      |    List(Pat.Var(Term.Name("root"))),
+      |    None,
+      |    Term.Apply(
+      |      Term.Select(
+      |        Term.Apply(
+      |          Term.Select(
+      |            Term.ApplyInfix(
+      |              Term.Name("project"),
+      |              Term.Name("in"),
+      |              Nil,
+      |              List(Term.Apply(Term.Name("file"), List(Lit.String("."))))
+      |            ),
+      |            Term.Name("settings")
+      |          ),
+      |          List(Term.Repeated(Term.Name("commonSettings")))
+      |        ),
+      |        Term.Name("settings")
+      |      ),
+      |      List(
+      |        Term.ApplyInfix(
+      |          Term.Name("name"),
+      |          Term.Name(":="),
+      |          Nil,
+      |          List(Lit.String("hello"))
+      |        )
+      |      )
+      |    )
+      |  )
+      |))
+      |""".stripMargin.trim.replaceAll("[,]\n *", ", ").replaceAll("\n *", "")
   }
 }
