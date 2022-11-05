98e72576394ae00ce49d34f5f2d78efe76ec98c0
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2022-11-04 21:36:32 -0700

    Trees: use common Tuple checks in all tuples (#2946)

--- scalameta/trees/shared/src/main/scala/scala/meta/Trees.scala
+++ scalameta/trees/shared/src/main/scala/scala/meta/Trees.scala
@@ -117,11 +117,7 @@ object Term {
   @ast class Tuple(args: List[Term] @nonEmpty) extends Term with Member.Tuple {
     // tuple may have one element (see scala.Tuple1)
     // however, this element may not be another single-element Tuple
-    checkFields(args match {
-      case Nil => false
-      case (t: Tuple) :: Nil => t.args.lengthCompare(1) > 0
-      case _ => true
-    })
+    checkParent(ParentChecks.MemberTuple)
   }
   @ast class Block(stats: List[Stat]) extends Term {
     checkParent(ParentChecks.TermBlock)
@@ -244,11 +240,7 @@ object Type {
   @ast class Tuple(args: List[Type] @nonEmpty) extends Type with Member.Tuple {
     // tuple may have one element (see scala.Tuple1)
     // however, this element may not be another single-element Tuple
-    checkFields(args match {
-      case Nil => false
-      case (t: Tuple) :: Nil => t.args.lengthCompare(1) > 0
-      case _ => true
-    })
+    checkParent(ParentChecks.MemberTuple)
   }
   @ast class With(lhs: Type, rhs: Type) extends Type
   @deprecated("And unused, replaced by ApplyInfix", "4.5.1")
@@ -341,11 +333,7 @@ object Pat {
   @ast class Tuple(args: List[Pat] @nonEmpty) extends Pat with Member.Tuple {
     // tuple may have one element (see scala.Tuple1)
     // however, this element may not be another single-element Tuple
-    checkFields(args match {
-      case Nil => false
-      case (t: Tuple) :: Nil => t.args.lengthCompare(1) > 0
-      case _ => true
-    })
+    checkParent(ParentChecks.MemberTuple)
   }
   @ast class Repeated(name: Term.Name) extends Pat
   @ast class Extract(fun: Term, args: List[Pat]) extends Pat {
