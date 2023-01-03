f48062c60fb73ada97a54383150b088f8e75be89
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2023-01-03 11:23:23 -0800

    SepRegion: add two subtraits, for indented and non

--- scalameta/parsers/shared/src/main/scala/scala/meta/internal/parsers/SepRegion.scala
+++ scalameta/parsers/shared/src/main/scala/scala/meta/internal/parsers/SepRegion.scala
@@ -4,30 +4,33 @@ sealed trait SepRegion {
   def indent = -1
   def closeOnNonCase = false
   def indentOnArrow = true
-  def isIndented = false
+  def isIndented: Boolean
+}
+
+sealed trait SepRegionIndented extends SepRegion {
+  override final def isIndented = true
+}
+sealed trait SepRegionNonIndented extends SepRegion {
+  override final def isIndented = false
 }
 
 case class RegionIndent(override val indent: Int, override val closeOnNonCase: Boolean)
-    extends SepRegion {
-  override def isIndented: Boolean = true
-}
+    extends SepRegionIndented
 
-case class RegionParen(canProduceLF: Boolean) extends SepRegion
+case class RegionParen(canProduceLF: Boolean) extends SepRegionNonIndented
 
-case object RegionBracket extends SepRegion
+case object RegionBracket extends SepRegionNonIndented
 
 case class RegionBrace(override val indent: Int, override val indentOnArrow: Boolean)
-    extends SepRegion
+    extends SepRegionNonIndented
 
-case class RegionCase(override val indent: Int) extends SepRegion
+case class RegionCase(override val indent: Int) extends SepRegionNonIndented
 
-case class RegionEnum(override val indent: Int) extends SepRegion
+case class RegionEnum(override val indent: Int) extends SepRegionNonIndented
 
-case class RegionIndentEnum(override val indent: Int) extends SepRegion {
-  override def isIndented: Boolean = true
-}
+case class RegionIndentEnum(override val indent: Int) extends SepRegionIndented
 
-case object RegionArrow extends SepRegion
+case object RegionArrow extends SepRegionNonIndented
 
 // NOTE: Special case for Enum region is needed because parsing of 'case' statement is done differently
-case object RegionEnumArtificialMark extends SepRegion
+case object RegionEnumArtificialMark extends SepRegionNonIndented
