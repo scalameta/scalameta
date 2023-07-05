2ab42d44224e50eba54a01f9ff901724213bc8e2
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2023-07-05 16:59:41 -0700

    Add tests with `match` within `while` condition

--- tests/shared/src/test/scala/scala/meta/tests/parsers/dotty/ControlSyntaxSuite.scala
+++ tests/shared/src/test/scala/scala/meta/tests/parsers/dotty/ControlSyntaxSuite.scala
@@ -3765,4 +3765,30 @@ class ControlSyntaxSuite extends BaseDottySuite {
     )
   }
 
+  test("while cond uses match") {
+    val code1 =
+      """|if sArr(last) == ')' then
+         |  while (sArr(last): @switch).match
+         |    case _ => false
+         |  do last -= 1
+         |""".stripMargin
+    val code2 =
+      """|if sArr(last) == ')' then
+         |  while (sArr(last): @switch) match
+         |    case _ => false
+         |  do last -= 1
+         |""".stripMargin
+    val layout =
+      """|if (sArr(last) == ')') while ((sArr(last): @switch) match {
+         |  case _ => false
+         |}) last -= 1
+         |""".stripMargin
+    assertNoDiff(parseStat(code1, dialect).reprint, layout)
+    val error2 =
+      """|<input>:2: error: illegal start of simple expression
+         |  while (sArr(last): @switch) match
+         |                              ^""".stripMargin
+    runTestError[Stat](code2, error2)
+  }
+
 }
