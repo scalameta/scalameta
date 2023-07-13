97df9181bc36921bd57959556bc62d341e19134f
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2023-07-13 11:29:00 -0700

    SBT: publish JVM/JS/Native separately

---
+++ project/Platforms.scala
@@ -0,0 +1,19 @@
+object Platforms {
+
+  private val envPlatform = "SCALAMETA_PLATFORM"
+
+  private val platforms: Map[String, sbtcrossproject.Platform] = Map(
+    "jvm" -> sbtcrossproject.JVMPlatform,
+    "js" -> scalajscrossproject.JSPlatform,
+    "native" -> scalanativecrossproject.NativePlatform
+  )
+
+  private val platformOpt =
+    Option(System.getenv(envPlatform)).map(_.trim.toLowerCase).filter(_.nonEmpty).map { x =>
+      platforms.get(x).getOrElse(throw new NoSuchElementException(s"Platform '$x' is unknown'"))
+    }
+
+  def shouldBuildPlatform(platform: sbtcrossproject.Platform): Boolean =
+    platformOpt.isEmpty || platformOpt.contains(platform)
+
+}
