066a8863fc0565d6504d922913c02acaa94ed028
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2023-09-07 18:06:01 -0700

    tokenizers.Chars: remove unused methods

--- scalameta/tokenizers/shared/src/main/scala/scala/meta/internal/tokenizers/Chars.scala
+++ scalameta/tokenizers/shared/src/main/scala/scala/meta/internal/tokenizers/Chars.scala
@@ -3,7 +3,6 @@ package internal
 package tokenizers
 
 import scala.annotation.switch
-import java.lang.{Character => JCharacter}
 import scala.language.postfixOps
 
 /** Contains constants and classifier methods for characters */
@@ -33,36 +32,6 @@ object Chars {
     if (0 <= num && num < base) num else -1
   }
 
-  /** Buffer for creating '\ u XXXX' strings. */
-  private[this] val char2uescapeArray = Array[Char]('\\', 'u', 0, 0, 0, 0)
-
-  /** Convert a character to a backslash-u escape */
-  def char2uescape(c: Char): String = {
-    @inline def hexChar(ch: Int): Char =
-      (if (ch < 10) '0' else 'A' - 10) + ch toChar
-
-    char2uescapeArray(2) = hexChar((c >> 12))
-    char2uescapeArray(3) = hexChar((c >> 8) % 16)
-    char2uescapeArray(4) = hexChar((c >> 4) % 16)
-    char2uescapeArray(5) = hexChar((c) % 16)
-
-    new String(char2uescapeArray)
-  }
-
-  /** Is character a line break? */
-  def isLineBreakChar(c: Char) = (c: @switch) match {
-    case LF | FF | CR | SU => true
-    case _ => false
-  }
-
-  /** Is character a whitespace character (but not a new line)? */
-  def isWhitespace(c: Char) =
-    c == ' ' || c == '\t' || c == CR
-
-  /** Can character form part of a doc comment variable xxx? */
-  def isVarPart(c: Char) =
-    '0' <= c && c <= '9' || 'A' <= c && c <= 'Z' || 'a' <= c && c <= 'z'
-
   /** Can character start an alphanumeric Scala identifier? */
   @inline def isIdentifierStart(c: Char): Boolean =
     (c == '_') || isIdentifierPart(c)
@@ -81,29 +50,12 @@ object Chars {
     chtp == Character.MATH_SYMBOL.toInt || chtp == Character.OTHER_SYMBOL.toInt
   }
 
-  private final val otherLetters = Set[Char]('\u0024', '\u005F') // '$' and '_'
-  private final val letterGroups = {
-    import JCharacter._
-    Set[Byte](LOWERCASE_LETTER, UPPERCASE_LETTER, OTHER_LETTER, TITLECASE_LETTER, LETTER_NUMBER)
-  }
-  def isScalaLetter(ch: Char) = letterGroups(JCharacter.getType(ch).toByte) || otherLetters(ch)
-
   /** Can character form part of a Scala operator name? */
   def isOperatorPart(c: Char): Boolean = (c: @switch) match {
     case '~' | '!' | '@' | '#' | '%' | '^' | '*' | '+' | '-' | '<' | '>' | '?' | ':' | '=' | '&' |
         '|' | '/' | '\\' =>
       true
     case c => isSpecial(c)
-  }
-
-  /**
-   * {{{
-   *  (#x20 | #x9 | #xD | #xA)
-   * }}}
-   */
-  final def isSpace(ch: Char): Boolean = ch match {
-    case '\u0009' | '\u000A' | '\u000D' | '\u0020' => true
-    case _ => false
   }
 
   /**
