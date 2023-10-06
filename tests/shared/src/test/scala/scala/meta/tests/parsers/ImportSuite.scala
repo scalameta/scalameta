cb2a83483687ddb730bd07292f3e124e9a9a0303
Tomasz Godzik <tgodzik@virtuslab.com>
2023-10-06 17:24:46 +0200

    improvement: Don't pattern match in tests in vals
    This is showing up as warnings in Scala 3 since the left side is more specialized and need an explicit unchecked annotation.

--- tests/shared/src/test/scala/scala/meta/tests/parsers/ImportSuite.scala
+++ tests/shared/src/test/scala/scala/meta/tests/parsers/ImportSuite.scala
@@ -8,96 +8,113 @@ import scala.meta.parsers.ParseException
 
 class ImportSuite extends ParseSuite {
   test("import foo.bar") {
-    val Import(Importer(TermName("foo"), Name(Indeterminate("bar")) :: Nil) :: Nil) =
-      templStat("import foo.bar")
+    assertTree(templStat("import foo.bar"))(
+      Import(Importer(TermName("foo"), Name(Indeterminate("bar")) :: Nil) :: Nil)
+    )
   }
 
   test("import foo.bar.baz") {
-    val Import(
-      Importer(Select(TermName("foo"), TermName("bar")), Name(Indeterminate("baz")) :: Nil) :: Nil
-    ) =
-      templStat("import foo.bar.baz")
+    assertTree(templStat("import foo.bar.baz"))(
+      Import(
+        Importer(Select(TermName("foo"), TermName("bar")), Name(Indeterminate("baz")) :: Nil) :: Nil
+      )
+    )
   }
 
   test("import super.foo.bar") {
-    val Import(
-      Importer(
-        Select(Super(Anonymous(), Anonymous()), TermName("foo")),
-        Name(Indeterminate("bar")) :: Nil
-      ) :: Nil
-    ) =
-      templStat("import super.foo.bar")
+    assertTree(templStat("import super.foo.bar"))(
+      Import(
+        Importer(
+          Select(Super(Anonymous(), Anonymous()), TermName("foo")),
+          Name(Indeterminate("bar")) :: Nil
+        ) :: Nil
+      )
+    )
   }
 
   test("import this.foo.bar") {
-    val Import(
-      Importer(Select(This(Anonymous()), TermName("foo")), Name(Indeterminate("bar")) :: Nil) :: Nil
-    ) =
-      templStat("import this.foo.bar")
+    assertTree(templStat("import this.foo.bar"))(
+      Import(
+        Importer(
+          Select(This(Anonymous()), TermName("foo")),
+          Name(Indeterminate("bar")) :: Nil
+        ) :: Nil
+      )
+    )
   }
 
   test("import foo.bar._") {
-    val Import(Importer(Select(TermName("foo"), TermName("bar")), Wildcard() :: Nil) :: Nil) =
-      templStat("import foo.bar._")
+    assertTree(templStat("import foo.bar._"))(
+      Import(Importer(Select(TermName("foo"), TermName("bar")), Wildcard() :: Nil) :: Nil)
+    )
   }
 
   test("import super.foo._") {
-    val Import(
-      Importer(Select(Super(Anonymous(), Anonymous()), TermName("foo")), Wildcard() :: Nil) :: Nil
-    ) =
-      templStat("import super.foo._")
+    assertTree(templStat("import super.foo._"))(
+      Import(
+        Importer(Select(Super(Anonymous(), Anonymous()), TermName("foo")), Wildcard() :: Nil) :: Nil
+      )
+    )
   }
 
   test("import this.foo._") {
-    val Import(Importer(Select(This(Anonymous()), TermName("foo")), Wildcard() :: Nil) :: Nil) =
-      templStat("import this.foo._")
+    assertTree(templStat("import this.foo._"))(
+      Import(Importer(Select(This(Anonymous()), TermName("foo")), Wildcard() :: Nil) :: Nil)
+    )
   }
 
   test("import foo.{bar}") {
-    val Import(Importer(TermName("foo"), Name(Indeterminate("bar")) :: Nil) :: Nil) =
-      templStat("import foo.{bar}")
+    assertTree(templStat("import foo.{bar}"))(
+      Import(Importer(TermName("foo"), Name(Indeterminate("bar")) :: Nil) :: Nil)
+    )
   }
 
   test("import foo.{bar, baz}") {
-    val Import(
-      Importer(TermName("foo"), Name(Indeterminate("bar")) :: (Name(Indeterminate("baz"))) :: Nil)
-        :: Nil
-    ) =
-      templStat("import foo.{bar, baz}")
+    assertTree(templStat("import foo.{bar, baz}"))(
+      Import(
+        Importer(TermName("foo"), Name(Indeterminate("bar")) :: (Name(Indeterminate("baz"))) :: Nil)
+          :: Nil
+      )
+    )
   }
 
   test("import foo.{bar => baz}") {
-    val Import(
-      Importer(TermName("foo"), Rename(Indeterminate("bar"), Indeterminate("baz")) :: Nil) :: Nil
-    ) =
-      templStat("import foo.{bar => baz}")
+    assertTree(templStat("import foo.{bar => baz}"))(
+      Import(
+        Importer(TermName("foo"), Rename(Indeterminate("bar"), Indeterminate("baz")) :: Nil) :: Nil
+      )
+    )
   }
 
   test("import foo.{bar => _}") {
-    val Import(Importer(TermName("foo"), Unimport(Indeterminate("bar")) :: Nil) :: Nil) =
-      templStat("import foo.{bar => _}")
+    assertTree(templStat("import foo.{bar => _}"))(
+      Import(Importer(TermName("foo"), Unimport(Indeterminate("bar")) :: Nil) :: Nil)
+    )
   }
 
   test("import foo.{_ => _}") {
-    val Import(Importer(TermName("foo"), Wildcard() :: Nil) :: Nil) =
-      templStat("import foo.{_ => _}")
+    assertTree(templStat("import foo.{_ => _}"))(
+      Import(Importer(TermName("foo"), Wildcard() :: Nil) :: Nil)
+    )
   }
 
   test("import foo.{bar => _, _}") {
-    val Import(
-      Importer(TermName("foo"), Unimport(Indeterminate("bar")) :: Wildcard() :: Nil) :: Nil
-    ) =
-      templStat("import foo.{bar => _, _}")
+    assertTree(templStat("import foo.{bar => _, _}"))(
+      Import(
+        Importer(TermName("foo"), Unimport(Indeterminate("bar")) :: Wildcard() :: Nil) :: Nil
+      )
+    )
   }
 
   test("import foo.{bar, baz => _, _}") {
-    val Import(
-      Importer(
-        TermName("foo"),
-        (Name(Indeterminate("bar"))) :: Unimport(Indeterminate("baz")) :: Wildcard() :: Nil
-      ) :: Nil
-    ) =
-      templStat("import foo.{bar, baz => _, _}")
+    assertTree(templStat("import foo.{bar, baz => _, _}"))(
+      Import(
+        Importer(
+          TermName("foo"),
+          (Name(Indeterminate("bar"))) :: Unimport(Indeterminate("baz")) :: Wildcard() :: Nil
+        ) :: Nil
+      )
+    )
   }
 
   test("import a.b.{ _, c => _ }") {
