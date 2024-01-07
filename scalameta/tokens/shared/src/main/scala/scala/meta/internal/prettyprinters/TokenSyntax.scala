422ee507f329a2359922058c0277cb32a04bf584
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-01-07 09:49:21 -0800

    Token: dialect consistency check from TokenSyntax (#3474)

--- scalameta/tokens/shared/src/main/scala/scala/meta/internal/prettyprinters/TokenSyntax.scala
+++ scalameta/tokens/shared/src/main/scala/scala/meta/internal/prettyprinters/TokenSyntax.scala
@@ -9,23 +9,6 @@ import scala.meta.tokens.Token._
 
 object TokenSyntax {
   def apply[T <: Token](dialect: Dialect): Syntax[T] = Syntax { x =>
-    def failXml() =
-      throw new UnsupportedOperationException(s"$dialect doesn't support xml literals")
-    def failQuasiquote() =
-      throw new UnsupportedOperationException(s"$dialect doesn't support unquoting")
-    def failViewBound() =
-      throw new UnsupportedOperationException(s"$dialect doesn't support view bounds")
-    x match {
-      case Xml.Start() if !dialect.allowXmlLiterals => failXml()
-      case Xml.Part(_) if !dialect.allowXmlLiterals => failXml()
-      case Xml.SpliceStart() if !dialect.allowXmlLiterals => failXml()
-      case Xml.SpliceEnd() if !dialect.allowXmlLiterals => failXml()
-      case Xml.End() if !dialect.allowXmlLiterals => failXml()
-      case Unquote() if !dialect.allowUnquotes => failQuasiquote()
-      case Ellipsis(_) if !dialect.allowUnquotes => failQuasiquote()
-      case Viewbound() if !dialect.allowViewBounds => failViewBound()
-      case _ => // do nothing, check passed
-    }
     s(x.text)
   }
 }
