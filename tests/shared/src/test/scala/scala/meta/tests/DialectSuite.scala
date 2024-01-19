4bc3f9ee1b9ce011d00bdc02edecff9cc52e31d3
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-01-19 09:07:43 -0800

    DialectSuite: add more tests, including toString (#3504)

--- tests/shared/src/test/scala/scala/meta/tests/DialectSuite.scala
+++ tests/shared/src/test/scala/scala/meta/tests/DialectSuite.scala
@@ -12,28 +12,48 @@ class DialectSuite extends FunSuite {
     assertEquals(Scala3, Scala3)
   }
 
-  test("scala3 != scala3copy") {
-    assertNotEquals(Scala3, scala3copy)
-  }
-
   test("scala3 isEquivalentTo scala3copy") {
+    assertNotEquals(Scala3, scala3copy)
     assert(Scala3.isEquivalentTo(scala3copy))
   }
 
+  test("scala3 isEquivalentTo scala3 without deprecated") {
+    val that = Scala3.withAllowAndTypes(false)
+    assertNotEquals(that, Scala3)
+    assert(Scala3.isEquivalentTo(that))
+  }
+
   test("scala3 isEquivalentTo scala3copy without deprecated") {
-    assert(Scala3.isEquivalentTo(scala3copy.withAllowAndTypes(false)))
+    val that = scala3copy.withAllowAndTypes(false)
+    assertNotEquals(that, scala3copy)
+    assertNotEquals(that, Scala3)
+    assert(Scala3.isEquivalentTo(that))
   }
 
   test("scala212 !isEquivalentTo scala213") {
+    assertNotEquals(Scala213, Scala212)
     assert(!Scala212.isEquivalentTo(Scala213))
   }
 
-  test("scala213 != scala213Source3") {
+  test("scala213 !isEquivalentTo scala213Source3") {
     assertNotEquals(Scala213, Scala213Source3)
+    assert(!Scala213.isEquivalentTo(Scala213Source3))
   }
 
-  test("scala213 !isEquivalentTo scala213Source3") {
-    assert(!Scala213.isEquivalentTo(Scala213Source3))
+  test("scala3 toString") {
+    assertEquals(Scala3.toString, "Scala33")
+  }
+
+  test("scala3copy toString") {
+    assertEquals(scala3copy.toString, "Dialect()")
+  }
+
+  test("scala3 without indentation toString") {
+    assertEquals(Scala3.withAllowSignificantIndentation(false).toString, "Dialect()")
+  }
+
+  test("scala3.unquoteTerm toString") {
+    assertEquals(Scala3.unquoteTerm(true).toString, "Dialect()")
   }
 
 }
