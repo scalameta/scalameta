a7eb487cdc948280d8c53ddf7d60cce27692f031
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-03-12 00:29:34 -0700

    Keywords: extract common base for all SoftKeywords (#3635)

---
+++ scalameta/parsers/shared/src/main/scala/scala/meta/internal/parsers/Keywords.scala
@@ -0,0 +1,24 @@
+package scala.meta.internal.parsers
+
+import scala.meta.tokens.Token
+import scala.reflect.classTag
+
+object Keywords {
+
+  private val identClass = classTag[Token.Ident].runtimeClass
+
+  abstract class IsWithPred(isEnabled: Boolean, pred: String => Boolean) {
+    private def checkEnabled(value: => String): Boolean = pred(value)
+    private val check: (=> String) => Boolean = if (isEnabled) checkEnabled else _ => false
+    @inline final def unapply(value: String): Boolean = check(value)
+    @inline final def unapply(token: Token.Ident): Boolean = check(token.text)
+    @inline final def unapply(token: Token): Boolean =
+      identClass.isInstance(token) && check(token.text)
+    @inline final def apply(token: Token.Ident): Boolean = unapply(token)
+    @inline final def apply(token: Token): Boolean = unapply(token)
+  }
+
+  abstract class IsWithName(isEnabled: Boolean, val name: String)
+      extends IsWithPred(isEnabled, name == _)
+
+}
--- scalameta/parsers/shared/src/main/scala/scala/meta/internal/parsers/SoftKeywords.scala
+++ scalameta/parsers/shared/src/main/scala/scala/meta/internal/parsers/SoftKeywords.scala
@@ -1,96 +1,21 @@
 package scala.meta.internal.parsers
 
 import scala.meta.Dialect
-import scala.meta.tokens.Token
 
 class SoftKeywords(dialect: Dialect) {
 
-  object KwAs {
-    val name = "as"
-    @inline def isEnabled = dialect.allowAsForImportRename
-    @inline final def unapply(token: Token): Boolean = isEnabled && name == token.text
-    @inline final def apply(token: Token): Boolean = unapply(token)
-    @inline final def unapply(token: String): Boolean = isEnabled && name == token
-  }
+  import Keywords._
 
-  object KwUsing {
-    val name = "using"
-    @inline def isEnabled = dialect.allowGivenUsing
-    @inline final def unapply(token: Token): Boolean = isEnabled && name == token.text
-    @inline final def apply(token: Token): Boolean = unapply(token)
-    @inline final def unapply(token: String): Boolean = isEnabled && name == token
-  }
-
-  object KwInline {
-    val name = "inline"
-    @inline def isEnabled = dialect.allowInlineMods
-    @inline final def unapply(token: Token): Boolean = isEnabled && name == token.text
-    @inline final def apply(token: Token): Boolean = unapply(token)
-    @inline final def unapply(token: String): Boolean = isEnabled && name == token
-  }
-
-  object KwOpaque {
-    val name = "opaque"
-    @inline def isEnabled = dialect.allowOpaqueTypes
-    @inline final def unapply(token: Token): Boolean = isEnabled && name == token.text
-    @inline final def apply(token: Token): Boolean = unapply(token)
-    @inline final def unapply(token: String): Boolean = isEnabled && name == token
-  }
-
-  object KwOpen {
-    val name = "open"
-    @inline def isEnabled = dialect.allowOpenClass
-    @inline final def unapply(token: Token): Boolean = isEnabled && name == token.text
-    @inline final def apply(token: Token): Boolean = unapply(token)
-    @inline final def unapply(token: String): Boolean = isEnabled && name == token
-  }
-
-  object KwTransparent {
-    val name = "transparent"
-    @inline def isEnabled = dialect.allowInlineMods
-    @inline final def unapply(token: Token): Boolean = isEnabled && name == token.text
-    @inline final def apply(token: Token): Boolean = unapply(token)
-    @inline final def unapply(token: String): Boolean = isEnabled && name == token
-  }
-
-  object KwDerives {
-    val name = "derives"
-    @inline def isEnabled = dialect.allowDerives
-    @inline final def unapply(token: Token): Boolean = isEnabled && name == token.text
-    @inline final def apply(token: Token): Boolean = unapply(token)
-    @inline final def unapply(token: String): Boolean = isEnabled && name == token
-  }
-
-  object KwEnd {
-    val name = "end"
-    @inline def isEnabled = dialect.allowEndMarker
-    @inline final def unapply(token: Token): Boolean = isEnabled && name == token.text
-    @inline final def apply(token: Token): Boolean = unapply(token)
-    @inline final def unapply(token: String): Boolean = isEnabled && name == token
-  }
-
-  object KwInfix {
-    val name = "infix"
-    @inline def isEnabled = dialect.allowInfixMods
-    @inline final def unapply(token: Token): Boolean = isEnabled && name == token.text
-    @inline final def apply(token: Token): Boolean = unapply(token)
-    @inline final def unapply(token: String): Boolean = isEnabled && name == token
-  }
-
-  object KwExtension {
-    val name = "extension"
-    @inline def isEnabled = dialect.allowExtensionMethods
-    @inline final def unapply(token: Token): Boolean = isEnabled && name == token.text
-    @inline final def apply(token: Token): Boolean = unapply(token)
-    @inline final def unapply(token: String): Boolean = isEnabled && name == token
-  }
-
-  object KwErased {
-    val name = "erased"
-    @inline def isEnabled = dialect.allowErasedDefs
-    @inline final def unapply(token: Token): Boolean = isEnabled && name == token.text
-    @inline final def apply(token: Token): Boolean = unapply(token)
-    @inline final def unapply(token: String): Boolean = isEnabled && name == token
-  }
+  object KwAs extends IsWithName(dialect.allowAsForImportRename, "as")
+  object KwUsing extends IsWithName(dialect.allowGivenUsing, "using")
+  object KwInline extends IsWithName(dialect.allowInlineMods, "inline")
+  object KwOpaque extends IsWithName(dialect.allowOpaqueTypes, "opaque")
+  object KwOpen extends IsWithName(dialect.allowOpenClass, "open")
+  object KwTransparent extends IsWithName(dialect.allowInlineMods, "transparent")
+  object KwDerives extends IsWithName(dialect.allowDerives, "derives")
+  object KwEnd extends IsWithName(dialect.allowEndMarker, "end")
+  object KwInfix extends IsWithName(dialect.allowInfixMods, "infix")
+  object KwExtension extends IsWithName(dialect.allowExtensionMethods, "extension")
+  object KwErased extends IsWithName(dialect.allowErasedDefs, "erased")
 
 }
