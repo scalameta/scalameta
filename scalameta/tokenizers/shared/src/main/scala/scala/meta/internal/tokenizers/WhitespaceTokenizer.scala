c93568e2a4b6dcfc7143c330d89994137efc78eb
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-08-20 05:37:30 -0700

    ScalametaTokenizer: extract whitespace handling (#3893)

---
+++ scalameta/tokenizers/shared/src/main/scala/scala/meta/internal/tokenizers/WhitespaceTokenizer.scala
@@ -0,0 +1,50 @@
+package scala.meta.internal.tokenizers
+
+import scala.meta.Dialect
+import scala.meta.inputs._
+import scala.meta.tokens._
+
+import scala.collection.mutable.ListBuffer
+
+trait WhitespaceTokenizer {
+  def pushHS(token: Token.HSpace): Unit
+  def pushVS(token: Token.EOL): Unit
+
+  def flush(): Unit
+}
+
+object WhitespaceTokenizer {
+
+  def apply(input: Input, dialect: Dialect)(implicit
+      tokens: java.util.Collection[Token]
+  ): WhitespaceTokenizer = new Grouping(input, dialect)
+
+  class Grouping(input: Input, dialect: Dialect)(implicit tokens: java.util.Collection[Token])
+      extends WhitespaceTokenizer {
+    private val bufHS = new ListBuffer[Token.HSpace] // just horizontal tokens
+    private val bufVS = new ListBuffer[Token.EOL] // consecutive EOL, without embedded horizontal space
+
+    private def flushWS[A <: Token](
+        buf: ListBuffer[A]
+    )(f: (Int, Int, List[A]) => Token.Whitespace): Unit = buf.lastOption.foreach { last =>
+      if (buf.length == 1) tokens.add(last)
+      else {
+        val res = buf.result()
+        tokens.add(f(res.head.start, last.end, res))
+      }
+      buf.clear()
+    }
+
+    override def flush(): Unit = {
+      flushWS(bufVS) { case (beg, end, res) => Token.MultiNL(input, dialect, beg, end, res) }
+      flushWS(bufHS) { case (beg, end, res) => Token.MultiHS(input, dialect, beg, end, res) }
+    }
+
+    override def pushHS(token: Token.HSpace): Unit = bufHS += token
+    override def pushVS(token: Token.EOL): Unit = {
+      bufVS += token
+      bufHS.clear() // simply ignore trailing space
+    }
+  }
+
+}
