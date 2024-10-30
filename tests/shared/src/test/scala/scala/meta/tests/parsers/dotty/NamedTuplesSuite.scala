a8e62639ae9c19054fd0d2258064030a1ceac3cd
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2024-10-30 09:25:11 -0700

    NamedTupleSuite: use shortcut methods (#4010)

--- tests/shared/src/test/scala/scala/meta/tests/parsers/dotty/NamedTuplesSuite.scala
+++ tests/shared/src/test/scala/scala/meta/tests/parsers/dotty/NamedTuplesSuite.scala
@@ -6,41 +6,38 @@ class NamedTuplesSuite extends BaseDottySuite {
 
   test("simple-named-term") {
     runTestAssert[Stat]("(a = 123, b = \"123\")")(Term.Tuple(
-      List(Term.Assign(Term.Name("a"), Lit.Int(123)), Term.Assign(Term.Name("b"), Lit.String("123")))
+      List(Term.Assign(tname("a"), lit(123)), Term.Assign(tname("b"), lit("123")))
     ))
   }
 
   test("complex-named-term") {
     runTestAssert[Stat]("(a = 123, b = \"123\", c = (a = 123, d = 2.01d))")(Term.Tuple(List(
-      Term.Assign(Term.Name("a"), Lit.Int(123)),
-      Term.Assign(Term.Name("b"), Lit.String("123")),
+      Term.Assign(tname("a"), lit(123)),
+      Term.Assign(tname("b"), lit("123")),
       Term.Assign(
-        Term.Name("c"),
-        Term.Tuple(List(
-          Term.Assign(Term.Name("a"), Lit.Int(123)),
-          Term.Assign(Term.Name("d"), Lit.Double(2.01d))
-        ))
+        tname("c"),
+        Term.Tuple(List(Term.Assign(tname("a"), lit(123)), Term.Assign(tname("d"), lit(2.01d))))
       )
     )))
   }
 
   test("simple-named-type") {
     runTestAssert[Type]("(a: Int, b: String)")(Type.Tuple(List(
-      Type.TypedParam(Type.Name("a"), Type.Name("Int"), Nil),
-      Type.TypedParam(Type.Name("b"), Type.Name("String"), Nil)
+      Type.TypedParam(pname("a"), pname("Int"), Nil),
+      Type.TypedParam(pname("b"), pname("String"), Nil)
     )))
 
   }
 
   test("complex-named-type") {
     runTestAssert[Type]("(a: Int, b: String, c: (a: Int, d: Double))")(Type.Tuple(List(
-      Type.TypedParam(Type.Name("a"), Type.Name("Int"), Nil),
-      Type.TypedParam(Type.Name("b"), Type.Name("String"), Nil),
+      Type.TypedParam(pname("a"), pname("Int"), Nil),
+      Type.TypedParam(pname("b"), pname("String"), Nil),
       Type.TypedParam(
-        Type.Name("c"),
+        pname("c"),
         Type.Tuple(List(
-          Type.TypedParam(Type.Name("a"), Type.Name("Int"), Nil),
-          Type.TypedParam(Type.Name("d"), Type.Name("Double"), Nil)
+          Type.TypedParam(pname("a"), pname("Int"), Nil),
+          Type.TypedParam(pname("d"), pname("Double"), Nil)
         )),
         Nil
       )
@@ -55,15 +52,12 @@ class NamedTuplesSuite extends BaseDottySuite {
          |}
          |""".stripMargin
     )(Term.Match(
-      Term.Name("a"),
-      Term.CasesBlock(List(Case(
-        Pat.Tuple(List(
-          Pat.Assign(Term.Name("a"), Lit.Int(123)),
-          Pat.Assign(Term.Name("b"), Lit.String("123"))
-        )),
+      tname("a"),
+      List(Case(
+        Pat.Tuple(List(Pat.Assign(tname("a"), lit(123)), Pat.Assign(tname("b"), lit("123")))),
         None,
-        Term.Block(Nil)
-      ))),
+        blk(Nil)
+      )),
       Nil
     ))
   }
@@ -75,21 +69,18 @@ class NamedTuplesSuite extends BaseDottySuite {
          |}
          |""".stripMargin
     )(Term.Match(
-      Term.Name("a"),
-      Term.CasesBlock(List(Case(
+      tname("a"),
+      List(Case(
         Pat.Tuple(List(
-          Pat.Assign(Term.Name("a"), Lit.Int(123)),
+          Pat.Assign(tname("a"), lit(123)),
           Pat.Assign(
-            Term.Name("b"),
-            Pat.Tuple(List(
-              Pat.Assign(Term.Name("c"), Lit.String("123")),
-              Pat.Assign(Term.Name("d"), Lit.Int(123))
-            ))
+            tname("b"),
+            Pat.Tuple(List(Pat.Assign(tname("c"), lit("123")), Pat.Assign(tname("d"), lit(123))))
           )
         )),
         None,
-        Term.Block(Nil)
-      ))),
+        blk(Nil)
+      )),
       Nil
     ))
   }
