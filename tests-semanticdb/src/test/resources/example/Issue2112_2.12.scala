4acdf4611cfd3c53d5406801ecc38cfb89d29979
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-01-05 18:22:26 -0800

    TextDocumentOps: process synthetic val RHS, too

--- tests-semanticdb/src/test/resources/example/Issue2112_2.12.scala
+++ tests-semanticdb/src/test/resources/example/Issue2112_2.12.scala
@@ -3,11 +3,11 @@ package example
 object Issue2112/*<=example.Issue2112.*/ {
   val myNumber/*<=example.Issue2112.myNumber.*/ = 3
   val myList/*<=example.Issue2112.myList.*/ = List/*=>scala.collection.immutable.List.*/(1, 2, 3)
-  myNumber/*=>local0*/ ::/*=>scala.collection.immutable.List#`::`().*/ myList/*=>example.Issue2112.myList.*/
-  myNumber/*=>local1*/ ::/*=>scala.collection.immutable.List#`::`().*/ 5 ::/*=>scala.collection.immutable.List#`::`().*/ Nil/*=>scala.collection.immutable.Nil.*/
+  myNumber/*=>example.Issue2112.myNumber.*/ ::/*=>scala.collection.immutable.List#`::`().*/ myList/*=>example.Issue2112.myList.*/
+  myNumber/*=>example.Issue2112.myNumber.*/ ::/*=>scala.collection.immutable.List#`::`().*/ 5 ::/*=>scala.collection.immutable.List#`::`().*/ Nil/*=>scala.collection.immutable.Nil.*/
 
   def foo/*<=example.Issue2112.foo().*/(myNumber/*<=example.Issue2112.foo().(myNumber)*/: Int/*=>scala.Int#*/) = {
-    myNumber/*=>local3*/ ::/*=>scala.collection.immutable.List#`::`().*/ myList/*=>example.Issue2112.myList.*/
-    myNumber/*=>local4*/ ::/*=>scala.collection.immutable.List#`::`().*/ 5 ::/*=>scala.collection.immutable.List#`::`().*/ Nil/*=>scala.collection.immutable.Nil.*/
+    myNumber/*=>example.Issue2112.foo().(myNumber)*/ ::/*=>scala.collection.immutable.List#`::`().*/ myList/*=>example.Issue2112.myList.*/
+    myNumber/*=>example.Issue2112.foo().(myNumber)*/ ::/*=>scala.collection.immutable.List#`::`().*/ 5 ::/*=>scala.collection.immutable.List#`::`().*/ Nil/*=>scala.collection.immutable.Nil.*/
   }
 }
