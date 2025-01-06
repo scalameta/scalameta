27cb4c43dbb1485fc6f43b71dc58435393556390
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-01-06 12:02:59 -0800

    TextDocumentOps: fix case of single-binder Extract
    Current logic handles only a `val` with a top-level Extract pattern but
    not one with a nested Extract.

--- tests-semanticdb/src/test/resources/example/ValPattern.scala
+++ tests-semanticdb/src/test/resources/example/ValPattern.scala
@@ -7,7 +7,7 @@ class ValPattern/*<=example.ValPattern#*/ {
     Some/*=>scala.Some.*/(1)
 
   var (leftVar/*<=example.ValPattern#leftVar().*/, rightVar/*<=example.ValPattern#rightVar().*/) = (1, 2)
-  var Some/*=>scala.Some.*/(number1Var/*<=example.ValPattern#number1Var.*/) =
+  var Some/*=>scala.Some.*/(number1Var/*<=example.ValPattern#number1Var().*/) =
     Some/*=>scala.Some.*/(1)
 
   def app/*<=example.ValPattern#app().*/(): Unit/*=>scala.Unit#*/ = {
@@ -23,11 +23,11 @@ class ValPattern/*<=example.ValPattern#*/ {
     )
     locally/*=>scala.Predef.locally().*/ {
       val (left/*<=local6*/, right/*<=local7*/) = (1, 2)
-      val Some/*=>scala.Some.*/(number1/*<=local9*/) =
+      val Some/*=>scala.Some.*/(number1/*<=local10*/) =
         Some/*=>scala.Some.*/(1)
 
       var (leftVar/*<=local11*/, rightVar/*<=local12*/) = (1, 2)
-      var Some/*=>scala.Some.*/(number1Var/*<=local14*/) =
+      var Some/*=>scala.Some.*/(number1Var/*<=local15*/) =
         Some/*=>scala.Some.*/(1)
       println/*=>scala.Predef.println(+1).*/(
         (
