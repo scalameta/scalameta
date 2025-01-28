0d648161f4365a0f47b810042da7cfeac2bab38d
Tomasz Godzik <tgodzik@virtuslab.com>
2025-01-28 17:40:03 +0100

    chore: Update Scala 3 to 3.3.5

--- project/Versions.scala
+++ project/Versions.scala
@@ -5,7 +5,7 @@ object Versions {
   val Scala211Versions = getVersions(2, 11, 12 to 12)
   val Scala212Versions = getVersions(2, 12, 17 to 20)
   val Scala213Versions = getVersions(2, 13, 13 to 16)
-  val Scala3Version = "3.3.4"
+  val Scala3Version = "3.3.5"
   val LatestScala211 = Scala211Versions.head
   val LatestScala212 = Scala212Versions.head
   val LatestScala213 = Scala213Versions.head
