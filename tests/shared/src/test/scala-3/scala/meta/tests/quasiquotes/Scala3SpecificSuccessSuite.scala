094ff9117521f3fadb46a0951d4eb04e7464c2a7
Jan Chyb <jan.chyb.99@gmail.com>
2025-05-15 15:48:07 +0200

    Test compile errors with inline quasiquotes

---
+++ tests/shared/src/test/scala-3/scala/meta/tests/quasiquotes/Scala3SpecificSuccessSuite.scala
@@ -0,0 +1,67 @@
+package scala.meta.tests
+
+package quasiquotes
+
+import scala.meta._
+import scala.meta.dialects.Scala3
+
+class Scala3SpecificSuccessSuite extends TreeSuiteBase {
+
+  test("multiline - parse when not inlined") {
+    def foo(name: Type.Name) =
+      q"""
+        class $name:
+          val a: Int = 1
+      """
+    assertPositions(
+      foo(Type.Name("AAA")),
+      """|<?>Defn.Class class AAA { val a: Int = 1 }</?> <none>
+         |<tparamClause>Type.ParamClause @?@</tparamClause> <none>
+         |<ctor>Ctor.Primary @?@</ctor> <none>
+         |<templ>Template { val a: Int = 1 }</templ> <none>
+         |<body>Template.Body { val a: Int = 1 }</body> <none>
+         |<stats0>Defn.Val val a: Int = 1</stats0> <none>
+         |""".stripMargin,
+      showPosition = true,
+      showFieldName = true,
+      skipFullTree = false
+    )
+  }
+
+  test("multiline - parse when inlined") {
+    inline def foo(name: Type.Name) =
+      q"""
+        class $name:
+          val a: Int = 1
+      """
+    assertEquals(
+      compileErrors("""foo(Type.Name("AAA"))"""),
+      s"""|error: double quotes are not allowed in single-line quasiquotes
+          |package scala.meta.tests
+          |             ^""".stripMargin
+    )
+  }
+
+  test("single-line - parse when not inlined") {
+    def foo() = q"val aaa: Int = 1"
+    assertPositions(
+      foo(),
+      """|<?>Defn.Val val aaa: Int = 1</?> [0:val aaa: Int = 1:16)
+         |""".stripMargin,
+      showPosition = true,
+      showFieldName = true,
+      skipFullTree = false
+    )
+  }
+
+  test("single-line - parse when inlined") {
+    inline def foo() = q"val aaa: Int = 1"
+    assertEquals(
+      compileErrors("""foo()"""),
+      s"""|error: `;` expected but `string constant` found
+          |package scala.meta.tests
+          |    ^""".stripMargin
+    )
+  }
+
+}
