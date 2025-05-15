9e3ced512f948a0039af8a9939ea94a4cc43856c
Jan Chyb <jan.chyb.99@gmail.com>
2025-05-15 18:06:02 +0200

    Fix quasiquote parsing in inlined code

--- tests/shared/src/test/scala-3/scala/meta/tests/quasiquotes/Scala3SpecificSuccessSuite.scala
+++ tests/shared/src/test/scala-3/scala/meta/tests/quasiquotes/Scala3SpecificSuccessSuite.scala
@@ -13,22 +13,12 @@ class Scala3SpecificSuccessSuite extends TreeSuiteBase {
         class $name:
           val a: Int = 1
       """
-    assertEquals(
-      compileErrors("""inlinedMultiline(Type.Name("AAA"))"""),
-      s"""error: double quotes are not allowed in single-line quasiquotes
-         |package quasiquotes
-         |^""".stripMargin
-    )
+    inlinedMultiline(Type.Name("AAA"))
   }
 
   test("single-line - parse when inlined") {
     inline def inlinedSingleline() = q"val aaa: Int = 1"
-    assertEquals(
-      compileErrors("""inlinedSingleline()"""),
-      s"""error: `;` expected but `string constant` found
-         |package scala.meta.tests
-         |                  ^""".stripMargin
-    )
+    inlinedSingleline()
   }
 
 }
