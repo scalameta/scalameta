eb2d1a4e0f5ec2351a109ec782f2848258cee0c5
reimai <naugadina@gmail.com>
2025-05-26 15:41:59 +0300

    sort all the lists in TextDocument proto, so it would not change between compilations

--- semanticdb/scalac/library/src/main/scala/scala/meta/internal/semanticdb/scalac/TextDocumentOps.scala
+++ semanticdb/scalac/library/src/main/scala/scala/meta/internal/semanticdb/scalac/TextDocumentOps.scala
@@ -6,8 +6,6 @@ import scala.meta.internal.scalacp._
 import scala.meta.internal.semanticdb.Implicits._
 import scala.meta.internal.semanticdb.Scala._
 import scala.meta.internal.{semanticdb => s}
-
-import scala.annotation.tailrec
 import scala.collection.mutable
 import scala.reflect.internal._
 import scala.reflect.internal.{Flags => gf}
@@ -42,6 +40,10 @@ trait TextDocumentOps {
 
   implicit class XtensionCompilationUnitDocument(unit: g.CompilationUnit) {
     def toTextDocument: s.TextDocument = toTextDocument(None)
+
+    implicit val rangeOrder: Ordering[Option[s.Range]] =
+      Ordering.Option(Ordering.by[s.Range, Int](_.startLine)
+        .orElse(Ordering.by[s.Range, Int](_.startCharacter)))
 
     def toTextDocument(explicitDialect: Option[m.Dialect]): s.TextDocument = {
       clearSymbolPointsCache()
@@ -637,10 +639,10 @@ trait TextDocumentOps {
         text = unit.source.toText,
         md5 = unit.source.toMD5,
         language = s.Language.SCALA,
-        symbols = finalSymbols,
-        occurrences = finalOccurrences,
-        diagnostics = diagnostics,
-        synthetics = synthetics.toScalaSeq
+        symbols = finalSymbols.sortBy(_.symbol),
+        occurrences = finalOccurrences.sortBy(_.range),
+        diagnostics = diagnostics.sortBy(_.range),
+        synthetics = synthetics.toScalaSeq.sortBy(_.range)
       )
     }
   }
