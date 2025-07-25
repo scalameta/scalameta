075d18cb23267ff1cb67dac624236161fd30de96
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-07-25 16:59:57 -0700

    Dialect: add allowNamedTuples, define scala 3.7

--- tests-semanticdb/src/test/scala-2.13/scala/meta/tests/api/SurfaceSuite.scala
+++ tests-semanticdb/src/test/scala-2.13/scala/meta/tests/api/SurfaceSuite.scala
@@ -114,6 +114,7 @@ class SurfaceSuite extends FunSuite {
          |scala.meta.dialects.Scala34 *
          |scala.meta.dialects.Scala35 *
          |scala.meta.dialects.Scala36 *
+         |scala.meta.dialects.Scala37 *
          |scala.meta.dialects.Scala3Future *
          |scala.meta.dialects.Typelevel211 *
          |scala.meta.dialects.Typelevel212 *
