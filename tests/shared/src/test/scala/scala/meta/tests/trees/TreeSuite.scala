6130be853b22f426a4f74da957d2fb7ebbedae9d
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2025-11-13 10:22:33 -0800

    Test tree copying and dialect preservation

--- tests/shared/src/test/scala/scala/meta/tests/trees/TreeSuite.scala
+++ tests/shared/src/test/scala/scala/meta/tests/trees/TreeSuite.scala
@@ -6,7 +6,7 @@ import scala.meta.internal.trees._
 
 import munit._
 
-class TreeSuite extends FunSuite {
+class TreeSuite extends TreeSuiteBase {
   test("Name.unapply") {
     assert(Name.unapply(Term.Name("a")).contains("a"))
     assert(Name.unapply(Type.Name("a")).contains("a"))
@@ -21,5 +21,32 @@ class TreeSuite extends FunSuite {
   }
 
   test(s"Unary opMap size")(assertEquals(Unary.opMap.size, 4))
+
+  test("#4351 copy preserves dialect: scala3-specific tree with inline") {
+    implicit val dialect: Dialect = dialects.Scala3
+    val tree = Defn
+      .Def(Nil, "m", Nil, None, blk(Defn.Def(Mod.Inline() :: Nil, "n", Nil, None, Lit.Unit())))
+    val copy = {
+      implicit val dialect: Dialect = dialects.Scala213
+      tree.copy(body = tree.body)
+    }
+    assertEquals(tree.origin.dialectOpt, Some(dialects.Scala3))
+    assertEquals(copy.origin.dialectOpt, Some(dialects.Scala213))
+    assertEquals(tree.structure, copy.structure)
+
+    val expectedSyntax =
+      """|def m = {
+         |  inline def n = ()
+         |}""".stripMargin.lf2nl
+    // `.syntax` uses implicit dialect unless the tree is parsed
+    assertEquals(tree.syntax, expectedSyntax)
+    assertEquals(copy.syntax, expectedSyntax)
+    // however, `.toString` attempts to take the dialect from the tree
+    assertEquals(tree.toString, expectedSyntax)
+    interceptMessage[UnsupportedOperationException]("Scala213 doesn't support inline modifiers")(
+      copy.toString
+    )
+
+  }
 
 }
