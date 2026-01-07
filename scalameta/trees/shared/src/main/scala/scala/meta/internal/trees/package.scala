9d87033678b871b64d00bfc5389487418f5891a8
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2026-01-07 08:34:36 -0800

    Trees: calculate precedence using Unicode masks

--- scalameta/trees/shared/src/main/scala/scala/meta/internal/trees/package.scala
+++ scalameta/trees/shared/src/main/scala/scala/meta/internal/trees/package.scala
@@ -4,8 +4,6 @@ package internal
 import scala.meta.classifiers._
 import scala.meta.internal.tokens.Chars
 
-import java.lang.{Character => JCharacter}
-
 import scala.annotation.{switch, tailrec}
 import scala.reflect.ClassTag
 
@@ -46,36 +44,29 @@ package object trees {
 
     def isUnaryOp: Boolean = Unary.opMap.contains(value)
 
-    def isAssignmentOp = value match {
-      case "!=" | "<=" | ">=" | "" => false
-      case _ => value.last == '=' && value.head != '=' && Chars.isOperatorPart(value.head)
-    }
+    def isAssignmentOp = precedence == 0
 
-    // opPrecedence?
-    def precedence: Int =
-      if (isAssignmentOp) 0
-      else if (isScalaLetter(value.head)) 1
-      else (value.head: @switch) match {
-        case '|' => 2
-        case '^' => 3
-        case '&' => 4
-        case '=' | '!' => 5
-        case '<' | '>' => 6
-        case ':' => 7
-        case '+' | '-' => 8
-        case '*' | '/' | '%' => 9
-        case _ => 10
+    def precedence: Int = {
+      @inline
+      def assignOr(p: Int) = if (value.last == '=') 0 else p
+      @inline
+      def assignIfPredOr(p: Int, pred: => Boolean) = if (value.last == '=' && pred) 0 else p
+      (value.head: @switch) match {
+        case '$' | '_' => assignOr(1) // also see more under default
+        case '|' => assignOr(2)
+        case '^' => assignOr(3)
+        case '&' => assignOr(4)
+        case '=' => 5 // never assign
+        case '!' => assignIfPredOr(5, value.length > 2) // excludes !=
+        case '<' | '>' => assignIfPredOr(6, value.length > 2) // excludes <=, >=
+        case ':' => assignOr(7)
+        case '+' | '-' => assignOr(8)
+        case '*' | '/' | '%' => assignOr(9)
+        case ch =>
+          if (Chars.isTypeMask(Chars.scalaLetterTypeMask)(ch)) 1
+          else assignIfPredOr(10, Chars.isOperatorPart(ch))
       }
-  }
-
-  object XtensionTreesString {
-    private final val otherLetters = Set[Char]('\u0024', '\u005F') // '$' and '_'
-    private final val letterGroups = {
-      import JCharacter._
-      Set[Byte](LOWERCASE_LETTER, UPPERCASE_LETTER, OTHER_LETTER, TITLECASE_LETTER, LETTER_NUMBER)
     }
-    private def isScalaLetter(ch: Char) = letterGroups(JCharacter.getType(ch).toByte) ||
-      otherLetters(ch)
   }
 
   implicit class XtensionTreesTerm(private val tree: Term) extends AnyVal {
