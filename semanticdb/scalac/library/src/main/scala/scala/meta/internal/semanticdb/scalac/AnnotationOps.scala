0e17a3f8a46718a0849fd579b12de6c4b1c09526
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2026-02-11 17:19:54 -0800

    AnnotationOps: support symbol/type arguments, too
    Also, we no longer fail on unknown constants but return NoTree.

--- semanticdb/scalac/library/src/main/scala/scala/meta/internal/semanticdb/scalac/AnnotationOps.scala
+++ semanticdb/scalac/library/src/main/scala/scala/meta/internal/semanticdb/scalac/AnnotationOps.scala
@@ -10,16 +10,37 @@ trait AnnotationOps {
       gann.atp.toSemanticTpe,
       if (gann.args.nonEmpty) gann.args.map(_.toSemanticTree)
       else gann.assocs.map { case (gname, garg) =>
-        s.AssignTree(s.IdTree(gname.toString()), getClassFileAnnotArg(garg))
+        s.AssignTree(s.IdTree(gname.toString()), toTree(garg))
       }
     )
   }
 
-  private def getClassFileAnnotArg(garg: g.ClassfileAnnotArg): s.Tree = garg match {
-    case garg: g.LiteralAnnotArg => s.LiteralTree(s.Constant(garg.const.value))
-    case garg: g.ArrayAnnotArg => s.ApplyTree(s.NoTree, garg.args.map(getClassFileAnnotArg))
+  private def toTree(garg: g.ClassfileAnnotArg): s.Tree = garg match {
+    case garg: g.LiteralAnnotArg => toTree(garg.const)
+    case garg: g.ArrayAnnotArg => s.ApplyTree(s.NoTree, garg.args.map(toTree))
     case garg: g.NestedAnnotArg => garg.annInfo.toSemantic
     case _ => s.NoTree
+  }
+
+  private def toTree(garg: g.Constant): s.Tree = garg.value match {
+    case x: g.Symbol => s.IdTree(x.toSemantic)
+    case x: g.Type => toTree(x.toSemanticTpe)
+    case x => s.Constant.opt(x).fold[s.Tree](s.NoTree)(s.LiteralTree.apply)
+  }
+
+  private def toTree(sarg: s.Type): s.Tree = sarg match {
+    case sarg: s.ConstantType => s.LiteralTree(sarg.constant)
+    case sarg: s.SingleType => typeToTree(sarg.prefix, sarg.symbol)
+    case sarg: s.TypeRef =>
+      val func = typeToTree(sarg.prefix, sarg.symbol)
+      if (sarg.typeArguments.isEmpty) func else s.TypeApplyTree(func, sarg.typeArguments)
+    case _ => s.NoTree
+  }
+
+  private def typeToTree(sarg: s.Type, ssym: String): s.Tree = {
+    val pre = toTree(sarg)
+    val sym = s.IdTree(ssym)
+    if (pre eq s.NoTree) sym else s.SelectTree(pre, Some(sym))
   }
 
 }
