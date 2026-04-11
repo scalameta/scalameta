8f888aaab5818c34eda17bb511204252ff541924
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2026-04-10 17:12:24 -0700

    Parsed/Tokenized: extract into own source file

---
+++ scalameta/parsers/shared/src/main/scala/scala/meta/parsers/Parsed.scala
@@ -0,0 +1,29 @@
+package scala.meta.parsers
+
+import org.scalameta.adt._
+import scala.meta.inputs._
+
+trait Parsed[+T] extends Product with Serializable with Metadata.Adt with Equals {
+
+  def fold[A](fe: Parsed.Error => A, ft: T => A): A = this match {
+    case x: Parsed.Success[_] => ft(x.tree)
+    case e: Parsed.Error => fe(e)
+  }
+
+  def get: T = fold(e => throw e.details, identity)
+  def orElse[U >: T](alt: => Parsed[U]): Parsed[U] = fold(_ => alt, _ => this)
+  def getOrElse[U >: T](alt: => U): U = fold(_ => alt, identity)
+
+  def toOption: Option[T] = fold(_ => None, t => Some(t))
+  def toEither: Either[Parsed.Error, T] = fold(e => Left(e), t => Right(t))
+
+}
+
+object Parsed {
+  case class Success[+T](tree: T) extends Parsed[T] {
+    override def toString = tree.toString
+  }
+  case class Error(pos: Position, message: String, details: Exception) extends Parsed[Nothing] {
+    override def toString = details.toString
+  }
+}
--- scalameta/tokenizers/shared/src/main/scala/scala/meta/tokenizers/Errors.scala
+++ scalameta/tokenizers/shared/src/main/scala/scala/meta/tokenizers/Errors.scala
@@ -2,32 +2,6 @@ package scala.meta
 package tokenizers
 
 import scala.meta.inputs._
-import scala.meta.tokens._
-
-sealed trait Tokenized {
-
-  def fold[A](fe: Tokenized.Error => A, ft: Tokens => A): A = this match {
-    case x: Tokenized.Success => ft(x.tokens)
-    case e: Tokenized.Error => fe(e)
-  }
-
-  def get: Tokens = fold(e => throw e.details, identity)
-  def orElse(alt: => Tokenized): Tokenized = fold(_ => alt, _ => this)
-  def getOrElse(alt: => Tokens): Tokens = fold(_ => alt, identity)
-
-  def toOption: Option[Tokens] = fold(_ => None, t => Some(t))
-  def toEither: Either[Tokenized.Error, Tokens] = fold(e => Left(e), t => Right(t))
-
-}
-
-object Tokenized {
-  case class Success(tokens: Tokens) extends Tokenized {
-    override def toString = tokens.toString
-  }
-  case class Error(pos: Position, message: String, details: Exception) extends Tokenized {
-    override def toString = details.toString
-  }
-}
 
 case class TokenizeException(pos: Position, shortMessage: String)
     extends InputException(pos, shortMessage) {
