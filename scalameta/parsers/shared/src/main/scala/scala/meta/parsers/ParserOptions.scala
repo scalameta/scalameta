3fc95e6988946a601b66c4c7fd20bf2bb4d340db
Albert Meltzer <7529386+kitbellew@users.noreply.github.com>
2026-04-11 09:22:18 -0700

    {Parser,Tokenizer}Options: make ctor private

--- scalameta/parsers/shared/src/main/scala/scala/meta/parsers/ParserOptions.scala
+++ scalameta/parsers/shared/src/main/scala/scala/meta/parsers/ParserOptions.scala
@@ -2,10 +2,15 @@ package scala.meta.parsers
 
 import scala.util.DynamicVariable
 
-final class ParserOptions(
+final class ParserOptions private[meta] (
     // options which control parsing
     val captureComments: Boolean = true
-)
+) {
+  def withCaptureComments(value: Boolean): ParserOptions = privateCopy(captureComments = value)
+
+  private def privateCopy(captureComments: Boolean = this.captureComments): ParserOptions =
+    new ParserOptions(captureComments = captureComments)
+}
 
 object ParserOptions {
   val default: ParserOptions = new ParserOptions()
--- scalameta/tokenizers/shared/src/main/scala/scala/meta/tokenizers/TokenizerOptions.scala
+++ scalameta/tokenizers/shared/src/main/scala/scala/meta/tokenizers/TokenizerOptions.scala
@@ -2,10 +2,15 @@ package scala.meta.tokenizers
 
 import scala.util.DynamicVariable
 
-final class TokenizerOptions(
+final class TokenizerOptions private[meta] (
     // options which control how tokens are emitted
     val groupWhitespace: Boolean = false
-)
+) {
+  def withGroupWhitespace(value: Boolean): TokenizerOptions = privateCopy(groupWhitespace = value)
+
+  private def privateCopy(groupWhitespace: Boolean = this.groupWhitespace): TokenizerOptions =
+    new TokenizerOptions(groupWhitespace = groupWhitespace)
+}
 
 object TokenizerOptions {
   val default: TokenizerOptions = new TokenizerOptions()
