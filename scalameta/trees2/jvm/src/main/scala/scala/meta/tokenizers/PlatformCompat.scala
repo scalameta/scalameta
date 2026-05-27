e7a95dca604f3a31ea370da67d35616bb690f1b3
zainab-ali <zainab@kebab-ca.se>
2026-05-27 14:51:36 +0100

    Specify class loader when loading Tokenize service.

--- scalameta/trees2/jvm/src/main/scala/scala/meta/tokenizers/PlatformCompat.scala
+++ scalameta/trees2/jvm/src/main/scala/scala/meta/tokenizers/PlatformCompat.scala
@@ -5,7 +5,7 @@ import java.util.ServiceLoader
 object PlatformCompat {
 
   lazy val loadTokenize: Option[Tokenize] = {
-    val factories = ServiceLoader.load(classOf[Tokenize]).iterator()
+    val factories = ServiceLoader.load(classOf[Tokenize], getClass().getClassLoader()).iterator()
     if (factories.hasNext) Some(factories.next()) else None
   }
 
