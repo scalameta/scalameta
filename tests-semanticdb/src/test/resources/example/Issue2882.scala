37f9ecf7d0d256d48a7507e5ca0f8bdd3cc72836
Jozef Koval <jozef.koval@protonmail.ch>
2026-06-11 16:08:27 +0200

    Emit constructor parameter occurrences at class parameter definition sites

--- tests-semanticdb/src/test/resources/example/Issue2882.scala
+++ tests-semanticdb/src/test/resources/example/Issue2882.scala
@@ -2,7 +2,7 @@ package example
 
 object Issue2882/*<=example.Issue2882.*/ {
 
-  case class FooFailure/*<=example.Issue2882.FooFailure#*/(msg/*<=example.Issue2882.FooFailure#msg.*/: String/*=>scala.Predef.String#*/)
+  case class FooFailure/*<=example.Issue2882.FooFailure#*/(msg/*<=example.Issue2882.FooFailure#msg.*//*<=example.Issue2882.FooFailure#`<init>`().(msg)*/: String/*=>scala.Predef.String#*/)
     extends Exception/*=>scala.package.Exception#*//*=>java.lang.Exception#`<init>`(+1).*/(s/*=>scala.StringContext#s().*/"Error: $msg/*=>example.Issue2882.FooFailure#`<init>`().(msg)*/")
 
 }
